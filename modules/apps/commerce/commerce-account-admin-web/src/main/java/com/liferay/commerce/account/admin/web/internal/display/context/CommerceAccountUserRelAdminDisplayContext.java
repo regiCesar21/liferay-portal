98130211b192372cf69ebaad101886335b98dee9
Alec Sloan <Alec.sloan@Liferay.com>
2019-09-24 09:46:19 -0700

    COMMERCE-1684 Source Formatting

--- modules/apps/commerce/commerce-account-admin-web/src/main/java/com/liferay/commerce/account/admin/web/internal/display/context/CommerceAccountUserRelAdminDisplayContext.java
+++ modules/apps/commerce/commerce-account-admin-web/src/main/java/com/liferay/commerce/account/admin/web/internal/display/context/CommerceAccountUserRelAdminDisplayContext.java
@@ -140,18 +140,9 @@ public class CommerceAccountUserRelAdminDisplayContext
 	public String getUserRoleIds(CommerceAccountUserRel commerceAccountUserRel)
 		throws PortalException {
 
-		List<UserGroupRole> userGroupRoles =
-			commerceAccountUserRel.getUserGroupRoles();
-
-		Stream<UserGroupRole> stream = userGroupRoles.stream();
-
-		Long[] rolesArray = stream.map(
-			UserGroupRole::getRoleId
-		).toArray(
-			Long[]::new
-		);
-
-		return StringUtil.merge(rolesArray, StringPool.COMMA_AND_SPACE);
+		return ListUtil.toString(
+			commerceAccountUserRel.getUserGroupRoles(), "roleId",
+			StringPool.COMMA_AND_SPACE);
 	}
 
 	public String getUserRoleItemSelectorUrl(
@@ -179,7 +170,7 @@ public class CommerceAccountUserRelAdminDisplayContext
 
 		itemSelectorURL.setParameter(
 			"checkedRoleIds", getUserRoleIds(commerceAccountUserRel));
-		itemSelectorURL.setParameter("rowChecker", "");
+		itemSelectorURL.setParameter("rowChecker", StringPool.BLANK);
 
 		itemSelectorURL.setWindowState(LiferayWindowState.POP_UP);
 
