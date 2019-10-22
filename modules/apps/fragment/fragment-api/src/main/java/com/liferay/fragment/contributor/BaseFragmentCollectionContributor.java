2c0a382313b247b9d25e8021b94cba543f9527ab
Eudaldo Alonso <eudaldo.alonso@liferay.com>
2019-10-22 17:28:05 +0200

    LPS-178527 Partial backport of LPS-103522: Extract method since this logic is going to be reused in subsequent commits
    Original commit message: LPS-103522 Extract code to a new method dbdc445df2a049dd0261116be9205160830ebd11

--- modules/apps/fragment/fragment-api/src/main/java/com/liferay/fragment/contributor/BaseFragmentCollectionContributor.java
+++ modules/apps/fragment/fragment-api/src/main/java/com/liferay/fragment/contributor/BaseFragmentCollectionContributor.java
@@ -69,31 +69,7 @@ public abstract class BaseFragmentCollectionContributor
 
 	@Override
 	public List<FragmentEntry> getFragmentEntries(int type, Locale locale) {
-		_initialize();
-
-		List<FragmentEntry> fragmentEntries = _fragmentEntries.getOrDefault(
-			type, Collections.emptyList());
-
-		Stream<FragmentEntry> stream = fragmentEntries.stream();
-
-		return stream.map(
-			fragmentEntry -> {
-				Map<Locale, String> names = _fragmentEntryNames.getOrDefault(
-					fragmentEntry.getFragmentEntryKey(),
-					Collections.emptyMap());
-
-				fragmentEntry.setName(
-					names.getOrDefault(
-						locale,
-						names.getOrDefault(
-							LocaleUtil.toLanguageId(LocaleUtil.getDefault()),
-							fragmentEntry.getName())));
-
-				return fragmentEntry;
-			}
-		).collect(
-			Collectors.toList()
-		);
+		return _getFragmentEntries(getFragmentEntries(type), locale);
 	}
 
 	@Override
@@ -211,6 +187,31 @@ public abstract class BaseFragmentCollectionContributor
 		_setLocalizedNames(name, names, getResourceBundleLoader());
 
 		return names;
+	}
+
+	private List<FragmentEntry> _getFragmentEntries(
+		List<FragmentEntry> fragmentEntries, Locale locale) {
+
+		Stream<FragmentEntry> stream = fragmentEntries.stream();
+
+		return stream.map(
+			fragmentEntry -> {
+				Map<Locale, String> names = _fragmentEntryNames.getOrDefault(
+					fragmentEntry.getFragmentEntryKey(),
+					Collections.emptyMap());
+
+				fragmentEntry.setName(
+					names.getOrDefault(
+						locale,
+						names.getOrDefault(
+							LocaleUtil.toLanguageId(LocaleUtil.getDefault()),
+							fragmentEntry.getName())));
+
+				return fragmentEntry;
+			}
+		).collect(
+			Collectors.toList()
+		);
 	}
 
 	private FragmentEntry _getFragmentEntry(URL url) throws Exception {
