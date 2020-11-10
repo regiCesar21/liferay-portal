d90fa3453866308ccd3f2cf66dde39fb2c1057d6
Alicia Garcia <alicia.garcia@liferay.com>
2020-11-10 14:12:42 +0100

    LPS-122888 obtain FileEntry by fileName or title
    (cherry picked from commit 6419de611500ea7b001f3e59946e3c29344b95ae)

--- modules/apps/document-library/document-library-web/src/main/java/com/liferay/document/library/web/internal/webdav/DLWebDAVStorageImpl.java
+++ modules/apps/document-library/document-library-web/src/main/java/com/liferay/document/library/web/internal/webdav/DLWebDAVStorageImpl.java
@@ -369,7 +369,7 @@ public class DLWebDAVStorageImpl extends BaseWebDAVStorageImpl {
 				}
 
 				try {
-					FileEntry fileEntry = _dlAppService.getFileEntry(
+					FileEntry fileEntry = _getFileEntryByName(
 						webDAVRequest.getGroupId(), parentFolderId,
 						getTitle(pathArray));
 
@@ -678,7 +678,7 @@ public class DLWebDAVStorageImpl extends BaseWebDAVStorageImpl {
 
 			if (webDAVRequest.isMac()) {
 				try {
-					FileEntry destFileEntry = _dlAppService.getFileEntry(
+					FileEntry destFileEntry = _getFileEntryByName(
 						groupId, newParentFolderId, title);
 
 					InputStream inputStream = fileEntry.getContentStream();
@@ -777,7 +777,7 @@ public class DLWebDAVStorageImpl extends BaseWebDAVStorageImpl {
 				httpServletRequest, file, title);
 
 			try {
-				FileEntry fileEntry = _dlAppService.getFileEntry(
+				FileEntry fileEntry = _getFileEntryByName(
 					webDAVRequest.getGroupId(), parentFolderId, title);
 
 				if (!hasLock(fileEntry, webDAVRequest.getLockUuid()) &&
@@ -945,7 +945,7 @@ public class DLWebDAVStorageImpl extends BaseWebDAVStorageImpl {
 			}
 
 			try {
-				FileEntry fileEntry = _dlAppService.getFileEntry(
+				FileEntry fileEntry = _getFileEntryByName(
 					groupId, parentFolderId, name);
 
 				if (!hasLock(fileEntry, lockUuid) &&
@@ -1209,6 +1209,23 @@ public class DLWebDAVStorageImpl extends BaseWebDAVStorageImpl {
 		resource.setPrimaryKey(folder.getPrimaryKey());
 
 		return resource;
+	}
+
+	private FileEntry _getFileEntryByName(
+			long groupId, long parentFolderId, String name)
+		throws PortalException {
+
+		try {
+			return _dlAppService.getFileEntry(groupId, parentFolderId, name);
+		}
+		catch (NoSuchFileEntryException noSuchFileEntryException) {
+			if (_log.isDebugEnabled()) {
+				_log.debug(noSuchFileEntryException, noSuchFileEntryException);
+			}
+
+			return _dlAppService.getFileEntryByFileName(
+				groupId, parentFolderId, name);
+		}
 	}
 
 	private ServiceContext _getServiceContext(
