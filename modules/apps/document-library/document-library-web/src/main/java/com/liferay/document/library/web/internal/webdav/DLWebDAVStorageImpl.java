22b026ba5c7a5a68c97fabe7f0811da5fd6a6194
Alicia Garcia <alicia.garcia@liferay.com>
2020-11-25 18:08:03 +0100

    LPS-122888 when update filename from the OS, the title should be the same to previous value
    (cherry picked from commit f794d2bba0ce7cb598aab418d76827e8e017c930)

--- modules/apps/document-library/document-library-web/src/main/java/com/liferay/document/library/web/internal/webdav/DLWebDAVStorageImpl.java
+++ modules/apps/document-library/document-library-web/src/main/java/com/liferay/document/library/web/internal/webdav/DLWebDAVStorageImpl.java
@@ -690,10 +690,7 @@ public class DLWebDAVStorageImpl extends BaseWebDAVStorageImpl {
 					_dlAppService.updateFileEntry(
 						destFileEntry.getFileEntryId(),
 						destFileEntry.getFileName(),
-						destFileEntry.getMimeType(),
-						_getTitle(
-							destFileEntry.getFileName(),
-							fileEntry.getFileName(), destFileEntry.getTitle()),
+						destFileEntry.getMimeType(), destFileEntry.getTitle(),
 						destFileEntry.getDescription(), StringPool.BLANK,
 						DLVersionNumberIncrease.MINOR, file, serviceContext);
 
@@ -713,10 +710,9 @@ public class DLWebDAVStorageImpl extends BaseWebDAVStorageImpl {
 
 			_dlAppService.updateFileEntry(
 				fileEntry.getFileEntryId(), fileName, fileEntry.getMimeType(),
-				_getTitle(
-					fileName, fileEntry.getFileName(), fileEntry.getTitle()),
-				fileEntry.getDescription(), StringPool.BLANK,
-				DLVersionNumberIncrease.MINOR, file, serviceContext);
+				fileEntry.getTitle(), fileEntry.getDescription(),
+				StringPool.BLANK, DLVersionNumberIncrease.MINOR, file,
+				serviceContext);
 
 			if (fileEntry.getFolderId() != newParentFolderId) {
 				_dlAppService.moveFileEntry(
@@ -797,11 +793,9 @@ public class DLWebDAVStorageImpl extends BaseWebDAVStorageImpl {
 
 				_dlAppService.updateFileEntry(
 					fileEntry.getFileEntryId(), fileName, contentType,
-					_getTitle(
-						fileName, fileEntry.getFileName(),
-						fileEntry.getTitle()),
-					fileEntry.getDescription(), StringPool.BLANK,
-					DLVersionNumberIncrease.MINOR, file, serviceContext);
+					fileEntry.getTitle(), fileEntry.getDescription(),
+					StringPool.BLANK, DLVersionNumberIncrease.MINOR, file,
+					serviceContext);
 			}
 			catch (NoSuchFileEntryException noSuchFileEntryException) {
 				if (_log.isDebugEnabled()) {
@@ -1255,16 +1249,6 @@ public class DLWebDAVStorageImpl extends BaseWebDAVStorageImpl {
 			webDAVRequest.getGroupId(), className);
 
 		return serviceContext;
-	}
-
-	private String _getTitle(
-		String newFileName, String originalFileName, String title) {
-
-		if (Objects.equals(originalFileName, newFileName)) {
-			return title;
-		}
-
-		return newFileName;
 	}
 
 	private static final Log _log = LogFactoryUtil.getLog(
