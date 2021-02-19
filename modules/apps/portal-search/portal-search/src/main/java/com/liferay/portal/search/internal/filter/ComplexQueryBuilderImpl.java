04578acaf90374f0b93137b62b5b33a6ee669787
Bryan Engler <bryan.engler@liferay.com>
2021-02-19 06:43:41 -0800

    LPS-126703 portal-search: Warn when additive clause isnt a "should" occur
    https://issues.liferay.com/browse/LPS-126703

--- modules/apps/portal-search/portal-search/src/main/java/com/liferay/portal/search/internal/filter/ComplexQueryBuilderImpl.java
+++ modules/apps/portal-search/portal-search/src/main/java/com/liferay/portal/search/internal/filter/ComplexQueryBuilderImpl.java
@@ -5,6 +5,8 @@
 
 package com.liferay.portal.search.internal.filter;
 
+import com.liferay.portal.kernel.log.Log;
+import com.liferay.portal.kernel.log.LogFactoryUtil;
 import com.liferay.portal.kernel.util.GetterUtil;
 import com.liferay.portal.kernel.util.Validator;
 import com.liferay.portal.search.filter.ComplexQueryBuilder;
@@ -87,6 +89,9 @@ public class ComplexQueryBuilderImpl implements ComplexQueryBuilder {
 		return _queries.booleanQuery();
 	}
 
+	private static final Log _log = LogFactoryUtil.getLog(
+		ComplexQueryBuilderImpl.class);
+
 	private BooleanQuery _booleanQuery;
 	private final List<ComplexQueryPart> _complexQueryParts = new ArrayList<>();
 	private final Queries _queries;
@@ -115,9 +120,20 @@ public class ComplexQueryBuilderImpl implements ComplexQueryBuilder {
 				return null;
 			}
 
+			String occur = complexQueryPart.getOccur();
+
+			if (complexQueryPart.isAdditive() && !occur.equals("should")) {
+				if (_log.isWarnEnabled()) {
+					_log.warn(
+						"Additive complex query part with " +
+							complexQueryPart.getOccur() +
+								" occur may not behave as expected");
+				}
+			}
+
 			addQueryClause(
-				getParentBooleanQuery(complexQueryPart.getParent()),
-				complexQueryPart.getOccur(), query);
+				getParentBooleanQuery(complexQueryPart.getParent()), occur,
+				query);
 
 			return query;
 		}
