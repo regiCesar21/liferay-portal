cb621f1b8394f410b834bad92fb6de09a392a7b9
Neil Griffin <neil.griffin.scm@gmail.com>
2021-06-04 11:30:51 -0400

    LPS-133689 Multi-value PortletRequest parameters contain null instead of an empty string

--- portal-impl/src/com/liferay/portlet/internal/PortletRequestImpl.java
+++ portal-impl/src/com/liferay/portlet/internal/PortletRequestImpl.java
@@ -1399,7 +1399,10 @@ public abstract class PortletRequestImpl implements LiferayPortletRequest {
 
 			if ((values != null) && (portletSpecMajorVersion >= 3)) {
 				for (int i = 0; i < values.length; i++) {
-					if ((values[i] != null) && values[i].isEmpty()) {
+					if (Objects.equals(
+							values[i],
+							LiferayMutablePortletParameters.NULL_PARAM_VALUE)) {
+
 						values[i] = null;
 					}
 				}
