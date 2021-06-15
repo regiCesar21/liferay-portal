4f82037867bf09abef4451fb5f1357a1910b9b2f
Jürgen Kappler <jurgen.kappler@liferay.com>
2021-06-15 16:16:46 +0200

    LPS-130506 We shouldn't reindex articles that are used to set default values, so add restriction for it
    (cherry picked from commit 10f1444af5e766d08b3a9b6397d33d4fe3d9823c)

--- modules/apps/journal/journal-service/src/main/java/com/liferay/journal/internal/search/JournalArticleIndexer.java
+++ modules/apps/journal/journal-service/src/main/java/com/liferay/journal/internal/search/JournalArticleIndexer.java
@@ -32,6 +32,8 @@ import com.liferay.petra.string.CharPool;
 import com.liferay.petra.string.StringPool;
 import com.liferay.portal.kernel.change.tracking.CTCollectionThreadLocal;
 import com.liferay.portal.kernel.dao.orm.IndexableActionableDynamicQuery;
+import com.liferay.portal.kernel.dao.orm.Property;
+import com.liferay.portal.kernel.dao.orm.PropertyFactoryUtil;
 import com.liferay.portal.kernel.exception.PortalException;
 import com.liferay.portal.kernel.log.Log;
 import com.liferay.portal.kernel.log.LogFactoryUtil;
@@ -783,6 +785,16 @@ public class JournalArticleIndexer extends BaseIndexer<JournalArticle> {
 			indexableActionableDynamicQuery =
 				_journalArticleLocalService.
 					getIndexableActionableDynamicQuery();
+
+			indexableActionableDynamicQuery.setAddCriteriaMethod(
+				dynamicQuery -> {
+					Property classNameIdProperty = PropertyFactoryUtil.forName(
+						"classNameId");
+
+					dynamicQuery.add(
+						classNameIdProperty.ne(
+							_portal.getClassNameId(DDMStructure.class)));
+				});
 
 			indexableActionableDynamicQuery.setInterval(
 				_batchIndexingHelper.getBulkSize(
