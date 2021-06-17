ab16be4c26321149dd660a1ac22d9c47051049b2
Brian I. Kim <il.kim@liferay.com>
2021-06-17 11:40:08 -0700

    COMMERCE-6788 Improve logic to make sure non-shippable items are not counted as shippable.

--- modules/apps/commerce/commerce-service/src/main/java/com/liferay/commerce/internal/order/status/ShippedCommerceOrderStatusImpl.java
+++ modules/apps/commerce/commerce-service/src/main/java/com/liferay/commerce/internal/order/status/ShippedCommerceOrderStatusImpl.java
@@ -10,6 +10,7 @@ import com.liferay.commerce.model.CommerceOrder;
 import com.liferay.commerce.model.CommerceOrderItem;
 import com.liferay.commerce.order.engine.CommerceOrderEngine;
 import com.liferay.commerce.order.status.CommerceOrderStatus;
+import com.liferay.commerce.product.model.CPDefinition;
 import com.liferay.commerce.service.CommerceOrderService;
 import com.liferay.commerce.util.CommerceShippingHelper;
 import com.liferay.portal.kernel.exception.PortalException;
@@ -89,8 +90,12 @@ public class ShippedCommerceOrderStatusImpl implements CommerceOrderStatus {
 		for (CommerceOrderItem shippedCommerceOrderItem :
 				commerceOrder.getCommerceOrderItems()) {
 
-			if (shippedCommerceOrderItem.getShippedQuantity() <
-					shippedCommerceOrderItem.getQuantity()) {
+			CPDefinition cpDefinition =
+				shippedCommerceOrderItem.getCPDefinition();
+
+			if ((shippedCommerceOrderItem.getShippedQuantity() <
+					shippedCommerceOrderItem.getQuantity()) &&
+				cpDefinition.isShippable()) {
 
 				allOrderItemsShipped = false;
 			}
@@ -100,8 +105,7 @@ public class ShippedCommerceOrderStatusImpl implements CommerceOrderStatus {
 				CommerceOrderConstants.ORDER_STATUS_PROCESSING) ||
 			 (commerceOrder.getOrderStatus() ==
 				 CommerceOrderConstants.ORDER_STATUS_PARTIALLY_SHIPPED)) &&
-			(allOrderItemsShipped ||
-			 !_commerceShippingHelper.isShippable(commerceOrder))) {
+			allOrderItemsShipped) {
 
 			return true;
 		}
