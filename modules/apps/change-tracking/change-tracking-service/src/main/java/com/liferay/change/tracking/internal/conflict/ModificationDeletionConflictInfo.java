bb617a6e9a9958e1cd5de97e3136691a26823c81
Preston-Crary <preston.crary@liferay.com>
2021-08-31 15:46:47 -0700

    LPS-138316 Report conflicts when a deletion in a publication is for an updated row.

---
+++ modules/apps/change-tracking/change-tracking-service/src/main/java/com/liferay/change/tracking/internal/conflict/ModificationDeletionConflictInfo.java
@@ -0,0 +1,45 @@
+/**
+ * SPDX-FileCopyrightText: (c) 2023 Liferay, Inc. https://liferay.com
+ * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
+ */
+
+package com.liferay.change.tracking.internal.conflict;
+
+import com.liferay.portal.kernel.language.LanguageUtil;
+
+import java.util.ResourceBundle;
+
+/**
+ * @author Preston Crary
+ */
+public class ModificationDeletionConflictInfo extends BaseConflictInfo {
+
+	public ModificationDeletionConflictInfo(long modelClassPK) {
+		_modelClassPK = modelClassPK;
+	}
+
+	@Override
+	public String getConflictDescription(ResourceBundle resourceBundle) {
+		return LanguageUtil.get(
+			resourceBundle, "modification-deletion-conflict");
+	}
+
+	@Override
+	public String getResolutionDescription(ResourceBundle resourceBundle) {
+		return LanguageUtil.get(
+			resourceBundle, "deletion-conflicts-with-a-modification");
+	}
+
+	@Override
+	public long getSourcePrimaryKey() {
+		return _modelClassPK;
+	}
+
+	@Override
+	public long getTargetPrimaryKey() {
+		return _modelClassPK;
+	}
+
+	private final long _modelClassPK;
+
+}
