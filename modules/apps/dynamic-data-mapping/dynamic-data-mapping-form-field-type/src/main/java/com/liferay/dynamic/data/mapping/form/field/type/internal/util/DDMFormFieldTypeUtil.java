079390aa8447cf9d902fd0dd6ca772252414b4cc
Marcela Cunha <marcela.cunha@liferay.com>
2021-09-01 18:13:44 -0300

    LPS-138296 Add method to retrieve the value because can be an array of boolean

--- modules/apps/dynamic-data-mapping/dynamic-data-mapping-form-field-type/src/main/java/com/liferay/dynamic/data/mapping/form/field/type/internal/util/DDMFormFieldTypeUtil.java
+++ modules/apps/dynamic-data-mapping/dynamic-data-mapping-form-field-type/src/main/java/com/liferay/dynamic/data/mapping/form/field/type/internal/util/DDMFormFieldTypeUtil.java
@@ -18,6 +18,11 @@ import com.liferay.dynamic.data.mapping.model.DDMFormField;
 import com.liferay.dynamic.data.mapping.model.LocalizedValue;
 import com.liferay.dynamic.data.mapping.render.DDMFormFieldRenderingContext;
 import com.liferay.petra.string.StringPool;
+import com.liferay.portal.kernel.json.JSONArray;
+import com.liferay.portal.kernel.json.JSONException;
+import com.liferay.portal.kernel.json.JSONFactoryUtil;
+import com.liferay.portal.kernel.log.Log;
+import com.liferay.portal.kernel.log.LogFactoryUtil;
 import com.liferay.portal.kernel.util.GetterUtil;
 
 import java.util.Locale;
@@ -60,5 +65,23 @@ public class DDMFormFieldTypeUtil {
 		return GetterUtil.getString(
 			ddmFormFieldRenderingContext.getProperty(propertyName));
 	}
+
+	public static String getValue(String valueString) {
+		try {
+			JSONArray jsonArray = JSONFactoryUtil.createJSONArray(valueString);
+
+			return GetterUtil.getString(jsonArray.get(0));
+		}
+		catch (JSONException jsonException) {
+			if (_log.isDebugEnabled()) {
+				_log.debug(jsonException, jsonException);
+			}
+		}
+
+		return valueString;
+	}
+
+	private static final Log _log = LogFactoryUtil.getLog(
+		DDMFormFieldTypeUtil.class);
 
 }
