472d39c5e4f88a4c957d47e0b8c63575f2f7ecce
Jorge Avalos <jorge.avalos@liferay.com>
2021-10-18 13:23:35 -0700

    LPS-139493 Update the layout when updating the fragmentEntryLink

--- modules/apps/fragment/fragment-service/src/main/java/com/liferay/fragment/service/impl/FragmentEntryLinkLocalServiceImpl.java
+++ modules/apps/fragment/fragment-service/src/main/java/com/liferay/fragment/service/impl/FragmentEntryLinkLocalServiceImpl.java
@@ -693,7 +693,10 @@ public class FragmentEntryLinkLocalServiceImpl
 
 		fragmentEntryLink.setLastPropagationDate(new Date());
 
-		fragmentEntryLinkPersistence.update(fragmentEntryLink);
+		fragmentEntryLink = fragmentEntryLinkPersistence.update(
+			fragmentEntryLink);
+
+		_updateFragmentEntryLinkLayout(fragmentEntryLink);
 	}
 
 	private String _getProcessedHTML(
@@ -814,6 +817,22 @@ public class FragmentEntryLinkLocalServiceImpl
 		}
 
 		return html;
+	}
+
+	private void _updateFragmentEntryLinkLayout(
+			FragmentEntryLink fragmentEntryLink)
+		throws PortalException {
+
+		Layout layout = _layoutLocalService.fetchLayout(
+			fragmentEntryLink.getClassPK());
+
+		if (layout == null) {
+			return;
+		}
+
+		layout.setModifiedDate(new Date());
+
+		_layoutLocalService.updateLayout(layout);
 	}
 
 	private static final String[] _FRAGMENT_ENTRY_PROCESSOR_KEYS = {
