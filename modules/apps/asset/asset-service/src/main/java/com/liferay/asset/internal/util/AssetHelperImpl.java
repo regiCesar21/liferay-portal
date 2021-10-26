b303e66fbb1fe99f6fe7f09deb1802b20cd73b63
balazssk <balazs.safrany-kovalik@liferay.com>
2021-10-26 15:02:29 +0200

    LPS-141570 Read same name attribute from assetEntryQuery

--- modules/apps/asset/asset-service/src/main/java/com/liferay/asset/internal/util/AssetHelperImpl.java
+++ modules/apps/asset/asset-service/src/main/java/com/liferay/asset/internal/util/AssetHelperImpl.java
@@ -628,6 +628,12 @@ public class AssetHelperImpl implements AssetHelper {
 			searchContext.setLike(true);
 		}
 
+		if (GetterUtil.getBoolean(
+				assetEntryQuery.getAttribute("showNonindexable"))) {
+
+			searchContext.setAttribute("showNonindexable", Boolean.TRUE);
+		}
+
 		_searchRequestBuilderFactory.builder(
 			searchContext
 		).sorts(
