b918c26e446fbdc1d70f9746dabcf0e797a4b944
balazssk <balazs.safrany-kovalik@liferay.com>
2021-11-11 08:49:39 +0100

    LPS-136518 Extract to separate test case
    (cherry picked from commit acc08948a64f84d3fee71d14f60fdb1177a31344)

--- modules/apps/portal/portal-util-test/src/testIntegration/java/com/liferay/portal/util/test/PortalImplGroupFriendlyURLTest.java
+++ modules/apps/portal/portal-util-test/src/testIntegration/java/com/liferay/portal/util/test/PortalImplGroupFriendlyURLTest.java
@@ -94,6 +94,13 @@ public class PortalImplGroupFriendlyURLTest {
 
 		_testGroupFriendlyURL(
 			_PUBLIC_LAYOUT_HOSTNAME, expectedURL, _group, _publicLayout);
+	}
+
+	@Test
+	public void testGetGroupFriendlyURLFromPublicLayoutLocalhost()
+		throws Exception {
+
+		String expectedURL = StringPool.BLANK;
 
 		_testGroupFriendlyURL("localhost", expectedURL, _group, _publicLayout);
 	}
