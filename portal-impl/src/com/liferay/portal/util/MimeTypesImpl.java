0562456b15ec0113ebfc12f91dc60bb0a876fe5e
shuyangzhou <shuyang.zhou@liferay.com>
2022-02-07 10:53:55 -0800

    LPS-147179 Remove invaild paths

--- portal-impl/src/com/liferay/portal/util/MimeTypesImpl.java
+++ portal-impl/src/com/liferay/portal/util/MimeTypesImpl.java
@@ -124,24 +124,6 @@ public class MimeTypesImpl implements MimeTypes, MimeTypesReaderMetKeys {
 		}
 		catch (Exception exception) {
 			_log.error(exception, exception);
-
-			return ContentTypes.APPLICATION_OCTET_STREAM;
-		}
-
-		if (contentType.contains("tika")) {
-			if (_log.isDebugEnabled()) {
-				_log.debug("Retrieved invalid content type " + contentType);
-			}
-
-			contentType = getContentType(fileName);
-		}
-
-		if (contentType.contains("tika")) {
-			if (_log.isDebugEnabled()) {
-				_log.debug("Retrieved invalid content type " + contentType);
-			}
-
-			contentType = ContentTypes.APPLICATION_OCTET_STREAM;
 		}
 
 		return contentType;
@@ -168,18 +150,9 @@ public class MimeTypesImpl implements MimeTypes, MimeTypesReaderMetKeys {
 		}
 		catch (Exception exception) {
 			_log.error(exception, exception);
-
-			return ContentTypes.APPLICATION_OCTET_STREAM;
 		}
 
-		if (!contentType.contains("tika")) {
-			return contentType;
-		}
-		else if (_log.isDebugEnabled()) {
-			_log.debug("Retrieved invalid content type " + contentType);
-		}
-
-		return ContentTypes.APPLICATION_OCTET_STREAM;
+		return contentType;
 	}
 
 	@Override
