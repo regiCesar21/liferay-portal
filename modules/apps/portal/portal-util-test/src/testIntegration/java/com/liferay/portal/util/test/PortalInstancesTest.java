1460c4b9b67ddc3a794581c4ba11034b33580732
shuyangzhou <shuyang.zhou@liferay.com>
2022-02-15 21:56:44 -0800

    LPS-147069 Set to null to force refetch, rather than "correcting" to out of date copy.

--- modules/apps/portal/portal-util-test/src/testIntegration/java/com/liferay/portal/util/test/PortalInstancesTest.java
+++ modules/apps/portal/portal-util-test/src/testIntegration/java/com/liferay/portal/util/test/PortalInstancesTest.java
@@ -30,9 +30,6 @@ import com.liferay.portal.test.rule.LiferayIntegrationTestRule;
 import com.liferay.portal.util.PortalInstances;
 import com.liferay.portal.util.PropsValues;
 
-import java.util.Map;
-import java.util.TreeMap;
-
 import org.junit.After;
 import org.junit.Assert;
 import org.junit.Before;
@@ -151,17 +148,13 @@ public class PortalInstancesTest {
 
 		LayoutSet layoutSet = layout.getLayoutSet();
 
-		TreeMap<String, String> virtualHostnames = TreeMapBuilder.put(
-			layoutHostname, StringPool.BLANK
-		).build();
-
 		_virtualHostLocalService.updateVirtualHosts(
 			_company.getCompanyId(), layoutSet.getLayoutSetId(),
-			virtualHostnames);
+			TreeMapBuilder.put(
+				layoutHostname, StringPool.BLANK
+			).build());
 
-		layoutSet.setVirtualHostnames(virtualHostnames);
-
-		layout.setLayoutSet(layoutSet);
+		layout.setLayoutSet(null);
 	}
 
 	private Company _company;
