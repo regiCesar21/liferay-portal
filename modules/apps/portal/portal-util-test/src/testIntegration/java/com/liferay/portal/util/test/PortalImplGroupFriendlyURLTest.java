b3cb21059d2773aa48bc10c8b43a9298b07aefff
Lourdes Fernandez Besada <lourdes.fernandez@gmail.com>
2022-02-23 07:22:00 +0100

    LPS-136518 Ensure that relevant properties has the expected value.

--- modules/apps/portal/portal-util-test/src/testIntegration/java/com/liferay/portal/util/test/PortalImplGroupFriendlyURLTest.java
+++ modules/apps/portal/portal-util-test/src/testIntegration/java/com/liferay/portal/util/test/PortalImplGroupFriendlyURLTest.java
@@ -12,6 +12,7 @@ import com.liferay.petra.string.StringPool;
 import com.liferay.portal.kernel.exception.PortalException;
 import com.liferay.portal.kernel.model.Company;
 import com.liferay.portal.kernel.model.Group;
+import com.liferay.portal.kernel.model.GroupConstants;
 import com.liferay.portal.kernel.model.Layout;
 import com.liferay.portal.kernel.model.LayoutSet;
 import com.liferay.portal.kernel.model.User;
@@ -19,6 +20,7 @@ import com.liferay.portal.kernel.service.CompanyLocalService;
 import com.liferay.portal.kernel.service.GroupLocalService;
 import com.liferay.portal.kernel.service.LayoutLocalService;
 import com.liferay.portal.kernel.service.VirtualHostLocalService;
+import com.liferay.portal.kernel.test.ReflectionTestUtil;
 import com.liferay.portal.kernel.test.rule.AggregateTestRule;
 import com.liferay.portal.kernel.test.util.CompanyTestUtil;
 import com.liferay.portal.kernel.test.util.GroupTestUtil;
@@ -56,6 +58,11 @@ public class PortalImplGroupFriendlyURLTest {
 
 	@BeforeClass
 	public static void setUpClass() throws Exception {
+		_originalVirtualHostDefaultSiteName =
+			ReflectionTestUtil.getAndSetFieldValue(
+				PropsValues.class, "VIRTUAL_HOSTS_DEFAULT_SITE_NAME",
+				GroupConstants.GUEST);
+
 		_company = CompanyTestUtil.addCompany();
 
 		_group = _groupLocalService.fetchGroup(
@@ -78,6 +85,10 @@ public class PortalImplGroupFriendlyURLTest {
 
 	@AfterClass
 	public static void tearDownClass() throws PortalException {
+		ReflectionTestUtil.setFieldValue(
+			PropsValues.class, "VIRTUAL_HOSTS_DEFAULT_SITE_NAME",
+			_originalVirtualHostDefaultSiteName);
+
 		_companyLocalService.deleteCompany(_company);
 	}
 
@@ -269,6 +280,8 @@ public class PortalImplGroupFriendlyURLTest {
 
 	@Inject
 	private static GroupLocalService _groupLocalService;
+
+	private static String _originalVirtualHostDefaultSiteName;
 
 	@Inject
 	private static VirtualHostLocalService _virtualHostLocalService;
