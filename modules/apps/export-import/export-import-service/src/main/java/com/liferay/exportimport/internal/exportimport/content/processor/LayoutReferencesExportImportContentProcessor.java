28a11361e9b6b4b5438da072591dc407c6e8d5b9
Roberto Díaz <roberto.diaz@liferay.com>
2022-04-13 16:11:55 +0200

    LPS-151464 Also for private pages
    (cherry picked from commit e489a108cfe87a977ebccda91106c00d1d9481b4)

--- modules/apps/export-import/export-import-service/src/main/java/com/liferay/exportimport/internal/exportimport/content/processor/LayoutReferencesExportImportContentProcessor.java
+++ modules/apps/export-import/export-import-service/src/main/java/com/liferay/exportimport/internal/exportimport/content/processor/LayoutReferencesExportImportContentProcessor.java
@@ -1013,6 +1013,11 @@ public class LayoutReferencesExportImportContentProcessor
 							group.getGroupId(), false, urlWithoutLocale);
 
 					if (layout != null) {
+						layout = _layoutLocalService.fetchLayoutByFriendlyURL(
+							group.getGroupId(), true, urlWithoutLocale);
+					}
+
+					if (layout != null) {
 						urlSB.append(localePath);
 
 						url = urlWithoutLocale;
