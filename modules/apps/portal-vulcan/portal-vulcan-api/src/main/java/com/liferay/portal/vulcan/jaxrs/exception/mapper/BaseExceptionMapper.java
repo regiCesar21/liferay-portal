5eeeb77b6e4b9c8db4e3ae75aff9049ef9862c55
Mariano Alvaro Saiz <mariano.alvaro@liferay.com>
2022-05-18 13:47:06 +0200

    LPS-152965 Prepare to use type as error code and log problem information with error code.

--- modules/apps/portal-vulcan/portal-vulcan-api/src/main/java/com/liferay/portal/vulcan/jaxrs/exception/mapper/BaseExceptionMapper.java
+++ modules/apps/portal-vulcan/portal-vulcan-api/src/main/java/com/liferay/portal/vulcan/jaxrs/exception/mapper/BaseExceptionMapper.java
@@ -5,6 +5,9 @@
 
 package com.liferay.portal.vulcan.jaxrs.exception.mapper;
 
+import com.liferay.petra.string.StringBundler;
+import com.liferay.portal.kernel.log.Log;
+import com.liferay.portal.kernel.log.LogFactoryUtil;
 import com.liferay.portal.util.PropsValues;
 
 import java.util.List;
@@ -56,13 +59,23 @@ public abstract class BaseExceptionMapper<T extends Throwable>
 	private Problem _getSanitizedProblem(T exception) {
 		Problem problem = getProblem(exception);
 
+		if (_log.isDebugEnabled()) {
+			_log.debug(
+				StringBundler.concat(
+					"[ERROR-CODE: ", problem.getType(), "] ",
+					problem.getTitle()),
+				exception);
+		}
+
 		if (PropsValues.HEADLESS_BASE_EXCEPTION_MAPPER_SANITIZE_PROBLEM) {
 			problem.setDetail(null);
 			problem.setTitle(null);
-			problem.setType(null);
 		}
 
 		return problem;
 	}
+
+	private static final Log _log = LogFactoryUtil.getLog(
+		BaseExceptionMapper.class);
 
 }
