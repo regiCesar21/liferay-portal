418c47749e4c9a290d1454316b82c1af0d428b87
Mariano Alvaro Saiz <mariano.alvaro@liferay.com>
2022-05-18 13:54:04 +0200

    LPS-152965 No need to log, already logged in BaseExceptionMapper.

--- modules/apps/portal-vulcan/portal-vulcan-impl/src/main/java/com/liferay/portal/vulcan/internal/jaxrs/exception/mapper/JsonMappingExceptionMapper.java
+++ modules/apps/portal-vulcan/portal-vulcan-impl/src/main/java/com/liferay/portal/vulcan/internal/jaxrs/exception/mapper/JsonMappingExceptionMapper.java
@@ -7,8 +7,6 @@ package com.liferay.portal.vulcan.internal.jaxrs.exception.mapper;
 
 import com.fasterxml.jackson.databind.JsonMappingException;
 
-import com.liferay.portal.kernel.log.Log;
-import com.liferay.portal.kernel.log.LogFactoryUtil;
 import com.liferay.portal.vulcan.jaxrs.exception.mapper.BaseExceptionMapper;
 import com.liferay.portal.vulcan.jaxrs.exception.mapper.Problem;
 
@@ -29,10 +27,6 @@ public class JsonMappingExceptionMapper
 
 	@Override
 	protected Problem getProblem(JsonMappingException jsonMappingException) {
-		if (_log.isDebugEnabled()) {
-			_log.debug(jsonMappingException, jsonMappingException);
-		}
-
 		List<JsonMappingException.Reference> references =
 			jsonMappingException.getPath();
 
@@ -49,8 +43,5 @@ public class JsonMappingExceptionMapper
 			Response.Status.BAD_REQUEST, "Unable to map JSON path: " + path,
 			"JsonMappingException");
 	}
-
-	private static final Log _log = LogFactoryUtil.getLog(
-		JsonMappingExceptionMapper.class);
 
 }
