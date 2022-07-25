cd5e3030dbdad3113a9aaa5fd5e87d045effbc16
pei-jung <pei-jung.lan@liferay.com>
2022-07-25 12:30:40 -0700

    LPS-155692 Allow this behavior to be disabled via feature flag

--- portal-impl/src/com/liferay/portal/events/ServicePreAction.java
+++ portal-impl/src/com/liferay/portal/events/ServicePreAction.java
@@ -1000,18 +1000,24 @@ public class ServicePreAction extends Action {
 			Group layoutGroup = layout.getGroup();
 
 			if (layoutGroup.isUser()) {
-				long originalPlid = ParamUtil.getLong(
-					PortalUtil.getOriginalServletRequest(httpServletRequest),
-					"p_l_id");
+				if (!GetterUtil.getBoolean(
+						PropsUtil.get("feature.flag.LPS-155692"))) {
 
-				if (originalPlid == plid) {
-					String message = "User pages cannot be accessed via p_l_id";
+					long originalPlid = ParamUtil.getLong(
+						PortalUtil.getOriginalServletRequest(
+							httpServletRequest),
+						"p_l_id");
 
-					if (_log.isWarnEnabled()) {
-						_log.warn(message);
+					if (originalPlid == plid) {
+						String message =
+							"User pages cannot be accessed via p_l_id";
+
+						if (_log.isWarnEnabled()) {
+							_log.warn(message);
+						}
+
+						throw new NoSuchLayoutException(message);
 					}
-
-					throw new NoSuchLayoutException(message);
 				}
 
 				if ((layout.isPrivateLayout() &&
