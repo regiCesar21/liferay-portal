b45b383b0681c15af47c950b94a38ce7002bb9ef
Richard Jeremias <ricmartinsrocha@gmail.com>
2022-09-19 18:13:27 -0300

    LPS-163762 Adapting focus on forms according PTR-3342

--- modules/apps/dynamic-data-mapping/dynamic-data-mapping-form-renderer/src/main/resources/META-INF/resources/js/components/AutoFocus.es.js
+++ modules/apps/dynamic-data-mapping/dynamic-data-mapping-form-renderer/src/main/resources/META-INF/resources/js/components/AutoFocus.es.js
@@ -34,21 +34,69 @@ export const AutoFocus = ({children}) => {
 				setTimeout(() => setIncrement((value) => value + 1), 5);
 			}
 			else {
-				if (!document.activeElement) {
-					const firstInput = childRef.current.querySelector('input');
+				if (
+					!document.activeElement ||
+					(!Liferay.ThemeDisplay.isControlPanel() &&
+						document.activeElement.querySelector('input') &&
+						document.activeElement.querySelector('input').type ===
+							'hidden') ||
+					containerElement.current.parentNode.className.includes(
+						'ddm-form-builder-app'
+					)
+				) {
+					removeTabs();
+
+					const currentTitle = containerElement?.current.parentNode.getElementsByClassName(
+						'lfr-ddm__default-page-header-title'
+					)[0];
+
+					scrollComponentToTop(currentTitle);
+
+					const componentTitle = currentTitle?.innerHTML;
+
+					const currentDescription = containerElement?.current.parentNode.getElementsByClassName(
+						'.lfr-ddm__default-page-header-description'
+					)[0];
+
+					const componentDescription = currentDescription?.innerHTML;
+
+					const currentPage = document.activeElement.querySelector(
+						'.ddm-layout-builder:not(.hide)'
+					);
+
+					const firstInput = currentPage?.querySelector(
+						'input:not([type="hidden"])'
+					);
+
+					const sidebarOpen = document.querySelector(
+						'.ddm-form-builder--sidebar-open'
+					);
+
+					const userViewContent = containerElement.current.querySelector(
+						'.ddm-user-view-content'
+					);
+
+					const defaultTitle = Liferay.Language.get('untitled-form');
 
 					if (
+						(!componentTitle || componentTitle === defaultTitle) &&
+						!componentDescription &&
 						firstInput &&
 						!containerElement.current.contains(
 							document.activeElement
 						)
 					) {
-						firstInput.focus();
+						firstInput.focus({
+							behavior: 'smooth',
+							preventScroll: false,
+						});
 
 						if (firstInput.select) {
 							firstInput.select();
 						}
 					}
+
+					scrollComponentToTop(currentTitle);
 				}
 			}
 		}
@@ -59,4 +107,32 @@ export const AutoFocus = ({children}) => {
 			childRef.current = node;
 		},
 	});
-};
+}
+
+function scrollComponentToTop(currentTitle) {
+	const containerPosition = currentTitle.getBoundingClientRect();
+
+	const menuSize = document.querySelector('.control-menu-container')
+		?.clientHeight;
+
+	window.scroll(
+		containerPosition.x - menuSize,
+		containerPosition.y - menuSize
+	);
+}
+
+function removeTabs() {
+	const firstPageComponent = document.querySelector(
+		'div[class^="lfr-layout-structure-item'
+	);
+
+	const formPortlet = firstPageComponent?.querySelector('.portlet-forms');
+
+	if (!formPortlet) {
+		document
+			.querySelectorAll(
+				'.lfr-ddm__default-page-header-title,.lfr-ddm__default-page-header-description,.lfr-ddm-form-page-title,.lfr-ddm-form-page-description'
+			)
+			?.forEach((element) => element.removeAttribute('tabindex'));
+	}
+}
