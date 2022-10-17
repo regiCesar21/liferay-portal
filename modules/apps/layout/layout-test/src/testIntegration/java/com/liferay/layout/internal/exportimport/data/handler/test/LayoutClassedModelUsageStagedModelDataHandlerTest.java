b9472046d12ef69148b74f3127ac9c9323c1ca89
Michael Bowerman <michael.bowerman@liferay.com>
2022-10-17 09:29:16 -0700

    LPS-165401 Add test class for LayoutClassedModelUsageStagedModelDataHandler

---
+++ modules/apps/layout/layout-test/src/testIntegration/java/com/liferay/layout/internal/exportimport/data/handler/test/LayoutClassedModelUsageStagedModelDataHandlerTest.java
@@ -0,0 +1,132 @@
+/**
+ * SPDX-FileCopyrightText: (c) 2023 Liferay, Inc. https://liferay.com
+ * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
+ */
+
+package com.liferay.layout.internal.exportimport.data.handler.test;
+
+import com.liferay.arquillian.extension.junit.bridge.junit.Arquillian;
+import com.liferay.exportimport.test.util.lar.BaseStagedModelDataHandlerTestCase;
+import com.liferay.layout.service.LayoutClassedModelUsageLocalService;
+import com.liferay.layout.test.util.LayoutTestUtil;
+import com.liferay.portal.kernel.exception.PortalException;
+import com.liferay.portal.kernel.model.Group;
+import com.liferay.portal.kernel.model.Layout;
+import com.liferay.portal.kernel.model.Role;
+import com.liferay.portal.kernel.model.StagedModel;
+import com.liferay.portal.kernel.service.ClassNameLocalService;
+import com.liferay.portal.kernel.test.rule.AggregateTestRule;
+import com.liferay.portal.kernel.test.util.RandomTestUtil;
+import com.liferay.portal.kernel.test.util.ServiceContextTestUtil;
+import com.liferay.portal.kernel.test.util.TestPropsValues;
+import com.liferay.portal.test.rule.Inject;
+import com.liferay.portal.test.rule.LiferayIntegrationTestRule;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.ClassRule;
+import org.junit.Rule;
+import org.junit.runner.RunWith;
+
+/**
+ * @author Michael Bowerman
+ */
+@RunWith(Arquillian.class)
+public class LayoutClassedModelUsageStagedModelDataHandlerTest
+	extends BaseStagedModelDataHandlerTestCase {
+
+	@ClassRule
+	@Rule
+	public static final AggregateTestRule aggregateTestRule =
+		new LiferayIntegrationTestRule();
+
+	@Before
+	@Override
+	public void setUp() throws Exception {
+		super.setUp();
+
+		_classNameLocalService.addClassName(_CLASS_NAME);
+
+		_classPK = RandomTestUtil.nextLong();
+		_containerKey = String.valueOf(RandomTestUtil.nextLong());
+	}
+
+	@After
+	@Override
+	public void tearDown() throws Exception {
+		super.setUp();
+
+		_classNameLocalService.deleteClassName(
+			_classNameLocalService.getClassName(_CLASS_NAME));
+	}
+
+	@Override
+	protected Map<String, List<StagedModel>> addDependentStagedModelsMap(
+			Group group)
+		throws Exception {
+
+		Map<String, List<StagedModel>> dependentStagedModelsMap =
+			new HashMap<>();
+
+		Layout layout = LayoutTestUtil.addLayout(group);
+
+		addDependentStagedModel(dependentStagedModelsMap, Layout.class, layout);
+
+		return dependentStagedModelsMap;
+	}
+
+	@Override
+	protected StagedModel addStagedModel(
+			Group group,
+			Map<String, List<StagedModel>> dependentStagedModelsMap)
+		throws Exception {
+
+		List<StagedModel> layoutDependentStagedModels =
+			dependentStagedModelsMap.get(Layout.class.getSimpleName());
+
+		Layout layout = (Layout)layoutDependentStagedModels.get(0);
+
+		return _layoutClassedModelUsageLocalService.addLayoutClassedModelUsage(
+			group.getGroupId(),
+			_classNameLocalService.getClassNameId(_CLASS_NAME), _classPK,
+			_containerKey,
+			_classNameLocalService.getClassNameId(_CONTAINER_TYPE_CLASS_NAME),
+			layout.getPlid(),
+			ServiceContextTestUtil.getServiceContext(
+				stagingGroup.getGroupId(), TestPropsValues.getUserId()));
+	}
+
+	@Override
+	protected StagedModel getStagedModel(String uuid, Group group)
+		throws PortalException {
+
+		return _layoutClassedModelUsageLocalService.
+			getLayoutClassedModelUsageByUuidAndGroupId(
+				uuid, group.getGroupId());
+	}
+
+	@Override
+	protected Class<? extends StagedModel> getStagedModelClass() {
+		return Role.class;
+	}
+
+	private static final String _CLASS_NAME = RandomTestUtil.randomString();
+
+	private static final String _CONTAINER_TYPE_CLASS_NAME =
+		RandomTestUtil.randomString();
+
+	@Inject
+	private ClassNameLocalService _classNameLocalService;
+
+	private long _classPK;
+	private String _containerKey;
+
+	@Inject
+	private LayoutClassedModelUsageLocalService
+		_layoutClassedModelUsageLocalService;
+
+}
