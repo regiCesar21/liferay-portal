3fdbe6fa197c8888d7629b843117b10bbc32e2fa
Brian Chan <brian.chan@liferay.com>
2022-11-17 11:56:26 -0300

    LPS-168582 SF

--- modules/apps/fragment/fragment-test/src/testIntegration/java/com/liferay/fragment/contributor/test/FragmentCollectionContributorTest.java
+++ modules/apps/fragment/fragment-test/src/testIntegration/java/com/liferay/fragment/contributor/test/FragmentCollectionContributorTest.java
@@ -135,7 +135,6 @@ public class FragmentCollectionContributorTest {
 			Assert.assertEquals(
 				editableFragmentEntryProcessorJSONObject.toString(), 2,
 				editableFragmentEntryProcessorJSONObject.length());
-
 			Assert.assertTrue(
 				editableFragmentEntryProcessorJSONObject.has("editable-1"));
 
@@ -145,7 +144,6 @@ public class FragmentCollectionContributorTest {
 
 			Assert.assertEquals(
 				"EDITABLE 1", editable1JSONObject.get("defaultValue"));
-
 			Assert.assertTrue(
 				editableFragmentEntryProcessorJSONObject.has("editable-2"));
 
