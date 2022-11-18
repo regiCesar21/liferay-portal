d99db904209f3fe675cb572df25df536118d4605
Lourdes Fernández Besada <lourdes.fernandez@liferay.com>
2022-11-18 11:00:21 +0100

    LPS-168788 Hide new behavior under a feature flag

--- modules/apps/fragment/fragment-entry-processor/fragment-entry-processor-drop-zone/src/main/java/com/liferay/fragment/entry/processor/drop/zone/listener/DropZoneContentPageEditorListener.java
+++ modules/apps/fragment/fragment-entry-processor/fragment-entry-processor-drop-zone/src/main/java/com/liferay/fragment/entry/processor/drop/zone/listener/DropZoneContentPageEditorListener.java
@@ -31,7 +31,9 @@ import com.liferay.portal.kernel.log.Log;
 import com.liferay.portal.kernel.log.LogFactoryUtil;
 import com.liferay.portal.kernel.service.ServiceContext;
 import com.liferay.portal.kernel.service.ServiceContextThreadLocal;
+import com.liferay.portal.kernel.util.GetterUtil;
 import com.liferay.portal.kernel.util.ListUtil;
+import com.liferay.portal.kernel.util.PropsUtil;
 import com.liferay.portal.kernel.util.Validator;
 
 import java.util.ArrayList;
@@ -264,7 +266,9 @@ public class DropZoneContentPageEditorListener
 			elementIds.add(element.id());
 		}
 
-		if (elementIds.size() < elements.size()) {
+		if (!GetterUtil.getBoolean(PropsUtil.get("feature.flag.LPS-167932")) ||
+			(elementIds.size() < elements.size())) {
+
 			List<String> childrenItemIds =
 				parentLayoutStructureItem.getChildrenItemIds();
 
