4a8024ccb5517c21591584a7c8262e28d8970bd9
Lourdes Fernández Besada <lourdes.fernandez@liferay.com>
2022-11-29 15:27:54 +0100

    LPS-169086 Adds logic for fragmentDropZoneId in the importer and the mapper
    # Conflicts:
    #	modules/apps/layout/layout-page-template-admin-web/src/main/java/com/liferay/layout/page/template/admin/web/internal/headless/delivery/dto/v1_0/structure/importer/FragmentDropZoneLayoutStructureItemImporter.java

--- modules/apps/layout/layout-page-template-admin-web/src/main/java/com/liferay/layout/page/template/admin/web/internal/headless/delivery/dto/v1_0/structure/exporter/FragmentDropZoneLayoutStructureItemExporter.java
+++ modules/apps/layout/layout-page-template-admin-web/src/main/java/com/liferay/layout/page/template/admin/web/internal/headless/delivery/dto/v1_0/structure/exporter/FragmentDropZoneLayoutStructureItemExporter.java
@@ -15,6 +15,7 @@
 package com.liferay.layout.page.template.admin.web.internal.headless.delivery.dto.v1_0.structure.exporter;
 
 import com.liferay.headless.delivery.dto.v1_0.PageElement;
+import com.liferay.headless.delivery.dto.v1_0.PageFragmentDropZoneDefinition;
 import com.liferay.layout.util.structure.FragmentDropZoneLayoutStructureItem;
 import com.liferay.layout.util.structure.LayoutStructureItem;
 
@@ -37,8 +38,20 @@ public class FragmentDropZoneLayoutStructureItemExporter
 		long groupId, LayoutStructureItem layoutStructureItem,
 		boolean saveInlineContent, boolean saveMappingConfiguration) {
 
+		FragmentDropZoneLayoutStructureItem
+			fragmentDropZoneLayoutStructureItem =
+				(FragmentDropZoneLayoutStructureItem)layoutStructureItem;
+
 		return new PageElement() {
 			{
+				definition = new PageFragmentDropZoneDefinition() {
+					{
+						fragmentDropZoneId =
+							fragmentDropZoneLayoutStructureItem.
+								getFragmentDropZoneId();
+					}
+				};
+
 				type = Type.FRAGMENT_DROP_ZONE;
 			}
 		};
--- modules/apps/layout/layout-page-template-admin-web/src/main/java/com/liferay/layout/page/template/admin/web/internal/headless/delivery/dto/v1_0/structure/importer/FragmentDropZoneLayoutStructureItemImporter.java
+++ modules/apps/layout/layout-page-template-admin-web/src/main/java/com/liferay/layout/page/template/admin/web/internal/headless/delivery/dto/v1_0/structure/importer/FragmentDropZoneLayoutStructureItemImporter.java
@@ -15,10 +15,13 @@
 package com.liferay.layout.page.template.admin.web.internal.headless.delivery.dto.v1_0.structure.importer;
 
 import com.liferay.headless.delivery.dto.v1_0.PageElement;
+import com.liferay.layout.util.structure.FragmentDropZoneLayoutStructureItem;
 import com.liferay.layout.util.structure.LayoutStructure;
 import com.liferay.layout.util.structure.LayoutStructureItem;
 import com.liferay.portal.kernel.model.Layout;
+import com.liferay.portal.kernel.util.GetterUtil;
 
+import java.util.Map;
 import java.util.Set;
 
 import org.osgi.service.component.annotations.Component;
@@ -28,6 +31,7 @@ import org.osgi.service.component.annotations.Component;
  */
 @Component(service = LayoutStructureItemImporter.class)
 public class FragmentDropZoneLayoutStructureItemImporter
+	extends BaseLayoutStructureItemImporter
 	implements LayoutStructureItemImporter {
 
 	@Override
@@ -37,8 +41,25 @@ public class FragmentDropZoneLayoutStructureItemImporter
 			Set<String> warningMessages)
 		throws Exception {
 
-		return layoutStructure.addFragmentDropZoneLayoutStructureItem(
-			parentItemId, position);
+		FragmentDropZoneLayoutStructureItem
+			fragmentDropZoneLayoutStructureItem =
+				(FragmentDropZoneLayoutStructureItem)
+					layoutStructure.addFragmentDropZoneLayoutStructureItem(
+						parentItemId, position);
+
+		Map<String, Object> definitionMap = getDefinitionMap(
+			pageElement.getDefinition());
+
+		if ((definitionMap == null) ||
+			!definitionMap.containsKey("fragmentDropZoneId")) {
+
+			return fragmentDropZoneLayoutStructureItem;
+		}
+
+		fragmentDropZoneLayoutStructureItem.setFragmentDropZoneId(
+			GetterUtil.getString(definitionMap.get("fragmentDropZoneId")));
+
+		return fragmentDropZoneLayoutStructureItem;
 	}
 
 	@Override
