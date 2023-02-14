97d370dc61b6291a4a760475e83ba548691ed6ce
Brian Chan <brian.chan@liferay.com>
2023-02-14 14:40:54 +0000

    LPS-172553 Rename

--- portal-impl/src/com/liferay/portal/service/impl/GroupLocalServiceImpl.java
+++ portal-impl/src/com/liferay/portal/service/impl/GroupLocalServiceImpl.java
@@ -360,14 +360,14 @@ public class GroupLocalServiceImpl extends GroupLocalServiceBaseImpl {
 			else {
 				int counter = 1;
 
-				groupKey = _createLongStagingGroupKey(
+				groupKey = _getGroupKey(
 					counter, groupKey, groupKeyMaxLength,
 					stagingGroupKeySuffix);
 
 				while (fetchGroup(user.getCompanyId(), groupKey) != null) {
 					counter++;
 
-					groupKey = _createLongStagingGroupKey(
+					groupKey = _getGroupKey(
 						counter, groupKey, groupKeyMaxLength,
 						stagingGroupKeySuffix);
 				}
@@ -5238,7 +5238,7 @@ public class GroupLocalServiceImpl extends GroupLocalServiceBaseImpl {
 
 	protected File publicLARFile;
 
-	private String _createLongStagingGroupKey(
+	private String _getGroupKey(
 		int counter, String groupKey, int groupKeyMaxLength,
 		String stagingGroupKeySuffix) {
 
