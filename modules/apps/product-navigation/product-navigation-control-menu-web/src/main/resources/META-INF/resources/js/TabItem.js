5e527d960f7b95a18077e85e3cce46c1ee649c07
Bárbara Cabrera <barbara.cabrera@liferay.com>
2023-02-16 15:38:27 +0100

    LPS-175927 Add a tooltip with the full content name in the content tab for Widget Pages and change the tooltip for the plus button to just show "Add Content"

--- modules/apps/product-navigation/product-navigation-control-menu-web/src/main/resources/META-INF/resources/js/TabItem.js
+++ modules/apps/product-navigation/product-navigation-control-menu-web/src/main/resources/META-INF/resources/js/TabItem.js
@@ -61,7 +61,10 @@ const TabItem = ({item}) => {
 			})}
 			ref={item.disabled ? null : sourceRef}
 		>
-			<div className="sidebar-body__add-panel__tab-item-body">
+			<div
+				className="sidebar-body__add-panel__tab-item-body"
+				title={item.label}
+			>
 				<div className="icon">
 					<ClayIcon symbol={item.icon} />
 				</div>
@@ -77,11 +80,13 @@ const TabItem = ({item}) => {
 
 			{!item.disabled && (
 				<ClayButton
+					aria-label={`${Liferay.Language.get('add-content')}`}
 					className="btn-monospaced sidebar-body__add-panel__tab-item-add"
+					data-tooltip-align="top-left"
 					displayType="unstyled"
 					onClick={() => addItem({item, plid, setWidgets, widgets})}
 					small
-					title={item.name}
+					title={`${Liferay.Language.get('add-content')}`}
 				>
 					<ClayIcon symbol="plus" />
 					<span className="sr-only">{item.name}</span>
