7f32dac248de10c8da097ee06e73f6ddcb01fe6b
Tai Pham <tai.pham@gs.liferay.com>
2023-02-22 14:00:04 +0700

    LPS-176371 Default language specified in a structure is not used when creating a new web content

--- modules/apps/journal/journal-web/src/main/java/com/liferay/journal/web/internal/display/context/JournalEditArticleDisplayContext.java
+++ modules/apps/journal/journal-web/src/main/java/com/liferay/journal/web/internal/display/context/JournalEditArticleDisplayContext.java
@@ -555,8 +555,13 @@ public class JournalEditArticleDisplayContext {
 		}
 
 		if (Validator.isNull(getArticleId())) {
-			_defaultArticleLanguageId = LocaleUtil.toLanguageId(
-				siteDefaultLocale);
+			_defaultArticleLanguageId =
+				_getDefaultArticleLanguageIdRelatedDDMStructure();
+
+			if (_defaultArticleLanguageId == null) {
+				_defaultArticleLanguageId = LocaleUtil.toLanguageId(
+					siteDefaultLocale);
+			}
 		}
 		else {
 			_defaultArticleLanguageId = LocalizationUtil.getDefaultLanguageId(
@@ -817,6 +822,27 @@ public class JournalEditArticleDisplayContext {
 		}
 
 		return false;
+	}
+
+	private String _getDefaultArticleLanguageIdRelatedDDMStructure() {
+		if (_ddmStructure != null) {
+			try {
+				JournalArticle ddmStructureArticle =
+					JournalArticleServiceUtil.getArticle(
+						_ddmStructure.getGroupId(),
+						DDMStructure.class.getName(),
+						_ddmStructure.getStructureId());
+
+				return ddmStructureArticle.getDefaultLanguageId();
+			}
+			catch (PortalException portalException) {
+				if (_log.isDebugEnabled()) {
+					_log.debug(portalException);
+				}
+			}
+		}
+
+		return null;
 	}
 
 	private long _getInheritedWorkflowDDMStructuresFolderId()
