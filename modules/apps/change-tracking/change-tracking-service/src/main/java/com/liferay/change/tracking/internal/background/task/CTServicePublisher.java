a183e4bb7371003a0a1e1a0a08fee089f6736f40
Brian Chan <brian.chan@liferay.com>
2023-02-28 22:46:25 +0530

    LPS-176611 constant it

--- modules/apps/change-tracking/change-tracking-service/src/main/java/com/liferay/change/tracking/internal/background/task/CTServicePublisher.java
+++ modules/apps/change-tracking/change-tracking-service/src/main/java/com/liferay/change/tracking/internal/background/task/CTServicePublisher.java
@@ -303,10 +303,9 @@ public class CTServicePublisher<T extends CTModel<T>> {
 			sb.append(" in (");
 
 			int i = 0;
-			int batchSize = 1000;
 
 			for (CTEntry ctEntry : ctEntries) {
-				if (i == batchSize) {
+				if (i == _BATCH_SIZE) {
 					sb.setStringAt(")", sb.index() - 1);
 					sb.append(" or ");
 					sb.append(tableName);
@@ -400,6 +399,8 @@ public class CTServicePublisher<T extends CTModel<T>> {
 			}
 		}
 	}
+
+	private static final int _BATCH_SIZE = 1000;
 
 	private Map<Serializable, CTEntry> _additionCTEntries;
 	private final CTEntryLocalService _ctEntryLocalService;
