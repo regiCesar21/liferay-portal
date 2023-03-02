02ce7c3c0d2b0d75fc5d8a281a868cb06931aa31
Lourdes Fernández Besada <lourdes.fernandez@liferay.com>
2023-03-02 17:54:34 +0100

    LPS-176371 SF rename

--- modules/apps/journal/journal-web/src/main/java/com/liferay/journal/web/internal/display/context/JournalEditArticleDisplayContext.java
+++ modules/apps/journal/journal-web/src/main/java/com/liferay/journal/web/internal/display/context/JournalEditArticleDisplayContext.java
@@ -550,8 +550,7 @@ public class JournalEditArticleDisplayContext {
 			if (getClassNameId() ==
 					JournalArticleConstants.CLASS_NAME_ID_DEFAULT) {
 
-				defaultArticleLanguageId =
-					_getDefaultArticleLanguageIdRelatedDDMStructure();
+				defaultArticleLanguageId = _getDDMStructureDefaultLanguageId();
 			}
 
 			if (defaultArticleLanguageId == null) {
@@ -836,17 +835,17 @@ public class JournalEditArticleDisplayContext {
 		return false;
 	}
 
-	private String _getDefaultArticleLanguageIdRelatedDDMStructure() {
+	private String _getDDMStructureDefaultLanguageId() {
 		DDMStructure ddmStructure = getDDMStructure();
 
 		if (ddmStructure != null) {
 			try {
-				JournalArticle ddmStructureArticle =
+				JournalArticle ddmStructureJournalArticle =
 					JournalArticleServiceUtil.getArticle(
 						ddmStructure.getGroupId(), DDMStructure.class.getName(),
 						ddmStructure.getStructureId());
 
-				return ddmStructureArticle.getDefaultLanguageId();
+				return ddmStructureJournalArticle.getDefaultLanguageId();
 			}
 			catch (PortalException portalException) {
 				if (_log.isDebugEnabled()) {
