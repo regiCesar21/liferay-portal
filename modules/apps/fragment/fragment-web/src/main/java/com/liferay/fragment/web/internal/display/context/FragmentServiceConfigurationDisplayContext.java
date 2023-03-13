3b09a2d40dda639d89d64fa4d343c2de748d12f1
Eudaldo Alonso <eudaldo.alonso@liferay.com>
2023-03-13 18:34:26 +0100

    LPS-177987 Implements custom view for fragment service configuration

---
+++ modules/apps/fragment/fragment-web/src/main/java/com/liferay/fragment/web/internal/display/context/FragmentServiceConfigurationDisplayContext.java
@@ -0,0 +1,89 @@
+/**
+ * SPDX-FileCopyrightText: (c) 2023 Liferay, Inc. https://liferay.com
+ * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
+ */
+
+package com.liferay.fragment.web.internal.display.context;
+
+import com.liferay.fragment.web.internal.configuration.admin.service.FragmentServiceManagedServiceFactory;
+import com.liferay.portal.configuration.metatype.annotations.ExtendedObjectClassDefinition;
+import com.liferay.portal.kernel.portlet.LiferayPortletResponse;
+import com.liferay.portal.kernel.theme.ThemeDisplay;
+import com.liferay.portal.kernel.util.PortalUtil;
+import com.liferay.portal.kernel.util.WebKeys;
+
+import java.util.Objects;
+
+import javax.portlet.ActionRequest;
+import javax.portlet.PortletURL;
+
+import javax.servlet.http.HttpServletRequest;
+
+/**
+ * @author Eudaldo Alonso
+ */
+public class FragmentServiceConfigurationDisplayContext {
+
+	public FragmentServiceConfigurationDisplayContext(
+		HttpServletRequest httpServletRequest,
+		LiferayPortletResponse liferayPortletResponse,
+		FragmentServiceManagedServiceFactory
+			fragmentServiceManagedServiceFactory,
+		String scope) {
+
+		_httpServletRequest = httpServletRequest;
+		_liferayPortletResponse = liferayPortletResponse;
+		_fragmentServiceManagedServiceFactory =
+			fragmentServiceManagedServiceFactory;
+		_scope = scope;
+	}
+
+	public String getEditFragmentServiceConfigurationURL() {
+		PortletURL editFragmentServiceConfigurationURL =
+			_liferayPortletResponse.createActionURL();
+
+		editFragmentServiceConfigurationURL.setParameter(
+			ActionRequest.ACTION_NAME,
+			"/instance_settings/edit_fragment_service_configuration");
+		editFragmentServiceConfigurationURL.setParameter(
+			"redirect", PortalUtil.getCurrentURL(_httpServletRequest));
+		editFragmentServiceConfigurationURL.setParameter("scope", _scope);
+		editFragmentServiceConfigurationURL.setParameter(
+			"scopePK", String.valueOf(_getScopePk()));
+
+		return editFragmentServiceConfigurationURL.toString();
+	}
+
+	public boolean isPropagateChangesEnabled() {
+		return _fragmentServiceManagedServiceFactory.isPropagateChanges(
+			_scope, _getScopePk());
+	}
+
+	private long _getScopePk() {
+		if (Objects.equals(
+				_scope,
+				ExtendedObjectClassDefinition.Scope.COMPANY.getValue())) {
+
+			ThemeDisplay themeDisplay =
+				(ThemeDisplay)_httpServletRequest.getAttribute(
+					WebKeys.THEME_DISPLAY);
+
+			return themeDisplay.getCompanyId();
+		}
+		else if (Objects.equals(
+					_scope,
+					ExtendedObjectClassDefinition.Scope.SYSTEM.getValue())) {
+
+			return 0L;
+		}
+
+		throw new IllegalArgumentException("Unsupported scope: " + _scope);
+	}
+
+	private final FragmentServiceManagedServiceFactory
+		_fragmentServiceManagedServiceFactory;
+	private final HttpServletRequest _httpServletRequest;
+	private final LiferayPortletResponse _liferayPortletResponse;
+	private final String _scope;
+
+}
---
+++ modules/apps/fragment/fragment-web/src/main/resources/META-INF/resources/fragment_service_configuration.jsp
@@ -0,0 +1,45 @@
+<%--
+/**
+ * SPDX-FileCopyrightText: (c) 2023 Liferay, Inc. https://liferay.com
+ * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
+ */
+--%>
+
+<%@ include file="/init.jsp" %>
+
+<%
+FragmentServiceConfigurationDisplayContext fragmentServiceConfigurationDisplayContext = (FragmentServiceConfigurationDisplayContext)request.getAttribute(FragmentServiceConfigurationDisplayContext.class.getName());
+%>
+
+<aui:form action="<%= fragmentServiceConfigurationDisplayContext.getEditFragmentServiceConfigurationURL() %>" method="post" name="fm">
+	<div class="sheet">
+		<liferay-ui:error exception="<%= ConfigurationModelListenerException.class %>" message="there-was-an-unknown-error" />
+
+		<div class="sheet-header">
+			<h2>
+				<liferay-ui:message key="fragment-configuration-name" />
+			</h2>
+		</div>
+
+		<div class="sheet-section">
+			<clay:checkbox
+				checked="<%= fragmentServiceConfigurationDisplayContext.isPropagateChangesEnabled() %>"
+				id='<%= liferayPortletResponse.getNamespace() + "propagateChanges" %>'
+				label='<%= LanguageUtil.get(request, "propagate-fragment-changes-automatically") %>'
+				name='<%= liferayPortletResponse.getNamespace() + "propagateChanges" %>'
+			/>
+
+			<div aria-hidden="true" class="form-feedback-group">
+				<div class="form-text text-weight-normal">
+					<liferay-ui:message key="propagate-fragment-changes-automatically-description" />
+				</div>
+			</div>
+		</div>
+
+		<div class="sheet-footer">
+			<aui:button primary="<%= true %>" type="submit" value="save" />
+
+			<aui:button type="cancel" />
+		</div>
+	</div>
+</aui:form>
