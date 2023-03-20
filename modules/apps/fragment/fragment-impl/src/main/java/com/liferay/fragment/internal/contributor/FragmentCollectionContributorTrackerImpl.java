5b25503c1d60e88760758cfa8fcfc17e47d7a5bd
Brian Chan <brian.chan@liferay.com>
2023-03-20 14:33:16 -0300

    LPS-178527 Rename

--- modules/apps/fragment/fragment-impl/src/main/java/com/liferay/fragment/internal/contributor/FragmentCollectionContributorTrackerImpl.java
+++ modules/apps/fragment/fragment-impl/src/main/java/com/liferay/fragment/internal/contributor/FragmentCollectionContributorTrackerImpl.java
@@ -260,11 +260,11 @@ public class FragmentCollectionContributorTrackerImpl
 				FragmentServiceConfiguration.class.getName(), ".scoped",
 				")(companyId=", companyId, "))");
 
-			Configuration[] configuration =
+			Configuration[] configurations =
 				_configurationAdmin.listConfigurations(filterString);
 
-			if (configuration != null) {
-				return configuration[0];
+			if (configurations != null) {
+				return configurations[0];
 			}
 
 			return null;
