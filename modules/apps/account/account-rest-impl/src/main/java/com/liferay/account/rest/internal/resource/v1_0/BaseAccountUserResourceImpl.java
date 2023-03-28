6574ed492ff35a71e0600faf1035bd79c0695f49
Brian Chan <brian.chan@liferay.com>
2023-03-28 10:00:57 -0700

    LPS-177624 regen

--- modules/apps/account/account-rest-impl/src/main/java/com/liferay/account/rest/internal/resource/v1_0/BaseAccountUserResourceImpl.java
+++ modules/apps/account/account-rest-impl/src/main/java/com/liferay/account/rest/internal/resource/v1_0/BaseAccountUserResourceImpl.java
@@ -566,6 +566,12 @@ public abstract class BaseAccountUserResourceImpl
 		return TransformUtil.transformToList(array, unsafeFunction);
 	}
 
+	protected <T, R, E extends Throwable> long[] transformToLongArray(
+		Collection<T> collection, UnsafeFunction<T, R, E> unsafeFunction) {
+
+		return TransformUtil.transformToLongArray(collection, unsafeFunction);
+	}
+
 	protected <T, R, E extends Throwable> List<R> unsafeTransform(
 			Collection<T> collection, UnsafeFunction<T, R, E> unsafeFunction)
 		throws E {
--- modules/apps/data-engine/data-engine-rest-impl/src/main/java/com/liferay/data/engine/rest/internal/resource/v2_0/BaseDataRecordResourceImpl.java
+++ modules/apps/data-engine/data-engine-rest-impl/src/main/java/com/liferay/data/engine/rest/internal/resource/v2_0/BaseDataRecordResourceImpl.java
@@ -1023,6 +1023,12 @@ public abstract class BaseDataRecordResourceImpl
 		return TransformUtil.transformToList(array, unsafeFunction);
 	}
 
+	protected <T, R, E extends Throwable> long[] transformToLongArray(
+		Collection<T> collection, UnsafeFunction<T, R, E> unsafeFunction) {
+
+		return TransformUtil.transformToLongArray(collection, unsafeFunction);
+	}
+
 	protected <T, R, E extends Throwable> List<R> unsafeTransform(
 			Collection<T> collection, UnsafeFunction<T, R, E> unsafeFunction)
 		throws E {
