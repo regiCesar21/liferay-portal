7ddeba51984c5ccd1d79617f34ce7a200b02ad85
Roberto Díaz <roberto.diaz@liferay.com>
2023-03-31 09:13:32 +0200

    LPS-180100 Short-circuit using the simplest clause

--- modules/apps/frontend-editor/frontend-editor-ckeditor-web/src/main/resources/META-INF/resources/_diffs/plugins/bbcode/bbcode_parser.js
+++ modules/apps/frontend-editor/frontend-editor-ckeditor-web/src/main/resources/META-INF/resources/_diffs/plugins/bbcode/bbcode_parser.js
@@ -626,8 +626,8 @@
 			var imageSrcInput = instance._extractData(STR_IMG, true);
 
 			if (
-				REGEX_IMAGE_SRC.test(imageSrcInput) ||
-				imageSrcInput.startsWith('data:image/')
+				imageSrcInput.startsWith('data:image/') ||
+				REGEX_IMAGE_SRC.test(imageSrcInput)
 			) {
 				imageSrc = CKTools.htmlEncodeAttr(imageSrcInput);
 			}
