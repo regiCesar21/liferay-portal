5fa948abbe2d70e9045f5ed8aeefddebbfad80cc
Jiefeng Wu <jeffwu0724@gmail.com>
2023-05-15 12:42:24 -0700

    LPS-144246 Wordsmith

--- portal-kernel/src/com/liferay/portal/kernel/util/SessionClicks.java
+++ portal-kernel/src/com/liferay/portal/kernel/util/SessionClicks.java
@@ -154,8 +154,8 @@ public class SessionClicks {
 			_log.warn(
 				StringBundler.concat(
 					"Session clicks has attempted to exceed the maximum size ",
-					"allowed for keys or values with {key=", key, ", value=",
-					value, "}"));
+					"allowed for keys or values with key \"", key,
+					"\" and value \"", value, "\""));
 		}
 
 		return false;
@@ -170,8 +170,8 @@ public class SessionClicks {
 			_log.warn(
 				StringBundler.concat(
 					"Session clicks has attempted to exceed the maximum ",
-					"number of allowed values with {key=", key, ", value=",
-					value, "}"));
+					"number of allowed values with key \"", key,
+					"\" and value \"", value, "\""));
 		}
 
 		return false;
