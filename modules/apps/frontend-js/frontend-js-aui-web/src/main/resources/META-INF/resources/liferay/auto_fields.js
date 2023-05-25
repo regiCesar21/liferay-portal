9726629b93a8d6aa4eadab0993732ef51c89f2b3
Antonio Ortega <60252917@liferay.com>
2023-05-25 13:23:25 +0200

    LPS-185690 Avoid using :visible since it is not a valid CSS Selector

--- modules/apps/frontend-js/frontend-js-aui-web/src/main/resources/META-INF/resources/liferay/auto_fields.js
+++ modules/apps/frontend-js/frontend-js-aui-web/src/main/resources/META-INF/resources/liferay/auto_fields.js
@@ -449,13 +449,21 @@ AUI.add(
 
 					var contentBox = instance._contentBox;
 
-					var visibleRows = contentBox.all('.lfr-form-row:visible');
+					const allRowsNodes = contentBox.all('.lfr-form-row')._nodes;
 
-					var visibleRowsSize = visibleRows.size();
+					const visibleRows = [];
 
-					var deleteRow = visibleRowsSize > 1;
+					for (let i = 0; i < allRowsNodes.length; i++) {
+						if (allRowsNodes[i].checkVisibility()) {
+							visibleRows.push(allRowsNodes[i]);
+						}
+					}
 
-					if (visibleRowsSize === 1) {
+					const visibleRowsLength = visibleRows.length;
+
+					let deleteRow = visibleRowsLength > 1;
+
+					if (visibleRowsLength === 1) {
 						instance.addRow(node);
 
 						deleteRow = true;
