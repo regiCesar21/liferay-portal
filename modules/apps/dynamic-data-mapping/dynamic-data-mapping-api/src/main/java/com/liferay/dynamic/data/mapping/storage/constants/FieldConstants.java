c191b9a10d2a82d1eafb12e2bc012e295a1bb8e2
Carolina Barbosa <carolina.barbosa@liferay.com>
2023-06-02 14:20:57 -0300

    LPS-152619 Remove Regex usage to avoid ReDoS vulnerabilities

--- modules/apps/dynamic-data-mapping/dynamic-data-mapping-api/src/main/java/com/liferay/dynamic/data/mapping/storage/constants/FieldConstants.java
+++ modules/apps/dynamic-data-mapping/dynamic-data-mapping-api/src/main/java/com/liferay/dynamic/data/mapping/storage/constants/FieldConstants.java
@@ -192,11 +192,12 @@ public class FieldConstants {
 			return value;
 		}
 		else if (type.equals(DOUBLE)) {
-			if (value.matches(_SCIENTIFIC_NOTATION_PATTERN)) {
+			try {
 				return new BigDecimal(value.trim());
 			}
-
-			return GetterUtil.getDouble(value);
+			catch (NumberFormatException numberFormatException) {
+				return GetterUtil.getDouble(value);
+			}
 		}
 		else if (type.equals(FLOAT)) {
 			return GetterUtil.getFloat(value);
@@ -244,9 +245,6 @@ public class FieldConstants {
 
 		return decimalFormat;
 	}
-
-	private static final String _SCIENTIFIC_NOTATION_PATTERN =
-		"^[+-]?\\d+(?:\\.\\d*(?:[eE][+-]?\\d+)+)+$";
 
 	private static final Log _log = LogFactoryUtil.getLog(FieldConstants.class);
 
