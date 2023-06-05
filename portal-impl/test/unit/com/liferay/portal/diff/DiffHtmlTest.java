2aef5df2b7ce3444c6d06a4c8f89978b719e6908
Jiefeng Wu <jeffwu0724@gmail.com>
2023-06-05 15:44:45 -0700

    LPS-142794 use Mockito before test to set up _secureXMLFactoryProvider in SecureXMLFactoryProviderUtil and avoid NullPointerException

--- portal-impl/test/unit/com/liferay/portal/diff/DiffHtmlTest.java
+++ portal-impl/test/unit/com/liferay/portal/diff/DiffHtmlTest.java
@@ -6,15 +6,23 @@
 package com.liferay.portal.diff;
 
 import com.liferay.portal.kernel.diff.DiffHtml;
+import com.liferay.portal.kernel.security.xml.SecureXMLFactoryProvider;
+import com.liferay.portal.kernel.security.xml.SecureXMLFactoryProviderUtil;
+import com.liferay.portal.kernel.test.ReflectionTestUtil;
 import com.liferay.portal.kernel.util.StringUtil;
 import com.liferay.portal.test.rule.LiferayUnitTestRule;
 
 import java.io.StringReader;
 
+import javax.xml.transform.TransformerFactory;
+
 import org.junit.Assert;
+import org.junit.BeforeClass;
 import org.junit.ClassRule;
 import org.junit.Rule;
 import org.junit.Test;
+
+import org.mockito.Mockito;
 
 /**
  * @author Adolfo Pérez
@@ -25,6 +33,19 @@ public class DiffHtmlTest {
 	@Rule
 	public static final LiferayUnitTestRule liferayUnitTestRule =
 		LiferayUnitTestRule.INSTANCE;
+
+	@BeforeClass
+	public static void setUpClass() throws Exception {
+		Mockito.when(
+			_secureXMLFactoryProvider.newTransformerFactory()
+		).thenReturn(
+			_transformerFactory
+		);
+
+		ReflectionTestUtil.setFieldValue(
+			SecureXMLFactoryProviderUtil.class, "_secureXMLFactoryProvider",
+			_secureXMLFactoryProvider);
+	}
 
 	@Test
 	public void testDiffMustNotHaveXMLDeclaration() throws Exception {
@@ -73,6 +94,11 @@ public class DiffHtmlTest {
 	public void testDiffWhereTargetIsNull() throws Exception {
 		_diffHtml.diff(new StringReader(StringUtil.randomString()), null);
 	}
+
+	private static final SecureXMLFactoryProvider _secureXMLFactoryProvider =
+		Mockito.mock(SecureXMLFactoryProvider.class);
+	private static final TransformerFactory _transformerFactory =
+		TransformerFactory.newInstance();
 
 	private final DiffHtml _diffHtml = new DiffHtmlImpl();
 
