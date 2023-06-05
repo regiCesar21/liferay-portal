97486fd94a148785561e69168feffc2d6ebb1f01
Jiefeng Wu <jeffwu0724@gmail.com>
2023-06-05 15:36:15 -0700

    LPS-142794 add TransformerFactory newTransformerFactory() to SecureXMLFactoryProvider, make it a default method to avoid major version bump

--- portal-kernel/src/com/liferay/portal/kernel/security/xml/SecureXMLFactoryProvider.java
+++ portal-kernel/src/com/liferay/portal/kernel/security/xml/SecureXMLFactoryProvider.java
@@ -7,6 +7,7 @@ package com.liferay.portal.kernel.security.xml;
 
 import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.stream.XMLInputFactory;
+import javax.xml.transform.TransformerFactory;
 
 import org.xml.sax.XMLReader;
 
@@ -16,6 +17,10 @@ import org.xml.sax.XMLReader;
 public interface SecureXMLFactoryProvider {
 
 	public DocumentBuilderFactory newDocumentBuilderFactory();
+
+	public default TransformerFactory newTransformerFactory() {
+		return null;
+	}
 
 	public XMLInputFactory newXMLInputFactory();
 
