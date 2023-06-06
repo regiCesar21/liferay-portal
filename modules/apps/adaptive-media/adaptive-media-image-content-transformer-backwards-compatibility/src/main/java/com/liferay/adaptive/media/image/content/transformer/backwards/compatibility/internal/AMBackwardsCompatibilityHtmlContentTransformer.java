d86cdbf7a3cd54301749a4ef9c760998dde22088
Lianne Louie <lianne.louie@liferay.com>
2023-06-06 15:10:21 -0700

    LPS-186835 - don't limit solely to the body of the document since we could lose template content this way

--- modules/apps/adaptive-media/adaptive-media-image-content-transformer-backwards-compatibility/src/main/java/com/liferay/adaptive/media/image/content/transformer/backwards/compatibility/internal/AMBackwardsCompatibilityHtmlContentTransformer.java
+++ modules/apps/adaptive-media/adaptive-media-image-content-transformer-backwards-compatibility/src/main/java/com/liferay/adaptive/media/image/content/transformer/backwards/compatibility/internal/AMBackwardsCompatibilityHtmlContentTransformer.java
@@ -73,9 +73,7 @@ public class AMBackwardsCompatibilityHtmlContentTransformer
 			imgElement.replaceWith(_parseNode(replacement));
 		}
 
-		Element body = document.body();
-
-		return body.html();
+		return document.html();
 	}
 
 	@Override
