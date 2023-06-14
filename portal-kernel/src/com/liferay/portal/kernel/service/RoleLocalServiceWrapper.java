7c72c4c66e63f1af35172d03d715a2f0dae71152
tototrinh <to.trinh@codeenginestudio.com>
2023-06-14 15:03:14 +0700

    LPS-178212 BuildService

--- portal-kernel/src/com/liferay/portal/kernel/service/RoleLocalServiceWrapper.java
+++ portal-kernel/src/com/liferay/portal/kernel/service/RoleLocalServiceWrapper.java
@@ -544,6 +544,17 @@ public class RoleLocalServiceWrapper
 	}
 
 	@Override
+	public java.util.List<Role> getGroupRolesAndTeamRoles(
+		long companyId, String name, java.util.List<String> excludedNames,
+		String title, String description, int[] types, long excludedTeamRoleId,
+		long teamGroupId, int start, int end) {
+
+		return _roleLocalService.getGroupRolesAndTeamRoles(
+			companyId, name, excludedNames, title, description, types,
+			excludedTeamRoleId, teamGroupId, start, end);
+	}
+
+	@Override
 	public int getGroupRolesAndTeamRolesCount(
 		long companyId, String keywords, java.util.List<String> excludedNames,
 		int[] types, long excludedTeamRoleId, long teamGroupId) {
@@ -551,6 +562,17 @@ public class RoleLocalServiceWrapper
 		return _roleLocalService.getGroupRolesAndTeamRolesCount(
 			companyId, keywords, excludedNames, types, excludedTeamRoleId,
 			teamGroupId);
+	}
+
+	@Override
+	public int getGroupRolesAndTeamRolesCount(
+		long companyId, String name, java.util.List<String> excludedNames,
+		String title, String description, int[] types, long excludedTeamRoleId,
+		long teamGroupId) {
+
+		return _roleLocalService.getGroupRolesAndTeamRolesCount(
+			companyId, name, excludedNames, title, description, types,
+			excludedTeamRoleId, teamGroupId);
 	}
 
 	@Override
