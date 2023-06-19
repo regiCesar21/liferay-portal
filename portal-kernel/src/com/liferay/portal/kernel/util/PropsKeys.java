1804611be0b4c8845f13d761c524b5f4aa1e1294
Gislayne Vitorino <gislayne.vitorino@liferay.com>
2023-06-19 17:47:03 -0300

    LPS-188085 (portal-kernel) Add new PropKey

--- portal-kernel/src/com/liferay/portal/kernel/util/PropsKeys.java
+++ portal-kernel/src/com/liferay/portal/kernel/util/PropsKeys.java
@@ -1751,6 +1751,9 @@ public interface PropsKeys {
 	public static final String LAYOUT_URL_FRIENDLIABLE =
 		"layout.url.friendliable";
 
+	public static final String LAYOUT_USER_PLID_ACCESS_DENIED =
+		"layout.user.plid.access.denied";
+
 	public static final String LAYOUT_USER_PRIVATE_LAYOUTS_AUTO_CREATE =
 		"layout.user.private.layouts.auto.create";
 
