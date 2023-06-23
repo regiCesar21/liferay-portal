26ad12b9093ba933a6bb89335032a541653e566d
Brian Chan <brian.chan@liferay.com>
2023-06-23 10:52:58 +0530

    LPS-188085 Should be ! now

--- portal-impl/src/com/liferay/portal/events/ServicePreAction.java
+++ portal-impl/src/com/liferay/portal/events/ServicePreAction.java
@@ -987,7 +987,7 @@ public class ServicePreAction extends Action {
 			Group layoutGroup = layout.getGroup();
 
 			if (layoutGroup.isUser()) {
-				if (GetterUtil.getBoolean(
+				if (!GetterUtil.getBoolean(
 						PropsUtil.get(
 							PropsKeys.LAYOUT_USER_ACCESS_VIA_PLID_ENABLED))) {
 
