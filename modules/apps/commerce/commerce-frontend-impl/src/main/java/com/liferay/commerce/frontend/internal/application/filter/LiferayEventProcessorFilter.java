941385035864f1506e838eda9d58ca57a2727842
Brian I. Kim <il.kim@liferay.com>
2023-07-11 08:27:15 -0700

    COMMERCE-9781 Replace Globals.LOCALE_KEY with WebKeys.LOCALE

--- modules/apps/commerce/commerce-frontend-impl/src/main/java/com/liferay/commerce/frontend/internal/application/filter/LiferayEventProcessorFilter.java
+++ modules/apps/commerce/commerce-frontend-impl/src/main/java/com/liferay/commerce/frontend/internal/application/filter/LiferayEventProcessorFilter.java
@@ -9,6 +9,7 @@ import com.liferay.portal.events.EventsProcessorUtil;
 import com.liferay.portal.kernel.log.Log;
 import com.liferay.portal.kernel.log.LogFactoryUtil;
 import com.liferay.portal.kernel.util.PropsKeys;
+import com.liferay.portal.kernel.util.WebKeys;
 import com.liferay.portal.util.PropsValues;
 
 import javax.servlet.http.HttpServletRequest;
@@ -20,8 +21,6 @@ import javax.ws.rs.container.ContainerRequestFilter;
 import javax.ws.rs.container.PreMatching;
 import javax.ws.rs.core.Context;
 import javax.ws.rs.ext.Provider;
-
-import org.apache.struts.Globals;
 
 import org.osgi.service.component.annotations.Component;
 
@@ -46,7 +45,7 @@ public class LiferayEventProcessorFilter implements ContainerRequestFilter {
 		try {
 			HttpSession httpSession = _httpServletRequest.getSession(false);
 
-			httpSession.removeAttribute(Globals.LOCALE_KEY);
+			httpSession.removeAttribute(WebKeys.LOCALE);
 
 			EventsProcessorUtil.process(
 				PropsKeys.SERVLET_SERVICE_EVENTS_PRE,
