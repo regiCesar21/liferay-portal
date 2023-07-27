72d5c52dd7108e78838f5145d05ab66c4d2b4ffb
jalber786 <jalber786@gmail.com>
2023-07-27 16:20:06 +0200

    LPS-191341 Added new Web API configuration section for the GraphQL Max depth configuration

---
+++ modules/apps/portal-vulcan/portal-vulcan-impl/src/main/java/com/liferay/portal/vulcan/internal/configuration/HeadlessAPICompanyConfiguration.java
@@ -0,0 +1,31 @@
+/**
+ * SPDX-FileCopyrightText: (c) 2023 Liferay, Inc. https://liferay.com
+ * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
+ */
+
+package com.liferay.portal.vulcan.internal.configuration;
+
+import aQute.bnd.annotation.metatype.Meta;
+
+import com.liferay.portal.configuration.metatype.annotations.ExtendedObjectClassDefinition;
+
+/**
+ * @author Alberto Javier Moreno Lage
+ */
+@ExtendedObjectClassDefinition(
+	category = "web-api", scope = ExtendedObjectClassDefinition.Scope.COMPANY
+)
+@Meta.OCD(
+	id = "com.liferay.portal.vulcan.internal.configuration.HeadlessAPICompanyConfiguration",
+	localization = "content/Language",
+	name = "headless-api-configuration-name"
+)
+public interface HeadlessAPICompanyConfiguration {
+
+	@Meta.AD(
+		deflt = "20", description = "query-depth-limit-description",
+		name = "query-depth-limit", required = false
+	)
+	public int queryDepthLimit();
+
+}
