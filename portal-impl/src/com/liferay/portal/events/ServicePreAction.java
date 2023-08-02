5ceb3661d1dbe32bc598906649f33e5cf87b333a
Michael Bowerman <michael.bowerman@liferay.com>
2023-08-02 16:37:34 -0700

    LPS-192547 Always allow users to access their own personal pages. The LPS-155692 logic should only run if the user is trying to access _someone else's_ personal page. There is no security issue with allowing the user to access their own page by p_l_id.

--- portal-impl/src/com/liferay/portal/events/ServicePreAction.java
+++ portal-impl/src/com/liferay/portal/events/ServicePreAction.java
@@ -977,7 +977,9 @@ public class ServicePreAction extends Action {
 		if (layout != null) {
 			Group layoutGroup = layout.getGroup();
 
-			if (layoutGroup.isUser()) {
+			if (layoutGroup.isUser() &&
+				(layoutGroup.getClassPK() != user.getUserId())) {
+
 				if (!GetterUtil.getBoolean(
 						PropsUtil.get(
 							PropsKeys.LAYOUT_USER_ACCESS_VIA_PLID_ENABLED))) {
