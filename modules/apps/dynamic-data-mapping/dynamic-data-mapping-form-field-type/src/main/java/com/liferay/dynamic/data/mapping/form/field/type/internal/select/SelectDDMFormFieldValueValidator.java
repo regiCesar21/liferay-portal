2cc9c265d58a052e5189766e78d4ec0d579409dc
Carolina Barbosa <carolina.barbosa@liferay.com>
2023-08-07 11:35:38 -0300

    LPS-150392 Remove unnecessary validation for select field and adjust test to handle the new behavior

--- modules/apps/dynamic-data-mapping/dynamic-data-mapping-form-field-type/src/main/java/com/liferay/dynamic/data/mapping/form/field/type/internal/select/SelectDDMFormFieldValueValidator.java
+++ modules/apps/dynamic-data-mapping/dynamic-data-mapping-form-field-type/src/main/java/com/liferay/dynamic/data/mapping/form/field/type/internal/select/SelectDDMFormFieldValueValidator.java
@@ -10,19 +10,11 @@ import com.liferay.dynamic.data.mapping.form.field.type.DDMFormFieldValueValidat
 import com.liferay.dynamic.data.mapping.model.DDMFormField;
 import com.liferay.dynamic.data.mapping.model.DDMFormFieldOptions;
 import com.liferay.dynamic.data.mapping.model.Value;
-import com.liferay.portal.kernel.json.JSONArray;
-import com.liferay.portal.kernel.json.JSONFactory;
-import com.liferay.portal.kernel.log.Log;
-import com.liferay.portal.kernel.log.LogFactoryUtil;
-import com.liferay.portal.kernel.util.Validator;
 
-import java.util.Locale;
-import java.util.Map;
 import java.util.Objects;
 import java.util.Set;
 
 import org.osgi.service.component.annotations.Component;
-import org.osgi.service.component.annotations.Reference;
 
 /**
  * @author Marcellus Tavares
@@ -38,28 +30,9 @@ public class SelectDDMFormFieldValueValidator
 	public void validate(DDMFormField ddmFormField, Value value)
 		throws DDMFormFieldValueValidationException {
 
-		if (Objects.equals(ddmFormField.getDataSourceType(), "manual")) {
-			try {
-				validateDDMFormFieldOptions(ddmFormField, value);
-			}
-			catch (DDMFormFieldValueValidationException
-						ddmFormFieldValueValidationException) {
-
-				throw ddmFormFieldValueValidationException;
-			}
-			catch (Exception exception) {
-				if (_log.isDebugEnabled()) {
-					_log.debug(exception, exception);
-				}
-
-				throw new DDMFormFieldValueValidationException(exception);
-			}
+		if (!Objects.equals(ddmFormField.getDataSourceType(), "manual")) {
+			return;
 		}
-	}
-
-	protected void validateDDMFormFieldOptions(
-			DDMFormField ddmFormField, Value value)
-		throws Exception {
 
 		DDMFormFieldOptions ddmFormFieldOptions =
 			ddmFormField.getDDMFormFieldOptions();
@@ -77,41 +50,6 @@ public class SelectDDMFormFieldValueValidator
 			throw new DDMFormFieldValueValidationException(
 				"Options must contain at least one alternative");
 		}
-
-		Map<Locale, String> selectedValues = value.getValues();
-
-		for (String selectedValue : selectedValues.values()) {
-			validateSelectedValue(ddmFormField, optionsValues, selectedValue);
-		}
 	}
-
-	protected void validateSelectedValue(
-			DDMFormField ddmFormField, Set<String> optionValues,
-			String selectedValue)
-		throws Exception {
-
-		JSONArray jsonArray = jsonFactory.createJSONArray(selectedValue);
-
-		for (int i = 0; i < jsonArray.length(); i++) {
-			if (Validator.isNull(jsonArray.getString(i)) &&
-				!ddmFormField.isRequired()) {
-
-				continue;
-			}
-
-			if (!optionValues.contains(jsonArray.getString(i))) {
-				throw new DDMFormFieldValueValidationException(
-					String.format(
-						"The selected option \"%s\" is not a valid alternative",
-						jsonArray.getString(i)));
-			}
-		}
-	}
-
-	@Reference
-	protected JSONFactory jsonFactory;
-
-	private static final Log _log = LogFactoryUtil.getLog(
-		SelectDDMFormFieldValueValidator.class);
 
 }
