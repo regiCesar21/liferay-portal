99088a7fa232f1c4b96fe0984bb86efc14ef5add
amosfong <amos.fong@liferay.com>
2023-08-11 13:10:14 -0700

    ISSUP-6207 regenerate

---
+++ modules/dxp/apps/osb/osb-koroneiki/osb-koroneiki-root/osb-koroneiki-root-api/src/main/java/com/liferay/osb/koroneiki/root/exception/DuplicateOktaUserException.java
@@ -0,0 +1,29 @@
+/**
+ * SPDX-FileCopyrightText: (c) {$year} Liferay, Inc. https://liferay.com
+ * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
+ */
+package com.liferay.osb.koroneiki.root.exception;
+
+import com.liferay.portal.kernel.exception.PortalException;
+
+/**
+ * @author Brian Wing Shun Chan
+ */
+public class DuplicateOktaUserException extends PortalException {
+
+	public DuplicateOktaUserException() {
+	}
+
+	public DuplicateOktaUserException(String msg) {
+		super(msg);
+	}
+
+	public DuplicateOktaUserException(String msg, Throwable throwable) {
+		super(msg, throwable);
+	}
+
+	public DuplicateOktaUserException(Throwable throwable) {
+		super(throwable);
+	}
+
+}
