a16684a45f7b7281c0a8f67f52f9c8c3c15c5df4
Rachael Koestartyo <rachael.koestartyo@liferay.com>
2023-08-18 00:37:04 -0700

    LPS-193731 Revert "LRAC-14025 remove AnalyticsMessage table and services"
    This reverts commit 4006bec96f59934adc1f947b8fb8eced19c921a3.

--- modules/apps/analytics/analytics-message-storage-service/src/main/resources/META-INF/sql/tables.sql
+++ modules/apps/analytics/analytics-message-storage-service/src/main/resources/META-INF/sql/tables.sql
@@ -21,3 +21,13 @@ create table AnalyticsDeleteMessage (
 	className VARCHAR(75) null,
 	classPK LONG
 );
+
+create table AnalyticsMessage (
+	mvccVersion LONG default 0 not null,
+	analyticsMessageId LONG not null primary key,
+	companyId LONG,
+	userId LONG,
+	userName VARCHAR(75) null,
+	createDate DATE null,
+	body BLOB
+);
