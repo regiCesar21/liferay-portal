57be5af3ce336e9963647ae8c2241902141daebf
Alan Huang <alan.huang@liferay.com>
2023-08-30 16:02:46 +0800

    LPS-193868 Move package, package should follow Bundle-SymbolicName

--- modules/dxp/apps/osb/osb-provisioning/osb-provisioning-license/osb-provisioning-license-service/src/main/java/com/liferay/osb/provisioning/license/internal/util/comparator/LicenseKeyExpirationDateComparator.java
+++ modules/dxp/apps/osb/osb-provisioning/osb-provisioning-license/osb-provisioning-license-service/src/main/java/com/liferay/osb/provisioning/license/internal/util/comparator/LicenseKeyExpirationDateComparator.java
@@ -3,7 +3,7 @@
  * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
  */
 
-package com.liferay.osb.util.comparator;
+package com.liferay.osb.provisioning.license.internal.util.comparator;
 
 import com.liferay.osb.provisioning.license.model.LicenseKey;
 import com.liferay.portal.kernel.util.DateUtil;
