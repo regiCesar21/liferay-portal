50a94ea41816e06baf6e7db7797d57e92646e89c
willnewbury <william.newbury@liferay.com>
2023-09-06 14:59:42 -0700

    ISSUP-6273 Implement new validation API

--- modules/dxp/apps/osb/osb-provisioning/osb-provisioning-rest/osb-provisioning-rest-impl/src/main/java/com/liferay/osb/provisioning/rest/internal/resource/v1_0/ContactResourceImpl.java
+++ modules/dxp/apps/osb/osb-provisioning/osb-provisioning-rest/osb-provisioning-rest-impl/src/main/java/com/liferay/osb/provisioning/rest/internal/resource/v1_0/ContactResourceImpl.java
@@ -5,11 +5,14 @@
 
 package com.liferay.osb.provisioning.rest.internal.resource.v1_0;
 
+import com.liferay.osb.koroneiki.phloem.rest.client.dto.v1_0.Contact;
+import com.liferay.osb.provisioning.identity.management.provider.ContactIdentityProvider;
+import com.liferay.osb.provisioning.identity.management.validator.EmailAddressValidator;
 import com.liferay.osb.provisioning.rest.resource.v1_0.ContactResource;
 
 import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
 import org.osgi.service.component.annotations.ServiceScope;
-
 
 /**
  * @author Will Newbury
@@ -19,4 +22,29 @@ import org.osgi.service.component.annotations.ServiceScope;
 	scope = ServiceScope.PROTOTYPE, service = ContactResource.class
 )
 public class ContactResourceImpl extends BaseContactResourceImpl {
+
+	@Override
+	public Boolean getContactValidate(String contactEmailAddress)
+		throws Exception {
+
+		boolean valid = true;
+
+		Contact contact = _contactIdentityProvider.fetchContactByEmailAddress(
+			contactEmailAddress, true);
+
+		if ((contact == null) &&
+			_emailAddressValidator.isLiferayDomain(contactEmailAddress)) {
+
+			valid = false;
+		}
+
+		return Boolean.valueOf(valid);
+	}
+
+	@Reference(target = "(provider=okta)")
+	private ContactIdentityProvider _contactIdentityProvider;
+
+	@Reference
+	private EmailAddressValidator _emailAddressValidator;
+
 }
