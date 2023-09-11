8c55256030ef94fd1ca95d17bef19dcea8141b83
ChrisKian <christopher.kian@liferay.com>
2023-09-11 10:13:15 -0700

    LPS-190666 When selecting a role for filtering, allow all viewable roles to be present.

--- modules/apps/site/site-memberships-web/src/main/java/com/liferay/site/memberships/web/internal/display/context/SelectRolesDisplayContext.java
+++ modules/apps/site/site-memberships-web/src/main/java/com/liferay/site/memberships/web/internal/display/context/SelectRolesDisplayContext.java
@@ -8,22 +8,20 @@ package com.liferay.site.memberships.web.internal.display.context;
 import com.liferay.portal.kernel.dao.orm.QueryUtil;
 import com.liferay.portal.kernel.dao.search.SearchContainer;
 import com.liferay.portal.kernel.exception.PortalException;
-import com.liferay.portal.kernel.model.Group;
 import com.liferay.portal.kernel.model.Role;
 import com.liferay.portal.kernel.model.role.RoleConstants;
 import com.liferay.portal.kernel.portlet.SearchDisplayStyleUtil;
-import com.liferay.portal.kernel.service.GroupLocalServiceUtil;
+import com.liferay.portal.kernel.security.permission.ActionKeys;
 import com.liferay.portal.kernel.service.RoleLocalServiceUtil;
+import com.liferay.portal.kernel.service.permission.RolePermissionUtil;
 import com.liferay.portal.kernel.theme.ThemeDisplay;
-import com.liferay.portal.kernel.util.ListUtil;
 import com.liferay.portal.kernel.util.ParamUtil;
 import com.liferay.portal.kernel.util.Validator;
 import com.liferay.portal.kernel.util.WebKeys;
+import com.liferay.portal.vulcan.util.TransformUtil;
 import com.liferay.portlet.rolesadmin.search.RoleSearch;
 import com.liferay.portlet.rolesadmin.search.RoleSearchTerms;
 import com.liferay.site.memberships.constants.SiteMembershipsPortletKeys;
-import com.liferay.site.memberships.web.internal.util.DepotRolesUtil;
-import com.liferay.users.admin.kernel.util.UsersAdminUtil;
 
 import java.util.List;
 
@@ -171,29 +169,33 @@ public class SelectRolesDisplayContext {
 		RoleSearchTerms searchTerms =
 			(RoleSearchTerms)roleSearch.getSearchTerms();
 
-		List<Role> roles = RoleLocalServiceUtil.search(
-			themeDisplay.getCompanyId(), searchTerms.getKeywords(),
-			new Integer[] {getRoleType()}, QueryUtil.ALL_POS, QueryUtil.ALL_POS,
-			roleSearch.getOrderByComparator());
+		List<Role> roles = TransformUtil.transform(
+			RoleLocalServiceUtil.search(
+				themeDisplay.getCompanyId(), searchTerms.getKeywords(),
+				new Integer[] {getRoleType()}, QueryUtil.ALL_POS,
+				QueryUtil.ALL_POS, roleSearch.getOrderByComparator()),
+			role -> {
+				if (role.getName(
+					).equals(
+						RoleConstants.ORGANIZATION_USER
+					) ||
+					role.getName(
+					).equals(
+						RoleConstants.SITE_MEMBER
+					) ||
+					!RolePermissionUtil.contains(
+						themeDisplay.getPermissionChecker(),
+						themeDisplay.getScopeGroupId(), role.getRoleId(),
+						ActionKeys.VIEW)) {
 
-		Group group = GroupLocalServiceUtil.fetchGroup(getGroupId());
+					return null;
+				}
 
-		if (group.isDepot()) {
-			roles = DepotRolesUtil.filterGroupRoles(
-				themeDisplay.getPermissionChecker(), getGroupId(), roles);
-		}
-		else {
-			roles = UsersAdminUtil.filterGroupRoles(
-				themeDisplay.getPermissionChecker(), getGroupId(), roles);
-		}
+				return role;
+			});
 
-		int rolesCount = roles.size();
-
-		roleSearch.setTotal(rolesCount);
-
-		roleSearch.setResults(
-			ListUtil.subList(
-				roles, roleSearch.getStart(), roleSearch.getEnd()));
+		roleSearch.setResults(roles);
+		roleSearch.setTotal(roles.size());
 
 		_roleSearch = roleSearch;
 
