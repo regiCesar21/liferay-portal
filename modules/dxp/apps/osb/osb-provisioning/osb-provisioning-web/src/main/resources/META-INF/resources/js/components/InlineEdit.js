dd97c38331abfe75a2f7e89d1b908d490742c8bb
amosfong <amos.fong@liferay.com>
2023-09-12 11:04:39 -0700

    ISSUP-6181 SF

--- modules/dxp/apps/osb/osb-provisioning/osb-provisioning-web/src/main/resources/META-INF/resources/js/components/InlineEdit.js
+++ modules/dxp/apps/osb/osb-provisioning/osb-provisioning-web/src/main/resources/META-INF/resources/js/components/InlineEdit.js
@@ -3,11 +3,6 @@
  * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
  */
 
-/**
- * SPDX-FileCopyrightText: (c) 2000 Liferay, Inc. https://liferay.com
- * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
- */
-
 import {ClayToggle} from '@clayui/form';
 import PropTypes from 'prop-types';
 import React, {useEffect, useState} from 'react';
--- modules/dxp/apps/osb/osb-provisioning/osb-provisioning-web/src/main/resources/META-INF/resources/js/components/license_download/CombinedLicenses.js
+++ modules/dxp/apps/osb/osb-provisioning/osb-provisioning-web/src/main/resources/META-INF/resources/js/components/license_download/CombinedLicenses.js
@@ -3,11 +3,6 @@
  * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
  */
 
-/**
- * SPDX-FileCopyrightText: (c) 2000 Liferay, Inc. https://liferay.com
- * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
- */
-
 import flatten from 'lodash.flatten';
 import partition from 'lodash.partition';
 import PropTypes from 'prop-types';
