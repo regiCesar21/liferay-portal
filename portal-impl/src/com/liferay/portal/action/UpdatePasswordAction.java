ebf237e99ef0f4f16a1d03544e13e72139facfed
Stian Sigvartsen <stian.sigvartsen@liferay.com>
2023-09-16 19:36:28 +0100

    LPS-189689 SF Simplify, avoid introducing a new request attribute

--- portal-impl/src/com/liferay/portal/action/UpdatePasswordAction.java
+++ portal-impl/src/com/liferay/portal/action/UpdatePasswordAction.java
@@ -143,8 +143,6 @@ public class UpdatePasswordAction implements Action {
 			String[] ticketParts = ticketParam.split(StringPool.UNDERLINE);
 
 			if (ticketParts.length != 2) {
-				httpServletRequest.setAttribute("oldFormat", Boolean.TRUE);
-
 				return null;
 			}
 
