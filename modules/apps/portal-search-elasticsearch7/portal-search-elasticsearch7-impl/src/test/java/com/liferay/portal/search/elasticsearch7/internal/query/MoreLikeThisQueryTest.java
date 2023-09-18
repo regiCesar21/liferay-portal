9918de7ef0cdc42cf687e3cf2fcd446bf4091e78
gustavolimav <gustavo.lima@liferay.com>
2023-09-18 18:28:04 -0300

    LPS-189226 LPS-193255 portal-search-elasticsearch7-impl: Ignore test because it is causing other tests to fail
    based on master code the test is ignored
    
    https://liferay.atlassian.net/browse/LPS-193255

--- modules/apps/portal-search-elasticsearch7/portal-search-elasticsearch7-impl/src/test/java/com/liferay/portal/search/elasticsearch7/internal/query/MoreLikeThisQueryTest.java
+++ modules/apps/portal-search-elasticsearch7/portal-search-elasticsearch7-impl/src/test/java/com/liferay/portal/search/elasticsearch7/internal/query/MoreLikeThisQueryTest.java
@@ -6,17 +6,12 @@
 package com.liferay.portal.search.elasticsearch7.internal.query;
 
 import com.liferay.portal.search.elasticsearch7.internal.LiferayElasticsearchIndexingFixtureFactory;
-import com.liferay.portal.search.query.MoreLikeThisQuery;
 import com.liferay.portal.search.test.util.indexing.IndexingFixture;
 import com.liferay.portal.search.test.util.query.BaseMoreLikeThisQueryTestCase;
 import com.liferay.portal.test.rule.LiferayUnitTestRule;
 
-import java.util.Collections;
-
-import org.elasticsearch.action.search.SearchPhaseExecutionException;
-
-import org.junit.Assert;
 import org.junit.ClassRule;
+import org.junit.Ignore;
 import org.junit.Rule;
 import org.junit.Test;
 
@@ -30,29 +25,10 @@ public class MoreLikeThisQueryTest extends BaseMoreLikeThisQueryTestCase {
 	public static final LiferayUnitTestRule liferayUnitTestRule =
 		LiferayUnitTestRule.INSTANCE;
 
+	@Ignore
 	@Override
 	@Test
 	public void testMoreLikeThisWithoutFields() throws Exception {
-		addDocuments("java eclipse", "eclipse liferay", "java liferay eclipse");
-
-		MoreLikeThisQuery moreLikeThisQuery = queries.moreLikeThis(
-			Collections.emptyList(), "java");
-
-		try {
-			assertSearch(moreLikeThisQuery, Collections.emptyList());
-
-			Assert.fail();
-		}
-		catch (SearchPhaseExecutionException searchPhaseExecutionException) {
-			Throwable throwable = searchPhaseExecutionException.getRootCause();
-
-			String message = throwable.getMessage();
-
-			Assert.assertTrue(
-				message,
-				message.contains(
-					"[more_like_this] query cannot infer the field"));
-		}
 	}
 
 	@Override
