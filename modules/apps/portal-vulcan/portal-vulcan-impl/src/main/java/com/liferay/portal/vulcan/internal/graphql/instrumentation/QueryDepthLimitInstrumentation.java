31512405e477e78fa1994a393557647d0ccf47c0
Carlos Correa <carlos.correa@liferay.com>
2023-10-06 09:18:15 +0200

    LPS-191341 Move

--- modules/apps/portal-vulcan/portal-vulcan-impl/src/main/java/com/liferay/portal/vulcan/internal/graphql/instrumentation/QueryDepthLimitInstrumentation.java
+++ modules/apps/portal-vulcan/portal-vulcan-impl/src/main/java/com/liferay/portal/vulcan/internal/graphql/instrumentation/QueryDepthLimitInstrumentation.java
@@ -3,7 +3,7 @@
  * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
  */
 
-package com.liferay.portal.vulcan.internal.graphql.servlet.instrumentation;
+package com.liferay.portal.vulcan.internal.graphql.instrumentation;
 
 import com.liferay.portal.vulcan.internal.graphql.exception.QueryDepthLimitExceededException;
 
@@ -29,9 +29,7 @@ public class QueryDepthLimitInstrumentation
 	}
 
 	@Override
-	protected AbortExecutionException mkAbortException(
-		int depth, int maxDepth) {
-
+	public AbortExecutionException mkAbortException(int depth, int maxDepth) {
 		throw new QueryDepthLimitExceededException(depth, _queryDepthLimit);
 	}
 
