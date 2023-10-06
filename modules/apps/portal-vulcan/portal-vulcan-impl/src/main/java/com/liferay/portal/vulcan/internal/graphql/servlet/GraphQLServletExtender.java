6ca546b68120eb4ab8862292421b64072885ba72
Carlos Correa <carlos.correa@liferay.com>
2023-10-06 13:15:49 +0200

    LPS-191341 Simplify, passing the function is not needed as in GraphQLServletExtender for each query we are getting the current value of the GraphQL query depth limit

--- modules/apps/portal-vulcan/portal-vulcan-impl/src/main/java/com/liferay/portal/vulcan/internal/graphql/servlet/GraphQLServletExtender.java
+++ modules/apps/portal-vulcan/portal-vulcan-impl/src/main/java/com/liferay/portal/vulcan/internal/graphql/servlet/GraphQLServletExtender.java
@@ -846,7 +846,7 @@ public class GraphQLServletExtender {
 				_configurationProvider.getCompanyConfiguration(
 					HeadlessAPICompanyConfiguration.class, companyId);
 
-			return QueryDepthLimitInstrumentation.of(
+			return new QueryDepthLimitInstrumentation(
 				headlessAPICompanyConfiguration.queryDepthLimit());
 		}
 		catch (Exception exception) {
