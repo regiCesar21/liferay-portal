2dc79642c68aef2061b4a570729e4421bef0e9ca
pei-jung <pei-jung.lan@liferay.com>
2023-10-24 18:08:06 -0700

    LPS-198982 Update LayoutPageTemplateEntry edit url to include tabs1 param

--- modules/apps/layout/layout-page-template-admin-web/src/main/java/com/liferay/layout/page/template/admin/web/internal/change/tracking/display/LayoutPageTemplateEntryCTDisplayRenderer.java
+++ modules/apps/layout/layout-page-template-admin-web/src/main/java/com/liferay/layout/page/template/admin/web/internal/change/tracking/display/LayoutPageTemplateEntryCTDisplayRenderer.java
@@ -8,6 +8,7 @@ package com.liferay.layout.page.template.admin.web.internal.change.tracking.disp
 import com.liferay.change.tracking.spi.display.BaseCTDisplayRenderer;
 import com.liferay.change.tracking.spi.display.CTDisplayRenderer;
 import com.liferay.layout.page.template.admin.constants.LayoutPageTemplateAdminPortletKeys;
+import com.liferay.layout.page.template.constants.LayoutPageTemplateEntryTypeConstants;
 import com.liferay.layout.page.template.model.LayoutPageTemplateEntry;
 import com.liferay.portal.kernel.exception.PortalException;
 import com.liferay.portal.kernel.util.Portal;
@@ -43,6 +44,27 @@ public class LayoutPageTemplateEntryCTDisplayRenderer
 		portletURL.setParameter(
 			"mvcRenderCommandName",
 			"/layout_page_template_admin/edit_layout_page_template_entry");
+
+		if ((layoutPageTemplateEntry.getType() ==
+				LayoutPageTemplateEntryTypeConstants.TYPE_BASIC) ||
+			(layoutPageTemplateEntry.getType() ==
+				LayoutPageTemplateEntryTypeConstants.TYPE_WIDGET_PAGE)) {
+
+			portletURL.setParameter("tabs1", "page-templates");
+		}
+
+		if (layoutPageTemplateEntry.getType() ==
+				LayoutPageTemplateEntryTypeConstants.TYPE_DISPLAY_PAGE) {
+
+			portletURL.setParameter("tabs1", "display-page-templates");
+		}
+
+		if (layoutPageTemplateEntry.getType() ==
+				LayoutPageTemplateEntryTypeConstants.TYPE_MASTER_LAYOUT) {
+
+			portletURL.setParameter("tabs1", "master-layouts");
+		}
+
 		portletURL.setParameter(
 			"layoutPageTemplateEntryId",
 			String.valueOf(
