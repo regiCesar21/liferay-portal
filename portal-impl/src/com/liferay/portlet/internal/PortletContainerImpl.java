5a32b920c28a630ff8aeee067c9266d1937920ac
Jeff Wu <jeffwu0724@gmail.com>
2023-11-07 11:12:05 -0800

    LPS-196525 escape redirect while getting redirectLocation from an external site

--- portal-impl/src/com/liferay/portlet/internal/PortletContainerImpl.java
+++ portal-impl/src/com/liferay/portlet/internal/PortletContainerImpl.java
@@ -539,33 +539,36 @@ public class PortletContainerImpl implements PortletContainer {
 			String redirectLocation =
 				liferayActionResponse.getRedirectLocation();
 
-			if (Validator.isNull(redirectLocation) &&
-				portlet.isActionURLRedirect()) {
+			if (Validator.isNull(redirectLocation)) {
+				if (portlet.isActionURLRedirect()) {
+					PortletURL portletURL = null;
 
-				PortletURL portletURL = null;
+					if (portletApp.getSpecMajorVersion() < 3) {
+						portletURL = PortletURLFactoryUtil.create(
+							liferayActionRequest, portlet, layout,
+							PortletRequest.RENDER_PHASE);
 
-				if (portletApp.getSpecMajorVersion() < 3) {
-					portletURL = PortletURLFactoryUtil.create(
-						liferayActionRequest, portlet, layout,
-						PortletRequest.RENDER_PHASE);
+						Map<String, String[]> renderParameters =
+							liferayActionResponse.getRenderParameterMap();
 
-					Map<String, String[]> renderParameters =
-						liferayActionResponse.getRenderParameterMap();
+						for (Map.Entry<String, String[]> entry :
+								renderParameters.entrySet()) {
 
-					for (Map.Entry<String, String[]> entry :
-							renderParameters.entrySet()) {
-
-						portletURL.setParameter(
-							entry.getKey(), entry.getValue());
+							portletURL.setParameter(
+								entry.getKey(), entry.getValue());
+						}
 					}
-				}
-				else {
-					portletURL = PortletURLFactoryUtil.create(
-						liferayActionRequest, portlet, layout.getPlid(),
-						PortletRequest.RENDER_PHASE, MimeResponse.Copy.ALL);
-				}
+					else {
+						portletURL = PortletURLFactoryUtil.create(
+							liferayActionRequest, portlet, layout.getPlid(),
+							PortletRequest.RENDER_PHASE, MimeResponse.Copy.ALL);
+					}
 
-				redirectLocation = portletURL.toString();
+					redirectLocation = portletURL.toString();
+				}
+			}
+			else {
+				redirectLocation = PortalUtil.escapeRedirect(redirectLocation);
 			}
 
 			return new ActionResult(events, redirectLocation);
