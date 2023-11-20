c7cebe756c24fd93199117b9a6f4e4c7349dba85
Alejandro Tardín <alejandro.tardin@liferay.com>
2023-11-20 16:11:45 +0100

    LPS-191341 Auto SF

--- modules/apps/portal-vulcan/portal-vulcan-impl/src/main/java/com/liferay/portal/vulcan/internal/settings/definition/HeadlessAPICompanyConfigurationBeanDeclaration.java
+++ modules/apps/portal-vulcan/portal-vulcan-impl/src/main/java/com/liferay/portal/vulcan/internal/settings/definition/HeadlessAPICompanyConfigurationBeanDeclaration.java
@@ -1,5 +1,5 @@
 /**
- * SPDX-FileCopyrightText: (c) 2000 Liferay, Inc. https://liferay.com
+ * SPDX-FileCopyrightText: (c) 2023 Liferay, Inc. https://liferay.com
  * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
  */
 
@@ -7,6 +7,7 @@ package com.liferay.portal.vulcan.internal.settings.definition;
 
 import com.liferay.portal.kernel.settings.definition.ConfigurationBeanDeclaration;
 import com.liferay.portal.vulcan.internal.configuration.HeadlessAPICompanyConfiguration;
+
 import org.osgi.service.component.annotations.Component;
 
 /**
