924eff693353a9bd4a6c934739b9b35f886aa578
Alejandro Tardín <alejandro.tardin@liferay.com>
2023-11-20 11:12:20 +0100

    LPS-191341 This is needed for company configurations to work in 7.3.x

---
+++ modules/apps/portal-vulcan/portal-vulcan-impl/src/main/java/com/liferay/portal/vulcan/internal/settings/definition/HeadlessAPICompanyConfigurationBeanDeclaration.java
@@ -0,0 +1,24 @@
+/**
+ * SPDX-FileCopyrightText: (c) 2000 Liferay, Inc. https://liferay.com
+ * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
+ */
+
+package com.liferay.portal.vulcan.internal.settings.definition;
+
+import com.liferay.portal.kernel.settings.definition.ConfigurationBeanDeclaration;
+import com.liferay.portal.vulcan.internal.configuration.HeadlessAPICompanyConfiguration;
+import org.osgi.service.component.annotations.Component;
+
+/**
+ * @author Alejandro Tardín
+ */
+@Component(service = ConfigurationBeanDeclaration.class)
+public class HeadlessAPICompanyConfigurationBeanDeclaration
+	implements ConfigurationBeanDeclaration {
+
+	@Override
+	public Class<?> getConfigurationBeanClass() {
+		return HeadlessAPICompanyConfiguration.class;
+	}
+
+}
