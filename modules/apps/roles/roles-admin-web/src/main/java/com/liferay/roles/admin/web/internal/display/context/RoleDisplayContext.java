ea71d39ad145523d4225e87eeac8bee0704a88fd
Mirna Gama <mirnabeatrizgamav@gmail.com>
2023-11-27 14:04:43 -0300

    LPS-202614 Remove segments item if it is an admin role

--- modules/apps/roles/roles-admin-web/src/main/java/com/liferay/roles/admin/web/internal/display/context/RoleDisplayContext.java
+++ modules/apps/roles/roles-admin-web/src/main/java/com/liferay/roles/admin/web/internal/display/context/RoleDisplayContext.java
@@ -17,7 +17,9 @@ import com.liferay.portal.kernel.security.permission.PermissionChecker;
 import com.liferay.portal.kernel.service.RoleServiceUtil;
 import com.liferay.portal.kernel.service.permission.RolePermissionUtil;
 import com.liferay.portal.kernel.theme.ThemeDisplay;
+import com.liferay.portal.kernel.util.ArrayUtil;
 import com.liferay.portal.kernel.util.Constants;
+import com.liferay.portal.kernel.util.GetterUtil;
 import com.liferay.portal.kernel.util.HashMapBuilder;
 import com.liferay.portal.kernel.util.ParamUtil;
 import com.liferay.portal.kernel.util.WebKeys;
@@ -27,6 +29,7 @@ import com.liferay.roles.admin.web.internal.role.type.contributor.util.RoleTypeC
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
+import java.util.Objects;
 
 import javax.portlet.PortletURL;
 import javax.portlet.RenderResponse;
@@ -80,7 +83,8 @@ public class RoleDisplayContext {
 
 		return new NavigationItemList() {
 			{
-				for (String assigneeTypeName : _ASSIGNEE_TYPE_NAMES) {
+				for (String assigneeTypeName :
+						_getAssigneeTypeNamesByRole(portletURL)) {
 					add(
 						navigationItem -> {
 							navigationItem.setActive(
@@ -164,6 +168,29 @@ public class RoleDisplayContext {
 		return false;
 	}
 
+	private String[] _getAssigneeTypeNamesByRole(PortletURL portletURL)
+		throws Exception {
+
+		String[] assigneeTypeNamesByRole = _ASSIGNEE_TYPE_NAMES;
+
+		Map<String, String[]> parameterMap = portletURL.getParameterMap();
+
+		String[] roleIdParameter = parameterMap.get("roleId");
+
+		if (roleIdParameter.length > 0) {
+			long roleId = GetterUtil.getLong(roleIdParameter[0]);
+
+			Role role = RoleServiceUtil.fetchRole(roleId);
+
+			if (Objects.equals(RoleConstants.ADMINISTRATOR, role.getName())) {
+				assigneeTypeNamesByRole = ArrayUtil.filter(
+					assigneeTypeNamesByRole, name -> !name.equals("segments"));
+			}
+		}
+
+		return assigneeTypeNamesByRole;
+	}
+	
 	private List<String> _getTabsNames() throws Exception {
 		List<String> tabsNames = new ArrayList<>();
 
