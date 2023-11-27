5a19d48c709aea4c3b17a0e3719278a01cb8231e
Brian Chan <brian.chan@liferay.com>
2023-11-27 04:30:40 -0800

    LPS-86794 LPS-199049 regen

--- modules/apps/remote-app/remote-app-uad/src/main/java/com/liferay/remote/app/uad/exporter/BaseRemoteAppEntryUADExporter.java
+++ modules/apps/remote-app/remote-app-uad/src/main/java/com/liferay/remote/app/uad/exporter/BaseRemoteAppEntryUADExporter.java
@@ -53,14 +53,6 @@ public abstract class BaseRemoteAppEntryUADExporter
 		sb.append("</model-name>");
 
 		sb.append(
-			"<column><column-name>remoteAppEntryId</column-name><column-value><![CDATA[");
-		sb.append(remoteAppEntry.getRemoteAppEntryId());
-		sb.append("]]></column-value></column>");
-		sb.append(
-			"<column><column-name>userId</column-name><column-value><![CDATA[");
-		sb.append(remoteAppEntry.getUserId());
-		sb.append("]]></column-value></column>");
-		sb.append(
 			"<column><column-name>userName</column-name><column-value><![CDATA[");
 		sb.append(remoteAppEntry.getUserName());
 		sb.append("]]></column-value></column>");
