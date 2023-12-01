9db68c55d31f4a122947d4ebbfcfe1f44120664e
Carolina Barbosa <carolina.barbosa@liferay.com>
2023-12-01 14:49:04 -0300

    LPS-201461 Use XMLHttpRequest instead of axios because of known vulnerability

--- modules/apps/dynamic-data-mapping/dynamic-data-mapping-form-field-type/src/main/resources/META-INF/resources/DocumentLibrary/DocumentLibrary.es.js
+++ modules/apps/dynamic-data-mapping/dynamic-data-mapping-form-field-type/src/main/resources/META-INF/resources/DocumentLibrary/DocumentLibrary.es.js
@@ -8,7 +8,6 @@ import ClayCard from '@clayui/card';
 import {ClayInput} from '@clayui/form';
 import ClayIcon from '@clayui/icon';
 import ClayProgressBar from '@clayui/progress-bar';
-import axios from 'axios';
 import {PagesVisitor, usePage} from 'dynamic-data-mapping-form-renderer';
 import {convertToFormData} from 'dynamic-data-mapping-form-renderer/js/util/fetch.es';
 import {ItemSelectorDialog} from 'frontend-js-web';
@@ -410,47 +409,53 @@ const Main = ({
 			return;
 		}
 
-		const data = {
-			[`${portletNamespace}file`]: file,
-		};
+		const request = new XMLHttpRequest();
 
-		axios
-			.post(guestUploadURL, convertToFormData(data), {
-				onUploadProgress: (event) => {
-					const progress = Math.round(
-						(event.loaded * 100) / event.total
-					);
+		request.upload.addEventListener('progress', (event) => {
+			disableSubmitButton();
 
-					setCurrentValue(null);
+			setCurrentValue(null);
 
-					setProgress(progress);
-
-					disableSubmitButton();
-				},
-			})
-			.then((response) => {
-				const {error, file} = response.data;
-
+			setProgress(Math.round((event.loaded * 100) / event.total));
+		});
+		request.addEventListener('readystatechange', (event) => {
+			if (request.readyState === 4) {
 				disableSubmitButton(false);
 
-				if (error) {
-					handleGuestUploadFileChanged(error.message, event, null);
+				let response;
+
+				try {
+					response = JSON.parse(request.responseText);
 				}
-				else {
+				catch (error) {
+					response = request.responseText;
+				}
+
+				if (response.success) {
 					handleGuestUploadFileChanged(
 						'',
 						event,
-						JSON.stringify(file)
+						JSON.stringify(response.file)
+					);
+				}
+				else {
+					handleGuestUploadFileChanged(
+						response.error.message,
+						event,
+						null
 					);
 				}
 
 				setProgress(0);
-			})
-			.catch(() => {
-				disableSubmitButton(false);
+			}
+		});
 
-				setProgress(0);
-			});
+		request.open('POST', guestUploadURL);
+		request.send(
+			convertToFormData({
+				[`${portletNamespace}file`]: file,
+			})
+		);
 	};
 
 	const hasCustomError =
