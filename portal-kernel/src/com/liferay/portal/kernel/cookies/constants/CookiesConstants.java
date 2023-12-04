cead9df47884f776a731a2e802b124c6ae805273
Janis Zhang <janis.zhang@liferay.com>
2023-12-04 12:10:12 +0800

    LPS-148334 Refactor

--- portal-kernel/src/com/liferay/portal/kernel/cookies/constants/CookiesConstants.java
+++ portal-kernel/src/com/liferay/portal/kernel/cookies/constants/CookiesConstants.java
@@ -1,15 +1,6 @@
 /**
- * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
- *
- * This library is free software; you can redistribute it and/or modify it under
- * the terms of the GNU Lesser General Public License as published by the Free
- * Software Foundation; either version 2.1 of the License, or (at your option)
- * any later version.
- *
- * This library is distributed in the hope that it will be useful, but WITHOUT
- * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
- * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
- * details.
+ * SPDX-FileCopyrightText: (c) 2023 Liferay, Inc. https://liferay.com
+ * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
  */
 
 package com.liferay.portal.kernel.cookies.constants;
@@ -29,42 +20,42 @@ public class CookiesConstants {
 
 	public static final int CONSENT_TYPE_PERSONALIZATION = 4;
 
-	public static final String KEY_COMMERCE_CONTINUE_AS_GUEST =
+	public static final int MAX_AGE = (int)(Time.YEAR / 1000);
+
+	public static final String NAME_COMMERCE_CONTINUE_AS_GUEST =
 		"COMMERCE_CONTINUE_AS_GUEST";
 
-	public static final String KEY_COMPANY_ID = "COMPANY_ID";
+	public static final String NAME_COMPANY_ID = "COMPANY_ID";
 
-	public static final String KEY_CONSENT_TYPE_FUNCTIONAL =
+	public static final String NAME_CONSENT_TYPE_FUNCTIONAL =
 		"CONSENT_TYPE_FUNCTIONAL";
 
-	public static final String KEY_CONSENT_TYPE_NECESSARY =
+	public static final String NAME_CONSENT_TYPE_NECESSARY =
 		"CONSENT_TYPE_NECESSARY";
 
-	public static final String KEY_CONSENT_TYPE_PERFORMANCE =
+	public static final String NAME_CONSENT_TYPE_PERFORMANCE =
 		"CONSENT_TYPE_PERFORMANCE";
 
-	public static final String KEY_CONSENT_TYPE_PERSONALIZATION =
+	public static final String NAME_CONSENT_TYPE_PERSONALIZATION =
 		"CONSENT_TYPE_PERSONALIZATION";
 
-	public static final String KEY_COOKIE_SUPPORT = "COOKIE_SUPPORT";
+	public static final String NAME_COOKIE_SUPPORT = "COOKIE_SUPPORT";
 
-	public static final String KEY_GUEST_LANGUAGE_ID = "GUEST_LANGUAGE_ID";
+	public static final String NAME_GUEST_LANGUAGE_ID = "GUEST_LANGUAGE_ID";
 
-	public static final String KEY_ID = "ID";
+	public static final String NAME_ID = "ID";
 
-	public static final String KEY_JSESSIONID = "JSESSIONID";
+	public static final String NAME_JSESSIONID = "JSESSIONID";
 
-	public static final String KEY_LOGIN = "LOGIN";
+	public static final String NAME_LOGIN = "LOGIN";
 
-	public static final String KEY_PASSWORD = "PASSWORD";
+	public static final String NAME_PASSWORD = "PASSWORD";
 
-	public static final String KEY_REMEMBER_ME = "REMEMBER_ME";
+	public static final String NAME_REMEMBER_ME = "REMEMBER_ME";
 
-	public static final String KEY_REMOTE_PREFERENCE_PREFIX =
+	public static final String NAME_REMOTE_PREFERENCE_PREFIX =
 		"REMOTE_PREFERENCE_";
 
-	public static final String KEY_USER_UUID = "USER_UUID";
-
-	public static final int MAX_AGE = (int)(Time.YEAR / 1000);
+	public static final String NAME_USER_UUID = "USER_UUID";
 
 }
