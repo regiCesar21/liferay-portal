92ef10a69fdc7ec38b59f5afea78993743b623c7
Matheus Santos <matheus.santos@liferay.com>
2023-12-05 11:48:47 -0300

    ISSD-505 buildService

---
+++ modules/dxp/apps/osb/osb-provisioning/osb-provisioning-api/src/main/java/com/liferay/osb/provisioning/exception/DuplicateRelatedSalesforceProjectKeyException.java
@@ -0,0 +1,29 @@
+/**
+ * SPDX-FileCopyrightText: (c) 2023 Liferay, Inc. https://liferay.com
+ * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
+ */
+package com.liferay.osb.provisioning.exception;
+
+import com.liferay.portal.kernel.exception.PortalException;
+
+/**
+ * @author Brian Wing Shun Chan
+ */
+public class DuplicateRelatedSalesforceProjectKeyException extends PortalException {
+
+	public DuplicateRelatedSalesforceProjectKeyException() {
+	}
+
+	public DuplicateRelatedSalesforceProjectKeyException(String msg) {
+		super(msg);
+	}
+
+	public DuplicateRelatedSalesforceProjectKeyException(String msg, Throwable throwable) {
+		super(msg, throwable);
+	}
+
+	public DuplicateRelatedSalesforceProjectKeyException(Throwable throwable) {
+		super(throwable);
+	}
+
+}
