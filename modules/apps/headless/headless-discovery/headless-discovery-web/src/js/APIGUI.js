c850cc04d66d594036a8a1e2175e974e522e9b6e
Jaime León <jaime.leon@liferay.com>
2023-12-05 13:05:30 +0100

    LPS-147834 Add additional checks and error message

--- modules/apps/headless/headless-discovery/headless-discovery-web/src/js/APIGUI.js
+++ modules/apps/headless/headless-discovery/headless-discovery-web/src/js/APIGUI.js
@@ -3,6 +3,7 @@
  * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
  */
 
+import ClayAlert from '@clayui/alert';
 import ClayButton from '@clayui/button';
 import ClayDropDown from '@clayui/drop-down';
 import ClayLayout from '@clayui/layout';
@@ -29,6 +30,7 @@ const APIGUI = () => {
 	const [showHeaders, setShowHeaders] = useState(false);
 	const [showGraphQL, setShowGraphQL] = useState(false);
 	const [headers, setHeaders] = useState([{key: '', value: ''}]);
+	const [origin, setOrigin] = useState('');
 
 	const {observer, onClose} = useModal({
 		onClose: () => setShowHeaders(false),
@@ -42,6 +44,12 @@ const APIGUI = () => {
 
 	useEffect(() => {
 		apiFetch(contextPath + '/o/openapi', 'get', {}).then((response) => {
+			setOrigin(
+				Object.values(response)[0][0].substring(
+					0,
+					Object.values(response)[0][0].indexOf('/o/')
+				)
+			);
 			setEndpoints(
 				Object.keys(response)
 					.flatMap((key) => response[key])
@@ -282,8 +290,14 @@ const APIGUI = () => {
 					</ClayModal>
 				)}
 
-				{!endpoint.startsWith('http://localhost:8080') ? (
-					<>Error!!</>
+				{showGraphQL ? (
+					<ClayLayout.Row className="vh-100">
+						<GraphiQL fetcher={graphQLFetcher} />
+					</ClayLayout.Row>
+				) : endpoint && !endpoint.startsWith(origin) ? (
+					<ClayAlert className="mt-4" displayType="danger">
+						Forbiden access.
+					</ClayAlert>
 				) : (
 					!showGraphQL && (
 						<SwaggerUI
@@ -298,7 +312,9 @@ const APIGUI = () => {
 							]}
 							url={
 								endpoint ||
-								endpoints.find((url) => url.includes('delivery'))
+								endpoints.find((url) =>
+									url.includes('delivery')
+								)
 							}
 						/>
 					)
