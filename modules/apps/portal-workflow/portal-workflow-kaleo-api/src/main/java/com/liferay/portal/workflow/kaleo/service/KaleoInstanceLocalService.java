8deea1e9639f2b6305c26bd28fc05706c4f9ad3d
Carolina Barbosa <carolina.barbosa@liferay.com>
2023-12-06 14:16:20 -0300

    LPS-140042 Partial backport of LPS-104155 | LPS-104160 Original commit: LPS-104155 | LPS-104160 Regen (5f4d4781024bf44afa547e96b0b392a74522471d)

--- modules/apps/portal-workflow/portal-workflow-kaleo-api/src/main/java/com/liferay/portal/workflow/kaleo/service/KaleoInstanceLocalService.java
+++ modules/apps/portal-workflow/portal-workflow-kaleo-api/src/main/java/com/liferay/portal/workflow/kaleo/service/KaleoInstanceLocalService.java
@@ -67,6 +67,7 @@ public interface KaleoInstanceLocalService
 	@Indexable(type = IndexableType.REINDEX)
 	public KaleoInstance addKaleoInstance(KaleoInstance kaleoInstance);
 
+	@Indexable(type = IndexableType.REINDEX)
 	public KaleoInstance addKaleoInstance(
 			long kaleoDefinitionVersionId, String kaleoDefinitionName,
 			int kaleoDefinitionVersion,
@@ -74,6 +75,7 @@ public interface KaleoInstanceLocalService
 			ServiceContext serviceContext)
 		throws PortalException;
 
+	@Indexable(type = IndexableType.REINDEX)
 	public KaleoInstance completeKaleoInstance(long kaleoInstanceId)
 		throws PortalException;
 
@@ -344,10 +346,12 @@ public interface KaleoInstanceLocalService
 	@Indexable(type = IndexableType.REINDEX)
 	public KaleoInstance updateKaleoInstance(KaleoInstance kaleoInstance);
 
+	@Indexable(type = IndexableType.REINDEX)
 	public KaleoInstance updateKaleoInstance(
 			long kaleoInstanceId, long rootKaleoInstanceTokenId)
 		throws PortalException;
 
+	@Indexable(type = IndexableType.REINDEX)
 	public KaleoInstance updateKaleoInstance(
 			long kaleoInstanceId, Map<String, Serializable> workflowContext,
 			ServiceContext serviceContext)
