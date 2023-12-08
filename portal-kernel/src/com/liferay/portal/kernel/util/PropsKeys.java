b38b38c22648b8b936f4af52bc0a9465d23cac4b
Julius Lee <meng-huan.lee@liferay.com>
2023-12-08 14:16:55 -0800

    LPS-65857 Add two warning messages for clustering configuration using: default cluster.link.auth.value and default MD5 configuration

--- portal-kernel/src/com/liferay/portal/kernel/util/PropsKeys.java
+++ portal-kernel/src/com/liferay/portal/kernel/util/PropsKeys.java
@@ -487,6 +487,9 @@ public interface PropsKeys {
 
 	public static final String CDN_HOST_HTTPS = "cdn.host.https";
 
+	public static final String CLUSTER_LINK_AUTH_VALUE =
+		"cluster.link.auth.value";
+
 	public static final String CLUSTER_LINK_AUTODETECT_ADDRESS =
 		"cluster.link.autodetect.address";
 
