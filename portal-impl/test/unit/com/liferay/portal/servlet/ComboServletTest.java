a9fc6fe14cbee4c7b6ea2882d5803410c46952f7
Brian Chan <brian.chan@liferay.com>
2023-12-11 23:43:29 +0100

    LPS-200229 Sublime Sort

--- portal-impl/test/unit/com/liferay/portal/servlet/ComboServletTest.java
+++ portal-impl/test/unit/com/liferay/portal/servlet/ComboServletTest.java
@@ -252,15 +252,11 @@ public class ComboServletTest extends PowerMockito {
 
 	@Test
 	public void testServiceWithNoncanonicalPaths() throws Exception {
-		_testService(null, "/../js/aui.js", _portalServletContext);
-
 		_testService("/js/aui.js", "/./js/aui.js", _portalServletContext);
-
 		_testService("/js/aui.js", "/js/./aui.js", _portalServletContext);
-
 		_testService("/js/aui.js", "/js//aui.js", _portalServletContext);
-
 		_testService("/js/aui.js", "/js/down/../aui.js", _portalServletContext);
+		_testService(null, "/../js/aui.js", _portalServletContext);
 	}
 
 	@Test
