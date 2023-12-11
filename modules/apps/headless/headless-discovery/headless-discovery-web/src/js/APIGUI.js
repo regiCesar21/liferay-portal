c4a914b907f411f6adc924e740491129cdc23121
jalber786 <alberto.moreno@liferay.com>
2023-12-11 12:53:24 +0100

    LPS-203647 Misspelling correction

--- modules/apps/headless/headless-discovery/headless-discovery-web/src/js/APIGUI.js
+++ modules/apps/headless/headless-discovery/headless-discovery-web/src/js/APIGUI.js
@@ -288,7 +288,7 @@ const APIGUI = () => {
 					</ClayLayout.Row>
 				) : endpoint && !endpoint.startsWith(origin) ? (
 					<ClayAlert className="mt-4" displayType="danger">
-						Forbiden access.
+						Forbidden access.
 					</ClayAlert>
 				) : (
 					!showGraphQL && (
