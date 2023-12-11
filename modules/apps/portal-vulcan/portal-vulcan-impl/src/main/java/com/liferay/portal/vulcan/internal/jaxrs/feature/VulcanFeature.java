744dc36e404c075c6cd369b1e1efde9506995501
sergiojimcos <sergio.jdelcoso@liferay.com>
2023-12-11 13:27:34 +0100

    LPS-191346 Apply PaginationProvider to PaginationContextProvider for REST API

--- modules/apps/portal-vulcan/portal-vulcan-impl/src/main/java/com/liferay/portal/vulcan/internal/jaxrs/feature/VulcanFeature.java
+++ modules/apps/portal-vulcan/portal-vulcan-impl/src/main/java/com/liferay/portal/vulcan/internal/jaxrs/feature/VulcanFeature.java
@@ -63,6 +63,7 @@ import com.liferay.portal.vulcan.internal.jaxrs.writer.interceptor.EntityExtensi
 import com.liferay.portal.vulcan.internal.jaxrs.writer.interceptor.NestedFieldsWriterInterceptor;
 import com.liferay.portal.vulcan.internal.jaxrs.writer.interceptor.PageEntityExtensionWriterInterceptor;
 import com.liferay.portal.vulcan.internal.param.converter.provider.DateParamConverterProvider;
+import com.liferay.portal.vulcan.pagination.provider.PaginationProvider;
 
 import javax.ws.rs.Priorities;
 import javax.ws.rs.core.Feature;
@@ -155,7 +156,7 @@ public class VulcanFeature implements Feature {
 			_nestedFieldsWriterInterceptor, Priorities.USER - 10);
 
 		featureContext.register(
-			new PaginationContextProvider(_configurationProvider, _portal));
+			new PaginationContextProvider(_paginationProvider, _portal));
 		featureContext.register(
 			new SiteParamConverterProvider(
 				_depotEntryLocalService, _groupLocalService));
@@ -219,6 +220,9 @@ public class VulcanFeature implements Feature {
 	private Language _language;
 
 	private NestedFieldsWriterInterceptor _nestedFieldsWriterInterceptor;
+
+	@Reference
+	private PaginationProvider _paginationProvider;
 
 	@Reference
 	private Portal _portal;
