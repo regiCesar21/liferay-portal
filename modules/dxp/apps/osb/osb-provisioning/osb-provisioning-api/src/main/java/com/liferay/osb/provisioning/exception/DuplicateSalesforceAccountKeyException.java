2ec5699ba88b6d237f0c7ff5143ae8f822362da9
Karoline Silva <karoline.silva@liferay.com>
2023-12-13 10:49:34 -0300

    ISSD-501 buildService

---
+++ modules/dxp/apps/osb/osb-provisioning/osb-provisioning-api/src/main/java/com/liferay/osb/provisioning/exception/DuplicateSalesforceAccountKeyException.java
@@ -0,0 +1,32 @@
+/**
+ * SPDX-FileCopyrightText: (c) 2023 Liferay, Inc. https://liferay.com
+ * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
+ */
+
+package com.liferay.osb.provisioning.exception;
+
+import com.liferay.portal.kernel.exception.PortalException;
+
+/**
+ * @author Brian Wing Shun Chan
+ */
+public class DuplicateSalesforceAccountKeyException extends PortalException {
+
+	public DuplicateSalesforceAccountKeyException() {
+	}
+
+	public DuplicateSalesforceAccountKeyException(String msg) {
+		super(msg);
+	}
+
+	public DuplicateSalesforceAccountKeyException(
+		String msg, Throwable throwable) {
+
+		super(msg, throwable);
+	}
+
+	public DuplicateSalesforceAccountKeyException(Throwable throwable) {
+		super(throwable);
+	}
+
+}
