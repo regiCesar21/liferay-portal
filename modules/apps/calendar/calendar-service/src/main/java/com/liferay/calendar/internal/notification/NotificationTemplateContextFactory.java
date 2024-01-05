b7278178be058ff2251e3e8945377f82320bac76
MarinhoFeliphe <feliphemarinho18@gmail.com>
2024-01-05 11:36:27 -0300

    LPS-205014 Move layout variable declaration inside the if statement because it's only used there

--- modules/apps/calendar/calendar-service/src/main/java/com/liferay/calendar/internal/notification/NotificationTemplateContextFactory.java
+++ modules/apps/calendar/calendar-service/src/main/java/com/liferay/calendar/internal/notification/NotificationTemplateContextFactory.java
@@ -22,7 +22,6 @@ import com.liferay.portal.kernel.language.LanguageUtil;
 import com.liferay.portal.kernel.model.Company;
 import com.liferay.portal.kernel.model.Group;
 import com.liferay.portal.kernel.model.GroupConstants;
-import com.liferay.portal.kernel.model.Layout;
 import com.liferay.portal.kernel.model.User;
 import com.liferay.portal.kernel.service.CompanyLocalService;
 import com.liferay.portal.kernel.service.GroupLocalService;
@@ -254,15 +253,13 @@ public class NotificationTemplateContextFactory {
 		Group group = _groupLocalService.getGroup(
 				user.getCompanyId(), GroupConstants.GUEST);
 
-		Layout layout = _layoutLocalService.fetchLayout(
-			group.getDefaultPublicPlid());
-
 		if (portalURL == null) {
 			portalURL = _getPortalURL(group.getCompanyId(), group.getGroupId());
 		}
 
 		if (layoutURL == null) {
-			layoutURL = PortalUtil.getLayoutActualURL(layout);
+			layoutURL = PortalUtil.getLayoutActualURL(
+				_layoutLocalService.fetchLayout(group.getDefaultPublicPlid()));
 		}
 
 		String url = portalURL + layoutURL;
