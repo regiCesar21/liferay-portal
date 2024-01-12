fa184b49c01b7b82af19b68c80fbf4bd56c211c6
Daniel Bonasser <danielbonasser@gmail.com>
2024-01-12 16:21:25 -0300

    LPS-139841 Remove axios from portal-workflow-metrics-web

--- modules/dxp/apps/portal-workflow/portal-workflow-metrics-web/src/main/resources/META-INF/resources/js/shared/rest/fetch.es.js
+++ modules/dxp/apps/portal-workflow/portal-workflow-metrics-web/src/main/resources/META-INF/resources/js/shared/rest/fetch.es.js
@@ -3,35 +3,10 @@
  * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
  */
 
-import axios from 'axios';
+export const baseURL = '/o/portal-workflow-metrics/v1.0';
 
-/**
- * @module fetch
- * @description Basic alias of fetch client to perform consults in portal rest API.
- * @example
- * import fetch from '@/shared/rest/fetch';
- * fetch.get('/process').then(res => console.log(res));
- */
-axios.defaults.headers.common[
-	'Accept-Language'
-] = Liferay.ThemeDisplay.getBCP47LanguageId();
-
-axios.defaults.params = {
-	['p_auth']: Liferay.authToken
-};
-
-const restClient = axios.create({
-	baseURL: '/o/portal-workflow-metrics/v1.0'
-});
-
-const baseURL = '/o/portal-workflow-metrics/v1.0';
-
-const headers = new Headers({
+export const headers = new Headers({
 	Accept: 'application/json',
 	'Accept-Language': Liferay.ThemeDisplay.getBCP47LanguageId(),
 	'Content-Type': 'application/json'
 });
-
-export {baseURL, headers};
-
-export default restClient;
