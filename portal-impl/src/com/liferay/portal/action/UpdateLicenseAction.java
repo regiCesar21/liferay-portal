fbfc74eca73bc4dc857a1e7a5a9d492ced3b522c
Brian Chan <brian.chan@liferay.com>
2024-01-30 10:50:21 +0900

    LPD-3994 Move it here to match portal-web/docroot/html/portal/update_password.jsp:

--- portal-impl/src/com/liferay/portal/action/UpdateLicenseAction.java
+++ portal-impl/src/com/liferay/portal/action/UpdateLicenseAction.java
@@ -11,6 +11,8 @@ import com.liferay.portal.kernel.json.JSONArray;
 import com.liferay.portal.kernel.json.JSONFactoryUtil;
 import com.liferay.portal.kernel.json.JSONObject;
 import com.liferay.portal.kernel.license.util.LicenseManagerUtil;
+import com.liferay.portal.kernel.log.Log;
+import com.liferay.portal.kernel.log.LogFactoryUtil;
 import com.liferay.portal.kernel.model.User;
 import com.liferay.portal.kernel.security.auth.AuthTokenUtil;
 import com.liferay.portal.kernel.servlet.HttpMethods;
