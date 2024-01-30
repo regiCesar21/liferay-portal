b25fb5cdde28c530ba8191367c89002eb76212e7
alvarosaugarlr <alvaro.saugar@liferay.com>
2024-01-30 16:47:37 +0100

    LPS-204341 sf

--- modules/apps/portal-security/portal-security-ldap-test/src/testIntegration/java/com/liferay/portal/security/ldap/internal/configuration/test/SystemLDAPConfigurationProviderImplTest.java
+++ modules/apps/portal-security/portal-security-ldap-test/src/testIntegration/java/com/liferay/portal/security/ldap/internal/configuration/test/SystemLDAPConfigurationProviderImplTest.java
@@ -1,5 +1,5 @@
 /**
- * SPDX-FileCopyrightText: (c) 2023 Liferay, Inc. https://liferay.com
+ * SPDX-FileCopyrightText: (c) 2024 Liferay, Inc. https://liferay.com
  * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
  */
 
@@ -130,8 +130,10 @@ public class SystemLDAPConfigurationProviderImplTest {
 		ConfigurationTestUtil.deleteFactoryConfiguration(
 			pid, SystemLDAPConfiguration.class.getName());
 
-		Assert.assertEquals(systemLDAPConfiguration.pageSize(), randomValueInstance);
-		Assert.assertEquals(systemLDAPConfiguration.rangeSize(), randomValueInstance);
+		Assert.assertEquals(
+			systemLDAPConfiguration.pageSize(), randomValueInstance);
+		Assert.assertEquals(
+			systemLDAPConfiguration.rangeSize(), randomValueInstance);
 
 		Assert.assertEquals(
 			_defaultSystemLDAPConfiguration.factoryInitial(),
--- modules/apps/portal-security/portal-security-ldap-test/src/testIntegration/java/com/liferay/portal/security/ldap/internal/exportimport/configuration/test/LDAPExportConfigurationProviderImplTest.java
+++ modules/apps/portal-security/portal-security-ldap-test/src/testIntegration/java/com/liferay/portal/security/ldap/internal/exportimport/configuration/test/LDAPExportConfigurationProviderImplTest.java
@@ -1,5 +1,5 @@
 /**
- * SPDX-FileCopyrightText: (c) 2023 Liferay, Inc. https://liferay.com
+ * SPDX-FileCopyrightText: (c) 2024 Liferay, Inc. https://liferay.com
  * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
  */
 
@@ -91,7 +91,6 @@ public class LDAPExportConfigurationProviderImplTest {
 			).put(
 				"exportGroupEnabled", true
 			).build());
-
 
 		String pid = ConfigurationTestUtil.createFactoryConfiguration(
 			LDAPExportConfiguration.class.getName(),
