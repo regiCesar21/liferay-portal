e83dc44077156c7cafa7df3ad1f55ecd98131e76
Marcos Martins <marcos.martins@liferay.com>
2024-01-30 17:18:55 -0300

    LPD-6938 apply for emulator

--- modules/dxp/apps/osb/osb-asah/osb-asah-common/src/main/resources/com/liferay/osb/asah/common/dog/dependencies/unsuppress_individual_statement_emulator.sql
+++ modules/dxp/apps/osb/osb-asah/osb-asah-common/src/main/resources/com/liferay/osb/asah/common/dog/dependencies/unsuppress_individual_statement_emulator.sql
@@ -1,13 +1,9 @@
 BEGIN
 	BEGIN TRANSACTION;
 
-	${anonymize_activities_statement}
-
 	DELETE FROM Suppression WHERE emailAddress = '${email_address}';
 
 	UPDATE BQIndividual SET suppressed = false WHERE emailAddress = '${email_address}';
-
-	UPDATE Identity_Raw SET individualId = NULL WHERE individualId = '${individual_id}' AND createDate BETWEEN timestamp '${range_start_date}' AND timestamp '${range_end_date}';
 
 	COMMIT TRANSACTION;
 
