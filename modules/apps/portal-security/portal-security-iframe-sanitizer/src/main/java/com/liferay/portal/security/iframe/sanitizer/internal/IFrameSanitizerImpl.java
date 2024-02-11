912a64f64b4ef1fb186d4872ce5b2befe3693784
Eudaldo Alonso <eudaldo.alonso@liferay.com>
2024-02-12 00:08:09 +0100

    LPD-17503 Uses parseBodyFragment, in that way we are going to parse the content into the body, if not if we have something like: <script>alert('Hello')</script>, we are going to parse it into the head

--- modules/apps/portal-security/portal-security-iframe-sanitizer/src/main/java/com/liferay/portal/security/iframe/sanitizer/internal/IFrameSanitizerImpl.java
+++ modules/apps/portal-security/portal-security-iframe-sanitizer/src/main/java/com/liferay/portal/security/iframe/sanitizer/internal/IFrameSanitizerImpl.java
@@ -89,7 +89,7 @@ public class IFrameSanitizerImpl implements Sanitizer {
 	}
 
 	private Document _getDocument(String content) {
-		Document document = Jsoup.parse(content);
+		Document document = Jsoup.parseBodyFragment(content);
 
 		document.outputSettings(
 			new Document.OutputSettings() {
