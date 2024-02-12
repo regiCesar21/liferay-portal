b56f27ae318296d84f2fd6c805c857a92a87bba4
Eric Yan <eric.yan@liferay.com>
2024-02-12 17:33:53 -0500

    LPD-2110 Move related test to friendly-url-test

--- modules/apps/friendly-url/friendly-url-test/src/testIntegration/java/com/liferay/friendly/url/servlet/test/FriendlyURLServletLocalizedFriendlyURLTest.java
+++ modules/apps/friendly-url/friendly-url-test/src/testIntegration/java/com/liferay/friendly/url/servlet/test/FriendlyURLServletLocalizedFriendlyURLTest.java
@@ -3,7 +3,7 @@
  * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
  */
 
-package com.liferay.portal.util.test;
+package com.liferay.friendly.url.servlet.test;
 
 import com.liferay.arquillian.extension.junit.bridge.junit.Arquillian;
 import com.liferay.layout.test.util.LayoutTestUtil;
@@ -55,7 +55,7 @@ import org.springframework.mock.web.MockHttpServletRequest;
  * @author Sergio González
  */
 @RunWith(Arquillian.class)
-public class PortalImplLocalizedFriendlyURLTest {
+public class FriendlyURLServletLocalizedFriendlyURLTest {
 
 	@ClassRule
 	@Rule
