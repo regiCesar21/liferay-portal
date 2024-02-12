8e5624716cbd637d405d1d8d29d12d7654891096
Alan Huang <alan.huang@liferay.com>
2024-02-12 23:00:51 +0800

    LPD-17543 SF

--- modules/apps/layout/layout-admin-web/src/main/java/com/liferay/layout/admin/web/internal/display/context/LayoutsAdminDisplayContext.java
+++ modules/apps/layout/layout-admin-web/src/main/java/com/liferay/layout/admin/web/internal/display/context/LayoutsAdminDisplayContext.java
@@ -797,6 +797,7 @@ public class LayoutsAdminDisplayContext {
 			layoutsSearchContainer.getOrderByComparator());
 
 		layoutsSearchContainer.setResults(layouts);
+
 		layoutsSearchContainer.setTotal(layoutsCount);
 
 		_layoutsSearchContainer = layoutsSearchContainer;
