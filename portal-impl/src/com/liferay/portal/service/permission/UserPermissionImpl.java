efdf775ec060dbb13ac10e344bec9ca833fe45d8
Crescenzo Rega <crescenzo.rega@liferay.com>
2024-02-15 17:41:42 +0100

    LPD-16328 Change log level

--- portal-impl/src/com/liferay/portal/service/permission/UserPermissionImpl.java
+++ portal-impl/src/com/liferay/portal/service/permission/UserPermissionImpl.java
@@ -182,7 +182,9 @@ public class UserPermissionImpl
 			}
 		}
 		catch (Exception exception) {
-			_log.error(exception, exception);
+			if (_log.isDebugEnabled()) {
+				_log.debug(exception, exception);
+			}
 		}
 
 		return false;
