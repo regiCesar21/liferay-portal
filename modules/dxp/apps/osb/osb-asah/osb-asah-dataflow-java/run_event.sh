b3c59590c751a09d5bf4fb8af7a0d32527804166
Riccardo Ferrari <riccardo.ferrari@liferay.com>
2024-02-19 17:50:37 +0100

    LPD-5070 Add custom network/subnetwork support to event run script

--- modules/dxp/apps/osb/osb-asah/osb-asah-dataflow-java/run_event.sh
+++ modules/dxp/apps/osb/osb-asah/osb-asah-dataflow-java/run_event.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-if [ "$#" -ne 1 ]
+if [ "$#" -lt 1 ]
 then
 	echo "Usage: run-event.sh [dxp-cloud-project]"
 	exit 1
@@ -10,6 +10,7 @@ PROJECT_ID=$(gcloud config get-value project)
 
 DXP_CLOUD_PROJECT=${1}
 MAIN_CLASS_NAME=com.liferay.osb.asah.dataflow.ingestion.event.EventIngestionPipeline
+NETWORK=${2:-default}
 OUTPUT_FOLDER=gs://${PROJECT_ID}-analytics-events
 PIPELINE_FOLDER=gs://${PROJECT_ID}-dataflow
 REGION=$(gcloud config get-value compute/region)
@@ -17,10 +18,19 @@ RUNNER=DataflowRunner
 SESSION_WINDOW_ALLOWED_LATENESS=5
 SESSION_WINDOW_GAP_DURATION=30
 
+if [ "${NETWORK}" != "default" ];
+then
+	SUBNETWORK=${3}
+
+	CUSTOM_NETWORK="\
+	  --network=${NETWORK} \
+	  --subnetwork=regions/${REGION}/subnetworks/${SUBNETWORK}"
+fi
+
 ../gradlew clean assemble execute \
 	-Dexec.args=" \
 		--inputSubscription=projects/${PROJECT_ID}/subscriptions/${DXP_CLOUD_PROJECT}_analytics_events_dataflow \
-		--jobName=eventingestionpipeline-marcellustavares-20220214-155ebd6
+		--jobName=eventingestionpipeline-${DXP_CLOUD_PROJECT} \
 		--outputDirectory=${OUTPUT_FOLDER} \
 		--outputFileNamePrefix=analytics-events \
 		--project=${PROJECT_ID} \
@@ -28,6 +38,6 @@ SESSION_WINDOW_GAP_DURATION=30
 		--runner=${RUNNER} \
 		--sessionWindowAllowedLateness=${SESSION_WINDOW_ALLOWED_LATENESS} \
 		--sessionWindowGapDuration=${SESSION_WINDOW_GAP_DURATION} \
-		--tempLocation=${PIPELINE_FOLDER}/temp" \
+		--tempLocation=${PIPELINE_FOLDER}/temp ${CUSTOM_NETWORK}" \
 	-Dexec.cleanupDaemonThreads=false \
 	-Dexec.mainClass=${MAIN_CLASS_NAME}
