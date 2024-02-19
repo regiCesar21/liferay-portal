c18317de8789b420d350fe6fc348b700e80c3435
Riccardo Ferrari <riccardo.ferrari@liferay.com>
2024-02-19 18:09:20 +0100

    LPD-5070 osb-asah-composer create composer script should add custom network environment variables

--- modules/dxp/apps/osb/osb-asah/osb-asah-composer/create_composer.sh
+++ modules/dxp/apps/osb/osb-asah/osb-asah-composer/create_composer.sh
@@ -1,12 +1,21 @@
 #!/bin/bash
 
 LCP_PROJECT_ID=${1:-asahdev}
+NETWORK=${2:-default}
 PROJECT_ID=$(gcloud config get-value project)
 PROJECT_NUMBER=$(gcloud projects list \
 	--filter="${PROJECT_ID}" \
 	--format="value(PROJECT_NUMBER)")
 REGION=$(gcloud config get-value compute/region)
 SERVICE_ACCOUNT=ac-composer-admin@${PROJECT_ID}.iam.gserviceaccount.com
+
+if [ "${NETWORK}" != "default" ];
+then
+	SUBNETWORK=${3}
+
+	CUSTOM_NETWORK="--network ${NETWORK} --subnetwork ${SUBNETWORK}"
+	CUSTOM_NETWORK_ENV=",NETWORK=${NETWORK},SUBNETWORK=${SUBNETWORK}"
+fi
 
 COMPOSER_ENVIRONMENT_NAME=ac-composer-${LCP_PROJECT_ID}
 
@@ -24,8 +33,9 @@ gcloud composer environments create ${COMPOSER_ENVIRONMENT_NAME} \
 	--async \
 	--enable-master-authorized-networks \
 	--environment-size large \
-	--env-variables LCP_PROJECT_ID=${LCP_PROJECT_ID},GOOGLE_PROJECT_ID=${PROJECT_ID},GOOGLE_REGION=${REGION} \
+	--env-variables LCP_PROJECT_ID=${LCP_PROJECT_ID},GOOGLE_PROJECT_ID=${PROJECT_ID},GOOGLE_REGION=${REGION}${CUSTOM_NETWORK_ENV} \
 	--location ${REGION} \
 	--master-authorized-networks ${MASTER_AUTHORIZED_NETWORKS} \
 	--project ${PROJECT_ID} \
-	--service-account ac-composer-admin@${PROJECT_ID}.iam.gserviceaccount.com
+	--service-account ac-composer-admin@${PROJECT_ID}.iam.gserviceaccount.com \
+	${CUSTOM_NETWORK}
