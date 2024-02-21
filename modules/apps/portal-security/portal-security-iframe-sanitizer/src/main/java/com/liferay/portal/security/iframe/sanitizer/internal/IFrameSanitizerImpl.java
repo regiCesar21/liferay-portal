aa90421484c5d2301a54306c36b8ba4957aaf1bf
NemethNorbert <norbert.nemeth@liferay.com>
2024-02-21 14:18:13 +0100

    LPD-16392 Implement missing methods Unique commit for 7.1

--- modules/apps/portal-security/portal-security-iframe-sanitizer/src/main/java/com/liferay/portal/security/iframe/sanitizer/internal/IFrameSanitizerImpl.java
+++ modules/apps/portal-security/portal-security-iframe-sanitizer/src/main/java/com/liferay/portal/security/iframe/sanitizer/internal/IFrameSanitizerImpl.java
@@ -9,10 +9,18 @@ import com.liferay.petra.string.StringBundler;
 import com.liferay.petra.string.StringPool;
 import com.liferay.portal.configuration.metatype.bnd.util.ConfigurableUtil;
 import com.liferay.portal.kernel.sanitizer.Sanitizer;
+import com.liferay.portal.kernel.sanitizer.SanitizerException;
 import com.liferay.portal.kernel.util.ContentTypes;
+import com.liferay.portal.kernel.util.StreamUtil;
 import com.liferay.portal.kernel.util.StringUtil;
 import com.liferay.portal.kernel.util.Validator;
 import com.liferay.portal.security.iframe.sanitizer.configuration.IFrameConfiguration;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.io.UnsupportedEncodingException;
 
 import java.util.Map;
 
@@ -33,6 +41,65 @@ import org.osgi.service.component.annotations.Modified;
 	service = Sanitizer.class
 )
 public class IFrameSanitizerImpl implements Sanitizer {
+
+	@Override
+	public byte[] sanitize(
+			long companyId, long groupId, long userId, String className,
+			long classPK, String contentType, String[] modes, byte[] bytes,
+			Map<String, Object> options)
+		throws SanitizerException {
+
+		if (bytes == null) {
+			return null;
+		}
+
+		try {
+			String content = new String(bytes, StringPool.UTF8);
+
+			String result = sanitize(
+				companyId, groupId, userId, className, classPK, contentType,
+				modes, content, options);
+
+			return result.getBytes(StringPool.UTF8);
+		}
+		catch (UnsupportedEncodingException uee) {
+			throw new SanitizerException(uee);
+		}
+	}
+
+	@Override
+	public void sanitize(
+			long companyId, long groupId, long userId, String className,
+			long classPK, String contentType, String[] modes,
+			InputStream inputStream, OutputStream outputStream,
+			Map<String, Object> options)
+		throws SanitizerException {
+
+		if ((inputStream == null) || (outputStream == null)) {
+			return;
+		}
+
+		ByteArrayOutputStream byteArrayOutputStream =
+			new ByteArrayOutputStream();
+
+		try {
+			StreamUtil.transfer(inputStream, byteArrayOutputStream);
+
+			String content = new String(
+				byteArrayOutputStream.toByteArray(), StringPool.UTF8);
+
+			String result = sanitize(
+				companyId, groupId, userId, className, classPK, contentType,
+				modes, content, options);
+
+			byte[] bytes = result.getBytes(StringPool.UTF8);
+
+			outputStream.write(bytes);
+		}
+		catch (IOException ioe) {
+			throw new SanitizerException(ioe);
+		}
+	}
 
 	@Override
 	public String sanitize(
