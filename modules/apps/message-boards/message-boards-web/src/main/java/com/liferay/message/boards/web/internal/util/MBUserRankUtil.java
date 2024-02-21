fea68eb3bcbf4c7685a075f72888035d9ce3895d
NemethNorbert <norbert.nemeth@liferay.com>
2024-02-21 10:46:16 +0100

    LPD-18323 render the entityValue instead of the full entity

--- modules/apps/message-boards/message-boards-web/src/main/java/com/liferay/message/boards/web/internal/util/MBUserRankUtil.java
+++ modules/apps/message-boards/message-boards-web/src/main/java/com/liferay/message/boards/web/internal/util/MBUserRankUtil.java
@@ -70,7 +70,7 @@ public class MBUserRankUtil {
 					if (_isEntityRank(
 							companyId, statsUser, entityType, entityValue)) {
 
-						rank[1] = curRank;
+						rank[1] = entityValue;
 
 						break;
 					}
