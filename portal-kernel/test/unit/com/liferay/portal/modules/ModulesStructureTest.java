15456d901e0bb9e4a9c77b74ae993f54865addfc
Peter Shin <peter.shin@liferay.com>
2024-02-23 14:28:19 -0600

    LPD-15162 OSB

--- portal-kernel/test/unit/com/liferay/portal/modules/ModulesStructureTest.java
+++ portal-kernel/test/unit/com/liferay/portal/modules/ModulesStructureTest.java
@@ -982,6 +982,7 @@ public class ModulesStructureTest {
 
 		if (name.equals("com.liferay.ant.bnd") ||
 			name.equals("com.liferay.arquillian.extension.junit.bridge") ||
+			name.equals("com.liferay.gradle.plugins.app.docker") ||
 			name.equals("com.liferay.gradle.plugins.defaults") ||
 			name.equals("com.liferay.portal.cache.test.util") ||
 			name.equals("com.liferay.poshi.core") ||
