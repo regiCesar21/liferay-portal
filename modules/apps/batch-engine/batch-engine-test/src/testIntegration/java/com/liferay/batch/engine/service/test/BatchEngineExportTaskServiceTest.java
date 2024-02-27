8379e02aef0f2fe0c3b34b692232f734e7c1341a
Vendel Toreki <vendel.toreki@liferay.com>
2024-02-27 17:23:02 +0100

    LPD-15576 Remove tests for methods that do not exist yet

--- modules/apps/batch-engine/batch-engine-test/src/testIntegration/java/com/liferay/batch/engine/service/test/BatchEngineExportTaskServiceTest.java
+++ modules/apps/batch-engine/batch-engine-test/src/testIntegration/java/com/liferay/batch/engine/service/test/BatchEngineExportTaskServiceTest.java
@@ -11,10 +11,8 @@ import com.liferay.batch.engine.internal.test.BlogPosting;
 import com.liferay.batch.engine.model.BatchEngineExportTask;
 import com.liferay.batch.engine.service.BatchEngineExportTaskLocalService;
 import com.liferay.batch.engine.service.BatchEngineExportTaskService;
-import com.liferay.portal.kernel.dao.orm.QueryUtil;
 import com.liferay.portal.kernel.model.User;
 import com.liferay.portal.kernel.security.auth.PrincipalException;
-import com.liferay.portal.kernel.test.AssertUtils;
 import com.liferay.portal.kernel.test.util.TestPropsValues;
 import com.liferay.portal.kernel.test.util.UserTestUtil;
 import com.liferay.portal.kernel.util.HashMapBuilder;
@@ -23,9 +21,6 @@ import com.liferay.portal.test.rule.Inject;
 import java.io.Serializable;
 
 import java.util.Collections;
-import java.util.List;
-
-import org.hamcrest.CoreMatchers;
 
 import org.junit.Assert;
 import org.junit.Before;
@@ -52,20 +47,24 @@ public class BatchEngineExportTaskServiceTest
 	public void testAddBatchEngineExportTask() throws Exception {
 		UserTestUtil.setUser(user);
 
-		AssertUtils.assertFailure(
-			PrincipalException.class, null,
-			() -> _batchEngineExportTaskService.addBatchEngineExportTask(
-				null, defaultCompany.getCompanyId(), user.getUserId(), null,
+		try {
+			_batchEngineExportTaskService.addBatchEngineExportTask(
+				defaultCompany.getCompanyId(), user.getUserId(), null,
 				BlogPosting.class.getName(), "JSON",
 				BatchEngineTaskExecuteStatus.INITIAL.name(),
 				Collections.emptyList(),
 				HashMapBuilder.<String, Serializable>put(
 					"siteId", TestPropsValues.getGroupId()
 				).build(),
-				null));
+				null);
+
+			Assert.fail();
+		}
+		catch (PrincipalException principalException) {
+		}
 
 		_batchEngineExportTaskService.addBatchEngineExportTask(
-			null, company.getCompanyId(), user.getUserId(), null,
+			company.getCompanyId(), user.getUserId(), null,
 			BlogPosting.class.getName(), "JSON",
 			BatchEngineTaskExecuteStatus.INITIAL.name(),
 			Collections.emptyList(),
@@ -85,11 +84,15 @@ public class BatchEngineExportTaskServiceTest
 		_batchEngineExportTaskService.getBatchEngineExportTask(
 			_companyAdminBatchEngineExportTask.getBatchEngineExportTaskId());
 
-		AssertUtils.assertFailure(
-			PrincipalException.class, null,
-			() -> _batchEngineExportTaskService.getBatchEngineExportTask(
+		try {
+			_batchEngineExportTaskService.getBatchEngineExportTask(
 				_defaultCompanyBatchEngineExportTask.
-					getBatchEngineExportTaskId()));
+					getBatchEngineExportTaskId());
+
+			Assert.fail();
+		}
+		catch (PrincipalException principalException) {
+		}
 
 		_batchEngineExportTaskService.getBatchEngineExportTask(
 			_userBatchEngineExportTask.getBatchEngineExportTaskId());
@@ -109,168 +112,35 @@ public class BatchEngineExportTaskServiceTest
 
 		UserTestUtil.setUser(user);
 
-		AssertUtils.assertFailure(
-			PrincipalException.class, null,
-			() -> _batchEngineExportTaskService.getBatchEngineExportTask(
+		try {
+			_batchEngineExportTaskService.getBatchEngineExportTask(
 				_companyAdminBatchEngineExportTask.
-					getBatchEngineExportTaskId()));
-		AssertUtils.assertFailure(
-			PrincipalException.class, null,
-			() -> _batchEngineExportTaskService.getBatchEngineExportTask(
+					getBatchEngineExportTaskId());
+
+			Assert.fail();
+		}
+		catch (PrincipalException principalException) {
+		}
+
+		try {
+			_batchEngineExportTaskService.getBatchEngineExportTask(
 				_defaultCompanyBatchEngineExportTask.
-					getBatchEngineExportTaskId()));
+					getBatchEngineExportTaskId());
+
+			Assert.fail();
+		}
+		catch (PrincipalException principalException) {
+		}
 
 		_batchEngineExportTaskService.getBatchEngineExportTask(
 			_userBatchEngineExportTask.getBatchEngineExportTaskId());
-	}
-
-	@Test
-	public void testGetBatchEngineExportTaskByExternalReferenceCode()
-		throws Exception {
-
-		// Company admin
-
-		UserTestUtil.setUser(companyAdminUser);
-
-		_batchEngineExportTaskService.
-			getBatchEngineExportTaskByExternalReferenceCode(
-				_companyAdminBatchEngineExportTask.getExternalReferenceCode(),
-				_companyAdminBatchEngineExportTask.getCompanyId());
-
-		AssertUtils.assertFailure(
-			PrincipalException.class, null,
-			() ->
-				_batchEngineExportTaskService.
-					getBatchEngineExportTaskByExternalReferenceCode(
-						_defaultCompanyBatchEngineExportTask.
-							getExternalReferenceCode(),
-						_defaultCompanyBatchEngineExportTask.getCompanyId()));
-
-		_batchEngineExportTaskService.
-			getBatchEngineExportTaskByExternalReferenceCode(
-				_userBatchEngineExportTask.getExternalReferenceCode(),
-				_userBatchEngineExportTask.getCompanyId());
-
-		// Omniadmin
-
-		UserTestUtil.setUser(omniadminUser);
-
-		_batchEngineExportTaskService.
-			getBatchEngineExportTaskByExternalReferenceCode(
-				_companyAdminBatchEngineExportTask.getExternalReferenceCode(),
-				_companyAdminBatchEngineExportTask.getCompanyId());
-		_batchEngineExportTaskService.
-			getBatchEngineExportTaskByExternalReferenceCode(
-				_defaultCompanyBatchEngineExportTask.getExternalReferenceCode(),
-				_defaultCompanyBatchEngineExportTask.getCompanyId());
-		_batchEngineExportTaskService.
-			getBatchEngineExportTaskByExternalReferenceCode(
-				_userBatchEngineExportTask.getExternalReferenceCode(),
-				_userBatchEngineExportTask.getCompanyId());
-
-		// User
-
-		UserTestUtil.setUser(user);
-
-		AssertUtils.assertFailure(
-			PrincipalException.class, null,
-			() ->
-				_batchEngineExportTaskService.
-					getBatchEngineExportTaskByExternalReferenceCode(
-						_companyAdminBatchEngineExportTask.
-							getExternalReferenceCode(),
-						_companyAdminBatchEngineExportTask.getCompanyId()));
-		AssertUtils.assertFailure(
-			PrincipalException.class, null,
-			() ->
-				_batchEngineExportTaskService.
-					getBatchEngineExportTaskByExternalReferenceCode(
-						_defaultCompanyBatchEngineExportTask.
-							getExternalReferenceCode(),
-						_defaultCompanyBatchEngineExportTask.getCompanyId()));
-
-		_batchEngineExportTaskService.
-			getBatchEngineExportTaskByExternalReferenceCode(
-				_userBatchEngineExportTask.getExternalReferenceCode(),
-				_userBatchEngineExportTask.getCompanyId());
-	}
-
-	@Test
-	public void testGetBatchEngineExportTasks() throws Exception {
-
-		// Company admin
-
-		UserTestUtil.setUser(companyAdminUser);
-
-		List<BatchEngineExportTask> batchEngineExportTasks =
-			_batchEngineExportTaskService.getBatchEngineExportTasks(
-				company.getCompanyId(), QueryUtil.ALL_POS, QueryUtil.ALL_POS);
-
-		Assert.assertThat(
-			batchEngineExportTasks,
-			CoreMatchers.hasItem(_companyAdminBatchEngineExportTask));
-		Assert.assertThat(
-			batchEngineExportTasks,
-			CoreMatchers.hasItem(_userBatchEngineExportTask));
-
-		AssertUtils.assertFailure(
-			PrincipalException.class, null,
-			() -> _batchEngineExportTaskService.getBatchEngineExportTasks(
-				defaultCompany.getCompanyId(), QueryUtil.ALL_POS,
-				QueryUtil.ALL_POS));
-
-		// Omniadmin
-
-		UserTestUtil.setUser(omniadminUser);
-
-		batchEngineExportTasks =
-			_batchEngineExportTaskService.getBatchEngineExportTasks(
-				company.getCompanyId(), QueryUtil.ALL_POS, QueryUtil.ALL_POS);
-
-		Assert.assertThat(
-			batchEngineExportTasks,
-			CoreMatchers.hasItem(_companyAdminBatchEngineExportTask));
-		Assert.assertThat(
-			batchEngineExportTasks,
-			CoreMatchers.hasItem(_userBatchEngineExportTask));
-
-		batchEngineExportTasks =
-			_batchEngineExportTaskService.getBatchEngineExportTasks(
-				defaultCompany.getCompanyId(), QueryUtil.ALL_POS,
-				QueryUtil.ALL_POS);
-
-		Assert.assertThat(
-			batchEngineExportTasks,
-			CoreMatchers.hasItem(_defaultCompanyBatchEngineExportTask));
-
-		// User
-
-		UserTestUtil.setUser(user);
-
-		batchEngineExportTasks =
-			_batchEngineExportTaskService.getBatchEngineExportTasks(
-				company.getCompanyId(), QueryUtil.ALL_POS, QueryUtil.ALL_POS);
-
-		Assert.assertThat(
-			batchEngineExportTasks,
-			CoreMatchers.not(
-				CoreMatchers.hasItem(_companyAdminBatchEngineExportTask)));
-		Assert.assertThat(
-			batchEngineExportTasks,
-			CoreMatchers.hasItem(_userBatchEngineExportTask));
-
-		AssertUtils.assertFailure(
-			PrincipalException.class, null,
-			() -> _batchEngineExportTaskService.getBatchEngineExportTasks(
-				defaultCompany.getCompanyId(), QueryUtil.ALL_POS,
-				QueryUtil.ALL_POS));
 	}
 
 	private BatchEngineExportTask _addBatchEngineExportTask(User user)
 		throws Exception {
 
 		return _batchEngineExportTaskLocalService.addBatchEngineExportTask(
-			null, user.getCompanyId(), user.getUserId(), null,
+			user.getCompanyId(), user.getUserId(), null,
 			BlogPosting.class.getName(), "JSON",
 			BatchEngineTaskExecuteStatus.INITIAL.name(),
 			Collections.emptyList(),
--- modules/apps/batch-engine/batch-engine-test/src/testIntegration/java/com/liferay/batch/engine/service/test/BatchEngineImportTaskServiceTest.java
+++ modules/apps/batch-engine/batch-engine-test/src/testIntegration/java/com/liferay/batch/engine/service/test/BatchEngineImportTaskServiceTest.java
@@ -8,22 +8,16 @@ package com.liferay.batch.engine.service.test;
 import com.liferay.arquillian.extension.junit.bridge.junit.Arquillian;
 import com.liferay.batch.engine.BatchEngineTaskExecuteStatus;
 import com.liferay.batch.engine.BatchEngineTaskOperation;
-import com.liferay.batch.engine.constants.BatchEngineImportTaskConstants;
 import com.liferay.batch.engine.internal.test.BlogPosting;
 import com.liferay.batch.engine.model.BatchEngineImportTask;
 import com.liferay.batch.engine.service.BatchEngineImportTaskLocalService;
 import com.liferay.batch.engine.service.BatchEngineImportTaskService;
-import com.liferay.portal.kernel.dao.orm.QueryUtil;
 import com.liferay.portal.kernel.model.User;
 import com.liferay.portal.kernel.security.auth.PrincipalException;
-import com.liferay.portal.kernel.test.AssertUtils;
 import com.liferay.portal.kernel.test.util.UserTestUtil;
 import com.liferay.portal.test.rule.Inject;
 
 import java.util.HashMap;
-import java.util.List;
-
-import org.hamcrest.CoreMatchers;
 
 import org.junit.Assert;
 import org.junit.Before;
@@ -50,20 +44,22 @@ public class BatchEngineImportTaskServiceTest
 	public void testAddBatchEngineImportTask() throws Exception {
 		UserTestUtil.setUser(user);
 
-		AssertUtils.assertFailure(
-			PrincipalException.class, null,
-			() -> _batchEngineImportTaskService.addBatchEngineImportTask(
-				null, defaultCompany.getCompanyId(), user.getUserId(), 10, null,
+		try {
+			_batchEngineImportTaskService.addBatchEngineImportTask(
+				defaultCompany.getCompanyId(), user.getUserId(), 10, null,
 				BlogPosting.class.getName(), new byte[0], "JSON",
 				BatchEngineTaskExecuteStatus.INITIAL.name(), null,
-				BatchEngineImportTaskConstants.IMPORT_STRATEGY_ON_ERROR_FAIL,
-				BatchEngineTaskOperation.CREATE.name(), new HashMap<>(), null));
+				BatchEngineTaskOperation.CREATE.name(), new HashMap<>(), null);
+
+			Assert.fail();
+		}
+		catch (PrincipalException principalException) {
+		}
 
 		_batchEngineImportTaskService.addBatchEngineImportTask(
-			null, company.getCompanyId(), user.getUserId(), 10, null,
+			company.getCompanyId(), user.getUserId(), 10, null,
 			BlogPosting.class.getName(), new byte[0], "JSON",
 			BatchEngineTaskExecuteStatus.INITIAL.name(), null,
-			BatchEngineImportTaskConstants.IMPORT_STRATEGY_ON_ERROR_FAIL,
 			BatchEngineTaskOperation.CREATE.name(), new HashMap<>(), null);
 	}
 
@@ -77,11 +73,14 @@ public class BatchEngineImportTaskServiceTest
 		_batchEngineImportTaskService.getBatchEngineImportTask(
 			_companyAdminBatchEngineImportTask.getBatchEngineImportTaskId());
 
-		AssertUtils.assertFailure(
-			PrincipalException.class, null,
-			() -> _batchEngineImportTaskService.getBatchEngineImportTask(
+		try {
+			_batchEngineImportTaskService.getBatchEngineImportTask(
 				_defaultCompanyBatchEngineImportTask.
-					getBatchEngineImportTaskId()));
+					getBatchEngineImportTaskId());
+			Assert.fail();
+		}
+		catch (PrincipalException principalException) {
+		}
 
 		_batchEngineImportTaskService.getBatchEngineImportTask(
 			_userBatchEngineImportTask.getBatchEngineImportTaskId());
@@ -101,171 +100,36 @@ public class BatchEngineImportTaskServiceTest
 
 		UserTestUtil.setUser(user);
 
-		AssertUtils.assertFailure(
-			PrincipalException.class, null,
-			() -> _batchEngineImportTaskService.getBatchEngineImportTask(
+		try {
+			_batchEngineImportTaskService.getBatchEngineImportTask(
 				_companyAdminBatchEngineImportTask.
-					getBatchEngineImportTaskId()));
-		AssertUtils.assertFailure(
-			PrincipalException.class, null,
-			() -> _batchEngineImportTaskService.getBatchEngineImportTask(
+					getBatchEngineImportTaskId());
+			Assert.fail();
+		}
+		catch (PrincipalException principalException) {
+		}
+
+		try {
+			_batchEngineImportTaskService.getBatchEngineImportTask(
 				_defaultCompanyBatchEngineImportTask.
-					getBatchEngineImportTaskId()));
+					getBatchEngineImportTaskId());
+
+			Assert.fail();
+		}
+		catch (PrincipalException principalException) {
+		}
 
 		_batchEngineImportTaskService.getBatchEngineImportTask(
 			_userBatchEngineImportTask.getBatchEngineImportTaskId());
-	}
-
-	@Test
-	public void testGetBatchEngineImportTaskByExternalReferenceCode()
-		throws Exception {
-
-		// Company admin
-
-		UserTestUtil.setUser(companyAdminUser);
-
-		_batchEngineImportTaskService.
-			getBatchEngineImportTaskByExternalReferenceCode(
-				_companyAdminBatchEngineImportTask.getExternalReferenceCode(),
-				_companyAdminBatchEngineImportTask.getCompanyId());
-
-		AssertUtils.assertFailure(
-			PrincipalException.class, null,
-			() ->
-				_batchEngineImportTaskService.
-					getBatchEngineImportTaskByExternalReferenceCode(
-						_defaultCompanyBatchEngineImportTask.
-							getExternalReferenceCode(),
-						_defaultCompanyBatchEngineImportTask.getCompanyId()));
-
-		_batchEngineImportTaskService.
-			getBatchEngineImportTaskByExternalReferenceCode(
-				_userBatchEngineImportTask.getExternalReferenceCode(),
-				_userBatchEngineImportTask.getCompanyId());
-
-		// Omniadmin
-
-		UserTestUtil.setUser(omniadminUser);
-
-		_batchEngineImportTaskService.
-			getBatchEngineImportTaskByExternalReferenceCode(
-				_companyAdminBatchEngineImportTask.getExternalReferenceCode(),
-				_companyAdminBatchEngineImportTask.getCompanyId());
-		_batchEngineImportTaskService.
-			getBatchEngineImportTaskByExternalReferenceCode(
-				_defaultCompanyBatchEngineImportTask.getExternalReferenceCode(),
-				_defaultCompanyBatchEngineImportTask.getCompanyId());
-		_batchEngineImportTaskService.
-			getBatchEngineImportTaskByExternalReferenceCode(
-				_userBatchEngineImportTask.getExternalReferenceCode(),
-				_userBatchEngineImportTask.getCompanyId());
-
-		// User
-
-		UserTestUtil.setUser(user);
-
-		AssertUtils.assertFailure(
-			PrincipalException.class, null,
-			() ->
-				_batchEngineImportTaskService.
-					getBatchEngineImportTaskByExternalReferenceCode(
-						_companyAdminBatchEngineImportTask.
-							getExternalReferenceCode(),
-						_companyAdminBatchEngineImportTask.getCompanyId()));
-		AssertUtils.assertFailure(
-			PrincipalException.class, null,
-			() ->
-				_batchEngineImportTaskService.
-					getBatchEngineImportTaskByExternalReferenceCode(
-						_defaultCompanyBatchEngineImportTask.
-							getExternalReferenceCode(),
-						_defaultCompanyBatchEngineImportTask.getCompanyId()));
-
-		_batchEngineImportTaskService.
-			getBatchEngineImportTaskByExternalReferenceCode(
-				_userBatchEngineImportTask.getExternalReferenceCode(),
-				_userBatchEngineImportTask.getCompanyId());
-	}
-
-	@Test
-	public void testGetBatchEngineImportTasks() throws Exception {
-
-		// Company admin
-
-		UserTestUtil.setUser(companyAdminUser);
-
-		List<BatchEngineImportTask> batchEngineImportTasks =
-			_batchEngineImportTaskService.getBatchEngineImportTasks(
-				company.getCompanyId(), QueryUtil.ALL_POS, QueryUtil.ALL_POS);
-
-		Assert.assertThat(
-			batchEngineImportTasks,
-			CoreMatchers.hasItem(_companyAdminBatchEngineImportTask));
-		Assert.assertThat(
-			batchEngineImportTasks,
-			CoreMatchers.hasItem(_userBatchEngineImportTask));
-
-		AssertUtils.assertFailure(
-			PrincipalException.class, null,
-			() -> _batchEngineImportTaskService.getBatchEngineImportTasks(
-				defaultCompany.getCompanyId(), QueryUtil.ALL_POS,
-				QueryUtil.ALL_POS));
-
-		// Omniadmin
-
-		UserTestUtil.setUser(omniadminUser);
-
-		batchEngineImportTasks =
-			_batchEngineImportTaskService.getBatchEngineImportTasks(
-				company.getCompanyId(), QueryUtil.ALL_POS, QueryUtil.ALL_POS);
-
-		Assert.assertThat(
-			batchEngineImportTasks,
-			CoreMatchers.hasItem(_companyAdminBatchEngineImportTask));
-		Assert.assertThat(
-			batchEngineImportTasks,
-			CoreMatchers.hasItem(_userBatchEngineImportTask));
-
-		batchEngineImportTasks =
-			_batchEngineImportTaskService.getBatchEngineImportTasks(
-				defaultCompany.getCompanyId(), QueryUtil.ALL_POS,
-				QueryUtil.ALL_POS);
-
-		Assert.assertThat(
-			batchEngineImportTasks,
-			CoreMatchers.hasItem(_defaultCompanyBatchEngineImportTask));
-
-		// User
-
-		UserTestUtil.setUser(user);
-
-		batchEngineImportTasks =
-			_batchEngineImportTaskService.getBatchEngineImportTasks(
-				company.getCompanyId(), QueryUtil.ALL_POS, QueryUtil.ALL_POS);
-
-		Assert.assertThat(
-			batchEngineImportTasks,
-			CoreMatchers.not(
-				CoreMatchers.hasItem(_companyAdminBatchEngineImportTask)));
-		Assert.assertThat(
-			batchEngineImportTasks,
-			CoreMatchers.hasItem(_userBatchEngineImportTask));
-
-		AssertUtils.assertFailure(
-			PrincipalException.class, null,
-			() -> _batchEngineImportTaskService.getBatchEngineImportTasks(
-				defaultCompany.getCompanyId(), QueryUtil.ALL_POS,
-				QueryUtil.ALL_POS));
 	}
 
 	private BatchEngineImportTask _addBatchEngineImportTask(User user)
 		throws Exception {
 
 		return _batchEngineImportTaskLocalService.addBatchEngineImportTask(
-			null, user.getCompanyId(), user.getUserId(), 10, null,
+			user.getCompanyId(), user.getUserId(), 10, null,
 			BlogPosting.class.getName(), new byte[0], "JSON",
 			BatchEngineTaskExecuteStatus.INITIAL.name(), null,
-			BatchEngineImportTaskConstants.IMPORT_STRATEGY_ON_ERROR_FAIL,
 			BatchEngineTaskOperation.CREATE.name(), new HashMap<>(), null);
 	}
 
