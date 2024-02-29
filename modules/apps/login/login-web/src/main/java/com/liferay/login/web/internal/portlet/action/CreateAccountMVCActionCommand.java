5e11f3a0aea760e6585c230491b5506209abc1c9
ChrisKian <christopher.kian@liferay.com>
2024-02-29 11:25:59 -0800

    LPD-15723 SF

--- modules/apps/login/login-web/src/main/java/com/liferay/login/web/internal/portlet/action/CreateAccountMVCActionCommand.java
+++ modules/apps/login/login-web/src/main/java/com/liferay/login/web/internal/portlet/action/CreateAccountMVCActionCommand.java
@@ -295,6 +295,7 @@ public class CreateAccountMVCActionCommand extends BaseMVCActionCommand {
 					"emailFromName", null);
 				String emailFromAddress = portletPreferences.getValue(
 					"emailFromAddress", null);
+
 				String emailToAddress = user.getEmailAddress();
 
 				String emailParam = "emailPasswordSent";
