50c833141805dd4910402f6582646a22b0c10efb
Jeff Wu <jeffwu0724@gmail.com>
2024-03-04 10:42:46 -0800

    LPD-1948 changed to use fixed FreeMarker version based on https://freemarker.apache.org/docs/pgui_config_incompatible_improvements.html, it is not allowed to use getVersion() to set the "incompatible improvements", as of 2.3.30, doing this will be logged as an error.

--- modules/apps/portal-template/portal-template-freemarker/src/main/java/com/liferay/portal/template/freemarker/internal/FreeMarkerManager.java
+++ modules/apps/portal-template/portal-template-freemarker/src/main/java/com/liferay/portal/template/freemarker/internal/FreeMarkerManager.java
@@ -113,7 +113,7 @@ public class FreeMarkerManager extends BaseTemplateManager {
 
 		if (beansWrapper == null) {
 			BeansWrapperBuilder beansWrapperBuilder = new BeansWrapperBuilder(
-				Configuration.getVersion());
+				Configuration.VERSION_2_3_30);
 
 			beansWrapper = beansWrapperBuilder.build();
 
@@ -255,7 +255,7 @@ public class FreeMarkerManager extends BaseTemplateManager {
 			return;
 		}
 
-		_configuration = new Configuration(Configuration.getVersion());
+		_configuration = new Configuration(Configuration.VERSION_2_3_30);
 
 		try {
 			Field field = ReflectionUtil.getDeclaredField(
--- portal-impl/src/com/liferay/portal/freemarker/FreeMarkerUtil.java
+++ portal-impl/src/com/liferay/portal/freemarker/FreeMarkerUtil.java
@@ -43,10 +43,10 @@ public class FreeMarkerUtil {
 			return _configuration;
 		}
 
-		_configuration = new Configuration(Configuration.getVersion());
+		_configuration = new Configuration(Configuration.VERSION_2_3_30);
 
 		DefaultObjectWrapperBuilder defaultObjectWrapperBuilder =
-			new DefaultObjectWrapperBuilder(Configuration.getVersion());
+			new DefaultObjectWrapperBuilder(Configuration.VERSION_2_3_30);
 
 		_configuration.setObjectWrapper(defaultObjectWrapperBuilder.build());
 
