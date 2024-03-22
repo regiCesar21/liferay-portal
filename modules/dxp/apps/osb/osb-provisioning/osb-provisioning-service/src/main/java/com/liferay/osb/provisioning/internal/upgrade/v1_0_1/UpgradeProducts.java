6cdb2c3ea8bf7f8239d8143efbc1b06bfd23d253
Felipe Veloso <felipe.veloso@liferay.com>
2024-03-22 15:46:14 -0300

    ISSD-3115 Upgrade Process to update product entity

---
+++ modules/dxp/apps/osb/osb-provisioning/osb-provisioning-service/src/main/java/com/liferay/osb/provisioning/internal/upgrade/v1_0_1/UpgradeProducts.java
@@ -0,0 +1,107 @@
+/**
+ * SPDX-FileCopyrightText: (c) 2024 Liferay, Inc. https://liferay.com
+ * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
+ */
+
+package com.liferay.osb.provisioning.internal.upgrade.v1_0_1;
+
+import com.liferay.osb.koroneiki.phloem.rest.client.constants.ExternalLinkDomain;
+import com.liferay.osb.koroneiki.phloem.rest.client.constants.ExternalLinkEntityName;
+import com.liferay.osb.koroneiki.phloem.rest.client.dto.v1_0.ExternalLink;
+import com.liferay.osb.koroneiki.phloem.rest.client.dto.v1_0.Product;
+import com.liferay.osb.provisioning.koroneiki.web.service.ExternalLinkWebService;
+import com.liferay.osb.provisioning.koroneiki.web.service.ProductWebService;
+import com.liferay.petra.string.StringPool;
+import com.liferay.portal.kernel.log.Log;
+import com.liferay.portal.kernel.log.LogFactoryUtil;
+import com.liferay.portal.kernel.upgrade.UpgradeProcess;
+
+import java.util.List;
+import java.util.Map;
+
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+
+/**
+ * @author Felipe Veloso
+ */
+@Component(service = UpgradeProducts.class)
+public class UpgradeProducts extends UpgradeProcess {
+
+	public void upgradeProductName(
+		String oldProductName, String newProductName,
+		String newDisplayName, String newDisplayGroupName) {
+
+		try {
+			Product product = _productWebService.fetchProductByName(
+				oldProductName);
+
+			if (product == null) {
+				List<Product> products = _productWebService.getProducts(
+					ExternalLinkDomain.SALESFORCE,
+					ExternalLinkEntityName.SALESFORCE_PRODUCT, oldProductName,
+					1, 1);
+
+				if (!products.isEmpty()) {
+					product = products.get(0);
+				}
+				else {
+					if (_log.isInfoEnabled()) {
+						_log.info(
+							"Product with name " + oldProductName +
+								" was not found.");
+					}
+				}
+			}
+
+			if (product != null) {
+				ExternalLink[] externalLinks = product.getExternalLinks();
+
+				for (ExternalLink externalLink : externalLinks) {
+					String entityId = externalLink.getEntityId();
+
+					if (entityId.equals(oldProductName)) {
+						externalLink.setEntityId(newProductName);
+
+						_externalLinkWebService.updateExternalLink(
+							StringPool.BLANK, StringPool.BLANK,
+							externalLink.getKey(), externalLink);
+
+						break;
+					}
+				}
+
+				product.setName(newProductName);
+
+				Map<String, String> properties = product.getProperties();
+
+				properties.put("display-group-name", newDisplayGroupName);
+				properties.put("display-name", newDisplayName);
+
+				product.setProperties(properties);
+
+				_productWebService.updateProduct(
+					StringPool.BLANK, StringPool.BLANK, product.getKey(),
+					product);
+			}
+		}
+		catch (Exception exception) {
+			_log.error(exception);
+		}
+	}
+
+	@Override
+	protected void doUpgrade() throws Exception {
+	}
+
+	@Reference
+	private static final Log _log = LogFactoryUtil.getLog(
+		UpgradeProducts.class);
+
+	@Reference
+	private ExternalLinkWebService _externalLinkWebService;
+
+	@Reference
+	private ProductWebService _productWebService;
+
+}
