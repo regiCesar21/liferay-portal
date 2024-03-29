3438875d025ebcbaa37f3a993c79b487a7e598d6
Tina Tian <tina.tian@liferay.com>
2024-03-28 18:13:40 -0700

    LPD-1558 SF, remove unused method

--- modules/apps/captcha/captcha-api/src/main/java/com/liferay/captcha/simplecaptcha/SimpleCaptchaImpl.java
+++ modules/apps/captcha/captcha-api/src/main/java/com/liferay/captcha/simplecaptcha/SimpleCaptchaImpl.java
@@ -372,11 +372,6 @@ public class SimpleCaptchaImpl implements Captcha {
 		return false;
 	}
 
-	protected boolean isExceededMaxChallenges(PortletRequest portletRequest) {
-		return isExceededMaxChallenges(
-			portal.getHttpServletRequest(portletRequest));
-	}
-
 	protected void setCaptchaConfiguration(
 		CaptchaConfiguration captchaConfiguration) {
 
