c737d5df1b34a303dd25788b1ee9ae08f3822422
Tina Tian <tina.tian@liferay.com>
2024-03-28 18:16:15 -0700

    LPD-1558 SF, inline the only usage

--- modules/apps/captcha/captcha-api/src/main/java/com/liferay/captcha/simplecaptcha/SimpleCaptchaImpl.java
+++ modules/apps/captcha/captcha-api/src/main/java/com/liferay/captcha/simplecaptcha/SimpleCaptchaImpl.java
@@ -89,8 +89,17 @@ public class SimpleCaptchaImpl implements Captcha {
 
 	@Override
 	public boolean isEnabled(HttpServletRequest httpServletRequest) {
-		if (isExceededMaxChallenges(httpServletRequest)) {
-			return false;
+		if (_captchaConfiguration.maxChallenges() > 0) {
+			HttpSession httpSession = _getHttpSession(httpServletRequest);
+
+			Integer count = (Integer)httpSession.getAttribute(
+				_getHttpSessionKey(WebKeys.CAPTCHA_COUNT, httpServletRequest));
+
+			if (isExceededMaxChallenges(count)) {
+				return false;
+			}
+
+			return true;
 		}
 
 		if (_captchaConfiguration.maxChallenges() >= 0) {
@@ -345,21 +354,6 @@ public class SimpleCaptchaImpl implements Captcha {
 			_wordRenderers[i] = (WordRenderer)_getInstance(
 				wordRendererClassName);
 		}
-	}
-
-	protected boolean isExceededMaxChallenges(
-		HttpServletRequest httpServletRequest) {
-
-		if (_captchaConfiguration.maxChallenges() > 0) {
-			HttpSession httpSession = _getHttpSession(httpServletRequest);
-
-			Integer count = (Integer)httpSession.getAttribute(
-				_getHttpSessionKey(WebKeys.CAPTCHA_COUNT, httpServletRequest));
-
-			return isExceededMaxChallenges(count);
-		}
-
-		return false;
 	}
 
 	protected boolean isExceededMaxChallenges(Integer count) {
