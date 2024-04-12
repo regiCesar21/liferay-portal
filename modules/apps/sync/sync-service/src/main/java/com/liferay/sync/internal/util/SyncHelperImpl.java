d88d465907960207c037688a42c8cd7ff7ce4191
Alan Huang <alan.huang@liferay.com>
2024-04-12 15:23:41 +0800

    LPD-21824 Auto SF

--- modules/apps/sync/sync-service/src/main/java/com/liferay/sync/internal/util/SyncHelperImpl.java
+++ modules/apps/sync/sync-service/src/main/java/com/liferay/sync/internal/util/SyncHelperImpl.java
@@ -597,6 +597,8 @@ public class SyncHelperImpl implements SyncHelper {
 		syncDLObject.setCompanyId(dlFileVersion.getCompanyId());
 		syncDLObject.setUserId(dlFileVersion.getStatusByUserId());
 		syncDLObject.setUserName(dlFileVersion.getStatusByUserName());
+		syncDLObject.setCreateDate(dlFileVersion.getCreateDate());
+		syncDLObject.setModifiedDate(dlFileVersion.getModifiedDate());
 		syncDLObject.setRepositoryId(dlFileVersion.getRepositoryId());
 		syncDLObject.setParentFolderId(dlFileVersion.getFolderId());
 		syncDLObject.setTreePath(dlFileVersion.getTreePath());
@@ -608,8 +610,6 @@ public class SyncHelperImpl implements SyncHelper {
 		syncDLObject.setVersion(dlFileVersion.getVersion());
 		syncDLObject.setVersionId(dlFileVersion.getFileVersionId());
 		syncDLObject.setSize(dlFileVersion.getSize());
-		syncDLObject.setCreateDate(dlFileVersion.getCreateDate());
-		syncDLObject.setModifiedDate(dlFileVersion.getModifiedDate());
 
 		if (calculateChecksum) {
 			if (Validator.isNull(dlFileVersion.getChecksum())) {
@@ -640,6 +640,8 @@ public class SyncHelperImpl implements SyncHelper {
 		syncDLObject.setCompanyId(dlFolder.getCompanyId());
 		syncDLObject.setUserId(userId);
 		syncDLObject.setUserName(userName);
+		syncDLObject.setCreateDate(dlFolder.getCreateDate());
+		syncDLObject.setModifiedDate(dlFolder.getModifiedDate());
 		syncDLObject.setRepositoryId(dlFolder.getRepositoryId());
 		syncDLObject.setParentFolderId(dlFolder.getParentFolderId());
 		syncDLObject.setTreePath(dlFolder.getTreePath());
@@ -649,8 +651,6 @@ public class SyncHelperImpl implements SyncHelper {
 		syncDLObject.setType(SyncDLObjectConstants.TYPE_FOLDER);
 		syncDLObject.setTypePK(dlFolder.getFolderId());
 		syncDLObject.setTypeUuid(dlFolder.getUuid());
-		syncDLObject.setCreateDate(dlFolder.getCreateDate());
-		syncDLObject.setModifiedDate(dlFolder.getModifiedDate());
 
 		return syncDLObject;
 	}
