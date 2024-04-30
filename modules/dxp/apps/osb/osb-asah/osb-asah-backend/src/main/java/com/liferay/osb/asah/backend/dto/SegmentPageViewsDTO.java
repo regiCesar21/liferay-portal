6557d37990580019d269564521edb5d25316f311
Ivica Čardić <ivica.cardic@gmail.com>
2024-04-30 17:03:54 +0200

    LPD-24077 - Add new graphql query to fetch views count for each segment

---
+++ modules/dxp/apps/osb/osb-asah/osb-asah-backend/src/main/java/com/liferay/osb/asah/backend/dto/SegmentPageViewsDTO.java
@@ -0,0 +1,35 @@
+/**
+ * SPDX-FileCopyrightText: (c) 2000 Liferay, Inc. https://liferay.com
+ * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
+ */
+
+package com.liferay.osb.asah.backend.dto;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+
+import com.liferay.osb.asah.backend.graphql.annotation.GraphQLType;
+
+/**
+ * @author Ivica Cardic
+ */
+@GraphQLType("SegmentPageViews")
+@JsonInclude(JsonInclude.Include.NON_NULL)
+public class SegmentPageViewsDTO {
+
+	public SegmentPageViewsDTO(String segmentId, long views) {
+		_segmentId = segmentId;
+		_views = views;
+	}
+
+	public String getSegmentId() {
+		return _segmentId;
+	}
+
+	public long getViews() {
+		return _views;
+	}
+
+	private final String _segmentId;
+	private final long _views;
+
+}
---
+++ modules/dxp/apps/osb/osb-asah/osb-asah-backend/src/main/java/com/liferay/osb/asah/backend/graphql/schema/SegmentPageViewsDataFetcher.java
@@ -0,0 +1,98 @@
+/**
+ * SPDX-FileCopyrightText: (c) 2000 Liferay, Inc. https://liferay.com
+ * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
+ */
+
+package com.liferay.osb.asah.backend.graphql.schema;
+
+import com.liferay.osb.asah.backend.dog.PagePathDog;
+import com.liferay.osb.asah.backend.dog.helper.SearchQueryContext;
+import com.liferay.osb.asah.backend.dto.SegmentPageViewsDTO;
+import com.liferay.osb.asah.backend.graphql.annotation.GraphQLTypeWiring;
+import com.liferay.osb.asah.backend.model.AdjacentPageViewsMetric;
+
+import graphql.schema.DataFetchingEnvironment;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+import org.apache.commons.collections.CollectionUtils;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+/**
+ * @author Ivica Cardic
+ */
+@Component
+@GraphQLTypeWiring(fieldName = "segmentPageViews", typeName = "QueryType")
+public class SegmentPageViewsDataFetcher
+	extends BaseDataFetcher<List<SegmentPageViewsDTO>> {
+
+	@Override
+	public List<SegmentPageViewsDTO> get(
+		DataFetchingEnvironment dataFetchingEnvironment,
+		SearchQueryContext searchQueryContext) {
+
+		List<SegmentPageViewsDTO> segmentPageViewsDTOs = new ArrayList<>();
+
+		List<Long> segmentIds = _getSegmentIds(dataFetchingEnvironment);
+
+		for (Long segmentId : segmentIds) {
+			Set<AdjacentPageViewsMetric> adjacentPagesViewsMetrics =
+				_pagePathDog.getAdjacentPagesViewsMetric(
+					searchQueryContext.getCanonicalUrl(),
+					searchQueryContext.getChannelIdAsLong(), segmentId,
+					searchQueryContext.getTimeRange(),
+					searchQueryContext.getTitle());
+
+			segmentPageViewsDTOs.add(
+				new SegmentPageViewsDTO(
+					String.valueOf(segmentId),
+					_getViews(adjacentPagesViewsMetrics)));
+		}
+
+		return segmentPageViewsDTOs;
+	}
+
+	private List<Long> _getSegmentIds(
+		DataFetchingEnvironment dataFetchingEnvironment) {
+
+		List<String> segmentIds = dataFetchingEnvironment.getArgument(
+			"segmentIds");
+
+		if (!CollectionUtils.isEmpty(segmentIds)) {
+			return segmentIds.stream(
+			).map(
+				Long::valueOf
+			).collect(
+				Collectors.toList()
+			);
+		}
+
+		return Collections.emptyList();
+	}
+
+	private long _getViews(
+		Set<AdjacentPageViewsMetric> adjacentPagesViewsMetrics) {
+
+		long views = 0;
+
+		for (AdjacentPageViewsMetric adjacentPageViewsMetric :
+				adjacentPagesViewsMetrics) {
+
+			if (adjacentPageViewsMetric.isPrevious()) {
+				views += adjacentPageViewsMetric.getViewsAsLong();
+			}
+		}
+
+		return views;
+	}
+
+	@Autowired
+	private PagePathDog _pagePathDog;
+
+}
