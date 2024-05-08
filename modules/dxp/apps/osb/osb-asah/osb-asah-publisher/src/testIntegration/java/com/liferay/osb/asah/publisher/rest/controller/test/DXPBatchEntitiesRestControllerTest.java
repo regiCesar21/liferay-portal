b75368ece0b823aac5a24553c4f9226cb4a3f40c
Riccardo Ferrari <riccardo.ferrari@liferay.com>
2024-05-08 14:44:39 +0200

    LPD-24827 osb-asah-publisher Update integration tests

--- modules/dxp/apps/osb/osb-asah/osb-asah-publisher/src/testIntegration/java/com/liferay/osb/asah/publisher/rest/controller/test/DXPBatchEntitiesRestControllerTest.java
+++ modules/dxp/apps/osb/osb-asah/osb-asah-publisher/src/testIntegration/java/com/liferay/osb/asah/publisher/rest/controller/test/DXPBatchEntitiesRestControllerTest.java
@@ -7,7 +7,9 @@ package com.liferay.osb.asah.publisher.rest.controller.test;
 
 import com.liferay.osb.asah.common.constants.HeaderConstants;
 import com.liferay.osb.asah.common.date.DateUtil;
+import com.liferay.osb.asah.common.entity.DataSource;
 import com.liferay.osb.asah.common.json.JSONUtil;
+import com.liferay.osb.asah.common.repository.DataSourceRepository;
 import com.liferay.osb.asah.common.storage.Storage;
 import com.liferay.osb.asah.common.storage.StorageConfiguration;
 import com.liferay.osb.asah.common.storage.StorageFactory;
@@ -27,6 +29,7 @@ import java.util.Date;
 
 import org.assertj.core.api.Assertions;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import org.mockito.ArgumentMatchers;
@@ -57,6 +60,38 @@ import org.springframework.web.util.UriComponentsBuilder;
 )
 public class DXPBatchEntitiesRestControllerTest
 	implements OSBAsahPublisherSpringTestContext {
+
+	@BeforeEach
+	public void setUp() {
+		DataSource dataSource1 = new DataSource("Liferay Italy");
+
+		dataSource1.setContactsSelected(true);
+		dataSource1.setCredentialType("Token Authentication");
+		dataSource1.setFaroBackendSecuritySignature(
+			"faroBackendSecuritySignature");
+		dataSource1.setId(123L);
+		dataSource1.setIsNew(Boolean.TRUE);
+		dataSource1.setProviderType("LIFERAY");
+		dataSource1.setState("READY");
+		dataSource1.setStatus("STARTED");
+		dataSource1.setURL("");
+
+		_dataSource1 = _dataSourceRepository.save(dataSource1);
+
+		DataSource dataSource2 = new DataSource("Liferay Brazil");
+
+		dataSource2.setCredentialType("Token Authentication");
+		dataSource2.setFaroBackendSecuritySignature(
+			"faroBackendSecuritySignature");
+		dataSource2.setId(456L);
+		dataSource2.setIsNew(Boolean.TRUE);
+		dataSource2.setProviderType("LIFERAY");
+		dataSource2.setState("READY");
+		dataSource2.setStatus("STARTED");
+		dataSource2.setURL("");
+
+		_dataSource2 = _dataSourceRepository.save(dataSource2);
+	}
 
 	@Test
 	public void testGetNoContent() {
@@ -203,15 +238,20 @@ public class DXPBatchEntitiesRestControllerTest
 	}
 
 	@Test
-	public void testPost() throws Exception {
+	public void testPostContactsSelected() throws Exception {
 		MultipartBodyBuilder multipartBodyBuilder = new MultipartBodyBuilder();
 
-		multipartBodyBuilder.part("file", _getFileSystemResource());
+		multipartBodyBuilder.part(
+			"file", _getFileSystemResource()
+		).filename(
+			"com.liferay.analytics.dxp.entity.rest.dto.v1_0.DXPEntity"
+		);
 
 		HttpHeaders httpHeaders = new HttpHeaders();
 
 		httpHeaders.add(
-			HeaderConstants.DATA_SOURCE_ID, RandomTestUtil.randomId());
+			HeaderConstants.DATA_SOURCE_ID,
+			String.valueOf(_dataSource1.getId()));
 		httpHeaders.add(HeaderConstants.PROJECT_ID, "test");
 		httpHeaders.setContentType(MediaType.MULTIPART_FORM_DATA);
 
@@ -224,6 +264,36 @@ public class DXPBatchEntitiesRestControllerTest
 			responseEntity.getStatusCode()
 		).isEqualTo(
 			HttpStatus.valueOf(200)
+		);
+	}
+
+	@Test
+	public void testPostNoContactsSelected() throws Exception {
+		MultipartBodyBuilder multipartBodyBuilder = new MultipartBodyBuilder();
+
+		multipartBodyBuilder.part(
+			"file", _getFileSystemResource()
+		).filename(
+			"com.liferay.analytics.dxp.entity.rest.dto.v1_0.DXPEntity"
+		);
+
+		HttpHeaders httpHeaders = new HttpHeaders();
+
+		httpHeaders.add(
+			HeaderConstants.DATA_SOURCE_ID,
+			String.valueOf(_dataSource2.getId()));
+		httpHeaders.add(HeaderConstants.PROJECT_ID, "test");
+		httpHeaders.setContentType(MediaType.MULTIPART_FORM_DATA);
+
+		ResponseEntity<Resource> responseEntity = _testRestTemplate.exchange(
+			"/dxp-batch-entities", HttpMethod.POST,
+			new HttpEntity<>(multipartBodyBuilder.build(), httpHeaders),
+			Resource.class);
+
+		Assertions.assertThat(
+			responseEntity.getStatusCode()
+		).isEqualTo(
+			HttpStatus.valueOf(400)
 		);
 	}
 
@@ -289,6 +359,12 @@ public class DXPBatchEntitiesRestControllerTest
 
 	private static final DateTimeFormatter _dateTimeFormatter =
 		DateTimeFormatter.ofPattern("EEE, dd MMM yyyy HH:mm:ss zzz");
+
+	private DataSource _dataSource1;
+	private DataSource _dataSource2;
+
+	@Autowired
+	private DataSourceRepository _dataSourceRepository;
 
 	@LocalServerPort
 	private int _serverPort;
