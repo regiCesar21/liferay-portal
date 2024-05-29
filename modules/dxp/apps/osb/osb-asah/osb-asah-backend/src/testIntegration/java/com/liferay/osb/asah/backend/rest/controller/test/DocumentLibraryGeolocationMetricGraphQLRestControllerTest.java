3e13307cd7a9318bfe0b59343d7cb95704e17fcf
Rachael Koestartyo <rachael.koestartyo@liferay.com>
2024-05-29 14:52:34 -0700

    LPD-27133 add integration test for Documents and Media downloads by location card

---
+++ modules/dxp/apps/osb/osb-asah/osb-asah-backend/src/testIntegration/java/com/liferay/osb/asah/backend/rest/controller/test/DocumentLibraryGeolocationMetricGraphQLRestControllerTest.java
@@ -0,0 +1,45 @@
+/**
+ * SPDX-FileCopyrightText: (c) 2024 Liferay, Inc. https://liferay.com
+ * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
+ */
+
+package com.liferay.osb.asah.backend.rest.controller.test;
+
+import com.liferay.osb.asah.test.util.annotation.BQSQLResource;
+
+import org.json.JSONObject;
+
+import org.skyscreamer.jsonassert.JSONAssert;
+
+/**
+ * @author Rachael Koestartyo
+ */
+@BQSQLResource(
+	resourcePath = "document_library_geolocation_metric_graphql_rest_controller_test.sql"
+)
+public class DocumentLibraryGeolocationMetricGraphQLRestControllerTest
+	extends BaseGraphQLRestControllerTestCase {
+
+	@Override
+	public String getBodyPath() {
+		return "document_library_geolocation_metric_body.json";
+	}
+
+	@Override
+	public String getExpectedResultPath() {
+		return "document_library_geolocation_metric_expected_result.json";
+	}
+
+	@Override
+	public String getQueryPath() {
+		return "document_library_geolocation_metric_query.graphql";
+	}
+
+	@Override
+	protected void assertJSONObject(
+		JSONObject expectedJSONObject, JSONObject jsonObject) {
+
+		JSONAssert.assertEquals(expectedJSONObject, jsonObject, true);
+	}
+
+}
