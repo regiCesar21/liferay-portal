ead7023e294a0842999be72e61450f6ce0cb2ca6
Ivica Čardić <ivica.cardic@gmail.com>
2024-06-03 20:20:22 +0200

    LPD-23686 - Allow /actuator requests pass security filter check

--- modules/dxp/apps/osb/osb-asah/osb-asah-backend/src/main/java/com/liferay/osb/asah/backend/servlet/filter/SecurityOncePerRequestFilter.java
+++ modules/dxp/apps/osb/osb-asah/osb-asah-backend/src/main/java/com/liferay/osb/asah/backend/servlet/filter/SecurityOncePerRequestFilter.java
@@ -15,6 +15,7 @@ import org.apache.commons.lang3.StringUtils;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.http.HttpMethod;
 import org.springframework.stereotype.Component;
 
 /**
@@ -27,6 +28,15 @@ public class SecurityOncePerRequestFilter
 
 	@Override
 	protected boolean isInvalidRequest(HttpServletRequest httpServletRequest) {
+		String method = httpServletRequest.getMethod();
+		String requestURI = httpServletRequest.getRequestURI();
+
+		if (method.equals(HttpMethod.GET.name()) &&
+			requestURI.startsWith("/actuator")) {
+
+			return false;
+		}
+
 		String faroBackendSecuritySignature = httpServletRequest.getHeader(
 			HeaderConstants.FARO_BACKEND_SECURITY_SIGNATURE);
 
