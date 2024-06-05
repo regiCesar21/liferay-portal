4fcdc357f97f8c4fb423783eda51d297e9d40cb4
Sillas Cavalcanti <sillas.cavalcanti@liferay.com>
2024-06-05 15:35:00 -0300

    LPD-27629 Create integration test get a list of visit by day and time

---
+++ modules/dxp/apps/osb/osb-asah/osb-asah-backend/src/testIntegration/java/com/liferay/osb/asah/backend/rest/controller/test/SiteVisitorHeatMapGraphQLRestControllerTest.java
@@ -0,0 +1,34 @@
+/**
+ * SPDX-FileCopyrightText: (c) 2024 Liferay, Inc. https://liferay.com
+ * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
+ */
+
+package com.liferay.osb.asah.backend.rest.controller.test;
+
+import com.liferay.osb.asah.test.util.annotation.BQSQLResource;
+
+/**
+ * @author Sillas Cavalcanti
+ */
+@BQSQLResource(
+	resourcePath = "site_visitor_heat_map_graphql_rest_controller_test.sql"
+)
+public class SiteVisitorHeatMapGraphQLRestControllerTest
+	extends BaseGraphQLRestControllerTestCase {
+
+	@Override
+	public String getBodyPath() {
+		return "site_visitor_heat_map_body.json";
+	}
+
+	@Override
+	public String getExpectedResultPath() {
+		return "site_visitor_heat_map_expected_result.json";
+	}
+
+	@Override
+	public String getQueryPath() {
+		return "site_visitor_heat_map_query.graphql";
+	}
+
+}
