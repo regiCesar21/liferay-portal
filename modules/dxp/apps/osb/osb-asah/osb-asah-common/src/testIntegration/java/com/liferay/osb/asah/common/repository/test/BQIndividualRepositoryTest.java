edacab5aceec7d2c233ebf0764c2189606458369
Leslie Wong <8095751+lesliewong92@users.noreply.github.com>
2024-06-21 15:29:04 -0700

    LPD-28874 Add tests

--- modules/dxp/apps/osb/osb-asah/osb-asah-common/src/testIntegration/java/com/liferay/osb/asah/common/repository/test/BQIndividualRepositoryTest.java
+++ modules/dxp/apps/osb/osb-asah/osb-asah-common/src/testIntegration/java/com/liferay/osb/asah/common/repository/test/BQIndividualRepositoryTest.java
@@ -11,6 +11,7 @@ import com.liferay.osb.asah.common.date.dog.TimeZoneDog;
 import com.liferay.osb.asah.common.date.dog.util.TimeZoneDogUtil;
 import com.liferay.osb.asah.common.model.Distribution;
 import com.liferay.osb.asah.common.model.Individual;
+import com.liferay.osb.asah.common.model.ReportIndividual;
 import com.liferay.osb.asah.common.repository.BQIndividualRepository;
 import com.liferay.osb.asah.test.util.annotation.BQSQLResource;
 import com.liferay.osb.asah.test.util.configuration.JDBCTestConfiguration;
@@ -22,6 +23,7 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.Date;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Optional;
 
@@ -518,6 +520,53 @@ public class BQIndividualRepositoryTest
 				11L, "jobTitle", null));
 	}
 
+	@BQSQLResource(resourcePath = "test_search_report_individuals.sql")
+	@Test
+	public void testCountReportIndividuals() {
+		Assertions.assertEquals(
+			15L,
+			_bqIndividualRepository.countReportIndividuals(null, null, null));
+		Assertions.assertEquals(
+			1L,
+			_bqIndividualRepository.countReportIndividuals(1L, "3", 11111L));
+		Assertions.assertEquals(
+			0L,
+			_bqIndividualRepository.countReportIndividuals(2L, "3", 11111L));
+		Assertions.assertEquals(
+			1, _bqIndividualRepository.countReportIndividuals(2L, "1", 11111L));
+	}
+
+	@BQSQLResource(resourcePath = "test_search_report_individuals.sql")
+	@Test
+	public void testCountReportIndividualsWithChannelId() {
+		Assertions.assertEquals(
+			8L, _bqIndividualRepository.countReportIndividuals(1L, null, null));
+		Assertions.assertEquals(
+			8L, _bqIndividualRepository.countReportIndividuals(2L, null, null));
+	}
+
+	@BQSQLResource(resourcePath = "test_search_report_individuals.sql")
+	@Test
+	public void testCountReportIndividualsWithQuery() {
+		Assertions.assertEquals(
+			2L,
+			_bqIndividualRepository.countReportIndividuals(null, "5", null));
+		Assertions.assertEquals(
+			1L,
+			_bqIndividualRepository.countReportIndividuals(null, "15", null));
+	}
+
+	@BQSQLResource(resourcePath = "test_search_report_individuals.sql")
+	@Test
+	public void testCountReportIndividualsWithSegmentId() {
+		Assertions.assertEquals(
+			2L,
+			_bqIndividualRepository.countReportIndividuals(null, null, 11111L));
+		Assertions.assertEquals(
+			1L,
+			_bqIndividualRepository.countReportIndividuals(null, null, 33333L));
+	}
+
 	@BQSQLResource(
 		resourcePath = "test_bq_individual_repository_with_suppression.sql"
 	)
@@ -554,6 +603,34 @@ public class BQIndividualRepositoryTest
 			"47ff64395860b1d498241d907069f649b98c198a95b3ba5303b87094058590c3");
 
 		Assertions.assertFalse(individualOptional.isPresent());
+	}
+
+	@BQSQLResource(resourcePath = "test_search_report_individuals.sql")
+	@Test
+	public void testFindReportIndividualById() {
+		Optional<ReportIndividual> reportIndividualOptional =
+			_bqIndividualRepository.findReportIndividualById("1");
+
+		Assertions.assertTrue(reportIndividualOptional.isPresent());
+
+		ReportIndividual reportIndividual = new ReportIndividual();
+
+		reportIndividual.setId("1");
+		reportIndividual.setSegmentIds(
+			new HashSet<>(Arrays.asList(11111L, 22222L, 33333L)));
+
+		Assertions.assertEquals(
+			reportIndividual, reportIndividualOptional.get());
+
+		reportIndividualOptional =
+			_bqIndividualRepository.findReportIndividualById("2");
+
+		Assertions.assertFalse(reportIndividualOptional.isPresent());
+
+		reportIndividualOptional =
+			_bqIndividualRepository.findReportIndividualById("abc");
+
+		Assertions.assertFalse(reportIndividualOptional.isPresent());
 	}
 
 	@BQSQLResource(
@@ -661,6 +738,117 @@ public class BQIndividualRepositoryTest
 			Arrays.asList("Tester"),
 			_bqIndividualRepository.searchIndividualFieldValuesDemographics(
 				11L, "jobTitle", null, pageRequest));
+	}
+
+	@BQSQLResource(resourcePath = "test_search_report_individuals.sql")
+	@Test
+	public void testSearchReportIndividuals() {
+		ReportIndividual reportIndividual1 = new ReportIndividual();
+
+		reportIndividual1.setId("3");
+		reportIndividual1.setSegmentIds(Collections.singleton(11111L));
+
+		ReportIndividual reportIndividual2 = new ReportIndividual();
+
+		reportIndividual2.setId("1");
+		reportIndividual2.setSegmentIds(Collections.singleton(22222L));
+
+		Assertions.assertEquals(
+			Collections.singletonList(reportIndividual1),
+			_bqIndividualRepository.searchReportIndividuals(
+				1L, PageRequest.of(0, 5), "3", 11111L));
+		Assertions.assertEquals(
+			Collections.emptyList(),
+			_bqIndividualRepository.searchReportIndividuals(
+				2L, PageRequest.of(0, 5), "3", 11111L));
+		Assertions.assertEquals(
+			Collections.singletonList(reportIndividual2),
+			_bqIndividualRepository.searchReportIndividuals(
+				2L, PageRequest.of(0, 5), "1", 11111L));
+	}
+
+	@BQSQLResource(resourcePath = "test_search_report_individuals.sql")
+	@Test
+	public void testSearchReportIndividualsWithChannelId() {
+		ReportIndividual reportIndividual1 = new ReportIndividual();
+
+		reportIndividual1.setId("1");
+		reportIndividual1.setSegmentIds(
+			new HashSet<>(Arrays.asList(11111L, 33333L)));
+
+		ReportIndividual reportIndividual2 = new ReportIndividual();
+
+		reportIndividual2.setId("3");
+		reportIndividual2.setSegmentIds(Collections.singleton(11111L));
+
+		ReportIndividual reportIndividual3 = new ReportIndividual();
+
+		reportIndividual3.setId("5");
+		reportIndividual3.setSegmentIds(Collections.emptySet());
+
+		ReportIndividual reportIndividual4 = new ReportIndividual();
+
+		reportIndividual4.setId("11");
+		reportIndividual4.setSegmentIds(Collections.emptySet());
+
+		ReportIndividual reportIndividual5 = new ReportIndividual();
+
+		reportIndividual5.setId("12");
+		reportIndividual5.setSegmentIds(Collections.emptySet());
+
+		Assertions.assertEquals(
+			Arrays.asList(
+				reportIndividual1, reportIndividual2, reportIndividual3,
+				reportIndividual4, reportIndividual5),
+			_bqIndividualRepository.searchReportIndividuals(
+				1L, PageRequest.of(0, 5), null, null));
+	}
+
+	@BQSQLResource(resourcePath = "test_search_report_individuals.sql")
+	@Test
+	public void testSearchReportIndividualsWithQuery() {
+		ReportIndividual reportIndividual1 = new ReportIndividual();
+
+		reportIndividual1.setId("5");
+		reportIndividual1.setSegmentIds(Collections.emptySet());
+
+		ReportIndividual reportIndividual2 = new ReportIndividual();
+
+		reportIndividual2.setId("15");
+		reportIndividual2.setSegmentIds(Collections.emptySet());
+
+		Assertions.assertEquals(
+			Arrays.asList(reportIndividual1, reportIndividual2),
+			_bqIndividualRepository.searchReportIndividuals(
+				null, PageRequest.of(0, 2), "5", null));
+		Assertions.assertEquals(
+			Collections.singletonList(reportIndividual2),
+			_bqIndividualRepository.searchReportIndividuals(
+				null, PageRequest.of(0, 2), "15", null));
+	}
+
+	@BQSQLResource(resourcePath = "test_search_report_individuals.sql")
+	@Test
+	public void testSearchReportIndividualsWithSegmentId() {
+		ReportIndividual reportIndividual1 = new ReportIndividual();
+
+		reportIndividual1.setId("1");
+		reportIndividual1.setSegmentIds(
+			new HashSet<>(Arrays.asList(11111L, 22222L, 33333L)));
+
+		ReportIndividual reportIndividual2 = new ReportIndividual();
+
+		reportIndividual2.setId("3");
+		reportIndividual2.setSegmentIds(Collections.singleton(11111L));
+
+		Assertions.assertEquals(
+			Collections.singletonList(reportIndividual1),
+			_bqIndividualRepository.searchReportIndividuals(
+				null, PageRequest.of(0, 2), null, 33333L));
+		Assertions.assertEquals(
+			Arrays.asList(reportIndividual1, reportIndividual2),
+			_bqIndividualRepository.searchReportIndividuals(
+				null, PageRequest.of(0, 2), null, 11111L));
 	}
 
 	private static final Long _SEGMENT_ID = 11L;
