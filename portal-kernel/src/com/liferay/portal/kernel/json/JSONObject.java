1422aec6e5458fbc3066e0a0937a4a643f29accd
Jaime Leon <jaime.leon@liferay.com>
2024-06-26 10:39:52 +0200

    LPD-29423 Backwards compat

--- portal-kernel/src/com/liferay/portal/kernel/json/JSONObject.java
+++ portal-kernel/src/com/liferay/portal/kernel/json/JSONObject.java
@@ -5,6 +5,8 @@
 
 package com.liferay.portal.kernel.json;
 
+import com.liferay.petra.function.UnsafeSupplier;
+
 import java.io.Externalizable;
 import java.io.Writer;
 
@@ -77,6 +79,9 @@ public interface JSONObject extends Externalizable, JSONSerializable {
 	public JSONObject put(String key, Object value);
 
 	public JSONObject put(String key, String value);
+
+	public JSONObject put(
+		String key, UnsafeSupplier<Object, Exception> valueUnsafeSupplier);
 
 	public JSONObject putException(Exception exception);
 
