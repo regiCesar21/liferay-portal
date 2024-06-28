d51a8f57fe1e440927d4432cb13eb920d75a2fea
Lourdes Fernández Besada <lourdes.fernandez@liferay.com>
2024-06-28 09:54:08 +0200

    LPD-29872 Adds asserts to ensure the theme inheritance

--- modules/apps/layout/layout-test/src/testIntegration/java/com/liferay/layout/service/test/LayoutImplTest.java
+++ modules/apps/layout/layout-test/src/testIntegration/java/com/liferay/layout/service/test/LayoutImplTest.java
@@ -324,6 +324,12 @@ public class LayoutImplTest {
 
 		_assertThemeId(layout, "admin_WAR_admintheme");
 
+		layout = _layoutLocalService.updateLookAndFeel(
+			_group.getGroupId(), layout.isPrivateLayout(), layout.getLayoutId(),
+			"classic_WAR_classictheme", "01", StringPool.BLANK);
+
+		_assertThemeId(layout, "classic_WAR_classictheme");
+
 		LayoutPageTemplateEntry masterLayoutPageTemplateEntry =
 			_layoutPageTemplateEntryLocalService.addLayoutPageTemplateEntry(
 				TestPropsValues.getUserId(), _group.getGroupId(), 0,
@@ -337,20 +343,27 @@ public class LayoutImplTest {
 
 		masterLayout = _layoutLocalService.updateLookAndFeel(
 			masterLayout.getGroupId(), masterLayout.isPrivateLayout(),
-			masterLayout.getLayoutId(), "minium_WAR_miniumtheme", "01",
+			masterLayout.getLayoutId(), StringPool.BLANK, StringPool.BLANK,
 			StringPool.BLANK);
 
 		layout = _layoutLocalService.updateMasterLayoutPlid(
 			layout.getGroupId(), layout.isPrivateLayout(), layout.getLayoutId(),
 			masterLayout.getPlid());
 
+		_assertThemeId(layout, "admin_WAR_admintheme");
+
+		_layoutLocalService.updateLookAndFeel(
+			masterLayout.getGroupId(), masterLayout.isPrivateLayout(),
+			masterLayout.getLayoutId(), "minium_WAR_miniumtheme", "01",
+			StringPool.BLANK);
+
 		_assertThemeId(layout, "minium_WAR_miniumtheme");
 
 		layout = _layoutLocalService.updateLookAndFeel(
 			_group.getGroupId(), layout.isPrivateLayout(), layout.getLayoutId(),
-			"classic_WAR_classictheme", "01", StringPool.BLANK);
+			"admin_WAR_admintheme", "01", StringPool.BLANK);
 
-		_assertThemeId(layout, "dialect_WAR_dialecttheme");
+		_assertThemeId(layout, "minium_WAR_miniumtheme");
 	}
 
 	private void _assertThemeId(Layout layout, String themeId)
