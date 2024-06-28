8708ca37a792e8d65e94d51d105050c71e2e67d3
Lourdes Fernández Besada <lourdes.fernandez@liferay.com>
2024-06-28 08:46:14 +0200

    LPD-29872 SF buy space

--- portal-impl/src/com/liferay/portal/model/impl/LayoutImpl.java
+++ portal-impl/src/com/liferay/portal/model/impl/LayoutImpl.java
@@ -759,20 +759,20 @@ public class LayoutImpl extends LayoutBaseImpl {
 	 */
 	@Override
 	public Theme getTheme() throws PortalException {
-		if (!isInheritLookAndFeel()) {
-			Layout masterLayout = _getMasterLayout();
+		if (isInheritLookAndFeel()) {
+			LayoutSet layoutSet = getLayoutSet();
 
-			if (masterLayout != null) {
-				return ThemeLocalServiceUtil.getTheme(
-					masterLayout.getCompanyId(), masterLayout.getThemeId());
-			}
-
-			return ThemeLocalServiceUtil.getTheme(getCompanyId(), getThemeId());
+			return layoutSet.getTheme();
 		}
 
-		LayoutSet layoutSet = getLayoutSet();
+		Layout masterLayout = _getMasterLayout();
 
-		return layoutSet.getTheme();
+		if (masterLayout != null) {
+			return ThemeLocalServiceUtil.getTheme(
+				masterLayout.getCompanyId(), masterLayout.getThemeId());
+		}
+
+		return ThemeLocalServiceUtil.getTheme(getCompanyId(), getThemeId());
 	}
 
 	@Override
