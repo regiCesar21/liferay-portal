be988c09b7bb193bdba078fd49fe99549ab637e6
Pedro Leite <pedro.leite@liferay.com>
2024-07-24 18:47:32 -0300

    LPD-16334 Test permissions for get methods of the KaleoDefinitionService to ensure only omniadmin and company admin can see kaleoDefinitions

--- modules/apps/portal-workflow/portal-workflow-kaleo-test/src/testIntegration/java/com/liferay/portal/workflow/kaleo/service/test/KaleoDefinitionServiceImplTest.java
+++ modules/apps/portal-workflow/portal-workflow-kaleo-test/src/testIntegration/java/com/liferay/portal/workflow/kaleo/service/test/KaleoDefinitionServiceImplTest.java
@@ -9,6 +9,7 @@ import com.liferay.arquillian.extension.junit.bridge.junit.Arquillian;
 import com.liferay.petra.string.StringBundler;
 import com.liferay.petra.string.StringPool;
 import com.liferay.portal.configuration.test.util.ConfigurationTestUtil;
+import com.liferay.portal.kernel.dao.orm.QueryUtil;
 import com.liferay.portal.kernel.model.Company;
 import com.liferay.portal.kernel.model.User;
 import com.liferay.portal.kernel.security.auth.PrincipalException;
@@ -17,23 +18,33 @@ import com.liferay.portal.kernel.security.permission.PermissionChecker;
 import com.liferay.portal.kernel.security.permission.PermissionCheckerFactoryUtil;
 import com.liferay.portal.kernel.security.permission.PermissionThreadLocal;
 import com.liferay.portal.kernel.service.ServiceContext;
+import com.liferay.portal.kernel.test.AssertUtils;
+import com.liferay.portal.kernel.test.randomizerbumpers.NumericStringRandomizerBumper;
+import com.liferay.portal.kernel.test.randomizerbumpers.UniqueStringRandomizerBumper;
 import com.liferay.portal.kernel.test.rule.AggregateTestRule;
 import com.liferay.portal.kernel.test.rule.DataGuard;
 import com.liferay.portal.kernel.test.util.CompanyTestUtil;
 import com.liferay.portal.kernel.test.util.RandomTestUtil;
 import com.liferay.portal.kernel.test.util.ServiceContextTestUtil;
+import com.liferay.portal.kernel.test.util.TestPropsValues;
 import com.liferay.portal.kernel.test.util.UserTestUtil;
 import com.liferay.portal.kernel.util.HashMapDictionaryBuilder;
+import com.liferay.portal.kernel.util.LocaleUtil;
 import com.liferay.portal.kernel.util.StringUtil;
 import com.liferay.portal.kernel.workflow.WorkflowConstants;
 import com.liferay.portal.test.rule.Inject;
 import com.liferay.portal.test.rule.LiferayIntegrationTestRule;
 import com.liferay.portal.test.rule.PermissionCheckerMethodTestRule;
 import com.liferay.portal.workflow.configuration.WorkflowDefinitionConfiguration;
+import com.liferay.portal.workflow.constants.WorkflowDefinitionConstants;
 import com.liferay.portal.workflow.kaleo.model.KaleoDefinition;
+import com.liferay.portal.workflow.kaleo.service.KaleoDefinitionLocalService;
 import com.liferay.portal.workflow.kaleo.service.KaleoDefinitionService;
+import com.liferay.portal.workflow.kaleo.service.KaleoDefinitionVersionLocalService;
 
 import java.io.InputStream;
+
+import java.util.Collections;
 
 import org.junit.After;
 import org.junit.Assert;
@@ -100,23 +111,13 @@ public class KaleoDefinitionServiceImplTest {
 
 		_setUpPermissionThreadLocal(_companyAdminUser);
 
-		try {
-			_kaleoDefinitionService.addKaleoDefinition(
-				RandomTestUtil.randomString(), RandomTestUtil.randomString(),
-				RandomTestUtil.randomString(),
-				_read("legal-marketing-definition.xml"), "company", 1,
-				_serviceContext);
-
-			Assert.fail();
-		}
-		catch (PrincipalException principalException) {
-			Assert.assertEquals(
-				StringBundler.concat(
-					"User ", _companyAdminUser.getUserId(), " must have ",
-					WorkflowConstants.RESOURCE_NAME,
-					",ADD_DEFINITION permission for null "),
-				principalException.getMessage());
-		}
+		AssertUtils.assertFailure(
+			PrincipalException.MustHavePermission.class,
+			StringBundler.concat(
+				"User ", _companyAdminUser.getUserId(), " must have ",
+				WorkflowConstants.RESOURCE_NAME, ",ADD_DEFINITION permission ",
+				"for null "),
+			this::_addKaleoDefinition);
 
 		// Administrator with "company.administrator.can.publish" enabled
 
@@ -126,12 +127,59 @@ public class KaleoDefinitionServiceImplTest {
 				"company.administrator.can.publish", true
 			).build());
 
-		Assert.assertNotNull(
-			_kaleoDefinitionService.addKaleoDefinition(
-				RandomTestUtil.randomString(), RandomTestUtil.randomString(),
-				RandomTestUtil.randomString(),
-				_read("legal-marketing-definition.xml"), "company", 1,
-				_serviceContext));
+		Assert.assertNotNull(_addKaleoDefinition());
+	}
+
+	@Test
+	public void testGetKaleoDefinition() throws Exception {
+		KaleoDefinition kaleoDefinition = _addKaleoDefinition();
+
+		User user = _addUser();
+
+		_setUpPermissionThreadLocal(user);
+
+		AssertUtils.assertFailure(
+			PrincipalException.MustBeCompanyAdmin.class,
+			StringBundler.concat(
+				"User ", user.getUserId(), " must be the company ",
+				"administrator to perform the action"),
+			() -> _kaleoDefinitionService.getKaleoDefinition(
+				kaleoDefinition.getKaleoDefinitionId()));
+
+		_setUpPermissionThreadLocal(_companyAdminUser);
+
+		Assert.assertEquals(
+			kaleoDefinition,
+			_kaleoDefinitionService.getKaleoDefinition(
+				kaleoDefinition.getKaleoDefinitionId()));
+	}
+
+	@Test
+	public void testGetScopeKaleoDefinitions() throws Exception {
+		User user = _addUser();
+
+		_setUpPermissionThreadLocal(user);
+
+		AssertUtils.assertFailure(
+			PrincipalException.MustBeCompanyAdmin.class,
+			StringBundler.concat(
+				"User ", user.getUserId(), " must be the company ",
+				"administrator to perform the action"),
+			() -> _kaleoDefinitionService.getScopeKaleoDefinitions(
+				WorkflowDefinitionConstants.SCOPE_ALL, true, QueryUtil.ALL_POS,
+				QueryUtil.ALL_POS, null, _serviceContext));
+
+		_setUpPermissionThreadLocal(_companyAdminUser);
+
+		KaleoDefinition singleApprover =
+			_kaleoDefinitionLocalService.getKaleoDefinition(
+				"Single Approver", _serviceContext);
+
+		Assert.assertEquals(
+			Collections.singletonList(singleApprover),
+			_kaleoDefinitionService.getScopeKaleoDefinitions(
+				WorkflowDefinitionConstants.SCOPE_ALL, true, QueryUtil.ALL_POS,
+				QueryUtil.ALL_POS, null, _serviceContext));
 	}
 
 	@Test
@@ -139,12 +187,7 @@ public class KaleoDefinitionServiceImplTest {
 
 		// Administrator with "company.administrator.can.publish" disabled
 
-		KaleoDefinition kaleoDefinition =
-			_kaleoDefinitionService.addKaleoDefinition(
-				RandomTestUtil.randomString(), RandomTestUtil.randomString(),
-				RandomTestUtil.randomString(),
-				_read("legal-marketing-definition.xml"), "company", 1,
-				_serviceContext);
+		KaleoDefinition kaleoDefinition = _addKaleoDefinition();
 
 		_setUpPermissionThreadLocal(_companyAdminUser);
 
@@ -180,12 +223,30 @@ public class KaleoDefinitionServiceImplTest {
 				kaleoDefinition.getContent(), _serviceContext));
 	}
 
-	private String _read(String name) throws Exception {
+	private KaleoDefinition _addKaleoDefinition() throws Exception {
+		return _kaleoDefinitionService.addKaleoDefinition(
+			RandomTestUtil.randomString(), RandomTestUtil.randomString(),
+			RandomTestUtil.randomString(), _read(), "company", 1,
+			_serviceContext);
+	}
+
+	private User _addUser() throws Exception {
+		return UserTestUtil.addUser(
+			_company.getCompanyId(), TestPropsValues.getUserId(),
+			RandomTestUtil.randomString(
+				NumericStringRandomizerBumper.INSTANCE,
+				UniqueStringRandomizerBumper.INSTANCE),
+			LocaleUtil.getDefault(), RandomTestUtil.randomString(),
+			RandomTestUtil.randomString(), new long[0], _serviceContext);
+	}
+
+	private String _read() throws Exception {
 		ClassLoader classLoader =
 			BaseKaleoLocalServiceTestCase.class.getClassLoader();
 
 		try (InputStream inputStream = classLoader.getResourceAsStream(
-				"com/liferay/portal/workflow/kaleo/dependencies/" + name)) {
+				"com/liferay/portal/workflow/kaleo/dependencies" +
+					"/legal-marketing-definition.xml")) {
 
 			return StringUtil.read(inputStream);
 		}
@@ -206,7 +267,14 @@ public class KaleoDefinitionServiceImplTest {
 	private static ConfigurationAdmin _configurationAdmin;
 
 	@Inject
+	private KaleoDefinitionLocalService _kaleoDefinitionLocalService;
+
+	@Inject
 	private KaleoDefinitionService _kaleoDefinitionService;
+
+	@Inject
+	private KaleoDefinitionVersionLocalService
+		_kaleoDefinitionVersionLocalService;
 
 	private String _originalName;
 	private PermissionChecker _originalPermissionChecker;
