ddc8309257635491bb5e8d140960ae50da52f396
Marcellus Tavares <marcellus.tavares@liferay.com>
2024-07-29 14:20:04 -0700

    LPD-26428 SF

--- modules/dxp/apps/osb/osb-asah/osb-asah-dataflow-java/src/main/java/com/liferay/osb/asah/dataflow/ingestion/event/BatchEventIngestionPipeline.java
+++ modules/dxp/apps/osb/osb-asah/osb-asah-dataflow-java/src/main/java/com/liferay/osb/asah/dataflow/ingestion/event/BatchEventIngestionPipeline.java
@@ -1,5 +1,5 @@
 /**
- * SPDX-FileCopyrightText: (c) 2004 Liferay, Inc. https://liferay.com
+ * SPDX-FileCopyrightText: (c) 2024 Liferay, Inc. https://liferay.com
  * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
  */
 
@@ -253,7 +253,7 @@ public class BatchEventIngestionPipeline {
 			}
 			catch (Exception exception) {
 				_logger.error(
-					"Event Extractor [" + analyticsEvent.id + "]: " +
+					"Analytics Event Extractor [" + analyticsEvent.id + "]: " +
 						exception.getMessage(),
 					exception);
 			}
