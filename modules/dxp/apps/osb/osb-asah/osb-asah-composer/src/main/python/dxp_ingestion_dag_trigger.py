d991375d5e35cd05ac0deb32831c69af75dabf4a
Marcellus Tavares <marcellus.tavares@liferay.com>
2024-08-06 16:11:32 -0700

    LPD-32850 Replace DXPEntity Dataflow ingestion dependency to BigQuery load job

---
+++ modules/dxp/apps/osb/osb-asah/osb-asah-composer/src/main/python/dxp_ingestion_dag_trigger.py
@@ -0,0 +1,204 @@
+#
+# Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+#
+# The contents of this file are subject to the terms of the Liferay Enterprise
+# Subscription License ("License"). You may not use this file except in
+# compliance with the License. You can obtain a copy of the License by
+# contacting Liferay, Inc. See the License for the specific language governing
+# permissions and limitations under the License, including but not limited to
+# distribution rights of the Software.
+#
+
+from airflow.models import Variable
+from airflow.models.baseoperator import chain
+from airflow.providers.google.cloud.operators.bigquery import \
+	BigQueryCreateExternalTableOperator
+
+from liferay.bigquery import BigQueryInsertJobFromTemplateOperator
+
+import airflow
+import os
+import pendulum
+import requests
+
+def create_dag(
+	ac_project_id, accounts_selected, contacts_selected, dag_id, dag_description):
+
+	with airflow.DAG(
+		dag_id=dag_id,
+		default_args={
+			'ac_project_id': ac_project_id,
+			'google_project_id': os.environ['GOOGLE_PROJECT_ID'],
+			'owner': 'Liferay'
+		},
+		description=dag_description,
+		max_active_runs=1,
+		schedule_interval=None,
+		start_date=pendulum.now() - pendulum.duration(days=2)
+	) as dag:
+
+		create_dxpentity_external_table = BigQueryCreateExternalTableOperator(
+			bucket="{{ dag.default_args['google_project_id'] }}-dxp-entities",
+			compression='GZIP',
+			destination_project_dataset_table="{{ dag.default_args['ac_project_id'] }}.dxpentity_external_{{ ts_nodash }}",
+			schema_fields=[
+				{
+					"fields": [
+						{
+							"mode": "REQUIRED",
+							"name": "columnId",
+							"type": "STRING"
+						},
+						{
+							"mode": "REQUIRED",
+							"name": "name",
+							"type": "STRING"
+						},
+						{
+							"mode": "REQUIRED",
+							"name": "value",
+	   						"type": "STRING"
+						}
+					],
+					"mode": "REPEATED",
+					"name": "expandoFields",
+					"type": "RECORD"
+				},
+				{
+					"fields": [
+						{
+							"mode": "REQUIRED",
+							"name": "name",
+							"type": "STRING"
+						},
+						{
+							"mode": "REQUIRED",
+							"name": "value",
+	   						"type": "STRING"
+						}
+					],
+					"mode": "REPEATED",
+					"name": "fields",
+					"type": "RECORD"
+				},
+				{
+					"mode": "REQUIRED",
+					"name": "id",
+					"type": "STRING"
+				},
+				{
+					"mode": "REQUIRED",
+					"name": "modifiedDate",
+					"type": "TIMESTAMP"
+				},
+				{
+					"mode": "REQUIRED",
+					"name": "type",
+					"type": "STRING"
+				}
+			],
+			source_format='NEWLINE_DELIMITED_JSON',
+			source_objects=["{{ params['bucketFolder'] }}/{{ params['filePrefix'] }}.gz"],
+			task_id="create_dxpentity_external_table"
+		)
+
+		dxpentity_insert_job  = BigQueryInsertJobFromTemplateOperator(
+			task_id='dxp_entity_insert'
+		)
+
+		bigquery_jobs = []
+
+		if accounts_selected:
+			bigquery_jobs += [
+				BigQueryInsertJobFromTemplateOperator(
+					task_id='account_entry_merge'
+				),
+				BigQueryInsertJobFromTemplateOperator(
+					task_id='account_group_merge'
+				)
+			]
+		
+		if contacts_selected:
+			bigquery_jobs += [
+				BigQueryInsertJobFromTemplateOperator(
+					task_id='expando_column_merge'
+				),
+				BigQueryInsertJobFromTemplateOperator(
+					task_id='expando_value_delete'
+				),
+				BigQueryInsertJobFromTemplateOperator(
+					task_id='expando_value_merge'
+				),
+				BigQueryInsertJobFromTemplateOperator(
+					task_id='group_merge'
+				),
+				BigQueryInsertJobFromTemplateOperator(
+					task_id='organization_merge'
+				),
+				BigQueryInsertJobFromTemplateOperator(
+					task_id='role_merge'
+				),
+				BigQueryInsertJobFromTemplateOperator(
+					task_id='team_merge'
+				),
+				BigQueryInsertJobFromTemplateOperator(
+					task_id='user_group_merge'
+				),
+				BigQueryInsertJobFromTemplateOperator(
+					task_id='user_merge'
+				),
+			]
+
+		individual_merge_job = BigQueryInsertJobFromTemplateOperator(
+			task_id='individual_merge'
+		)
+
+		chain(
+			create_dxpentity_external_table, dxpentity_insert_job, 
+			bigquery_jobs, individual_merge_job
+		)
+
+		return dag
+
+response = requests.get(
+	Variable.get('osb.asah.backend.url'),
+	headers={
+		'OSB-Asah-Faro-Backend-Security-Signature': Variable.get('osb.asah.faro.backend.security.signature'),
+		'OSB-Asah-Project-ID': 'osbasah',
+		'User-Agent': 'LiferayAnalyticsCloud'
+	}
+)
+
+for project in response.json():
+
+	#
+	# DXP Entity
+	#
+
+	downstream_task_ids = []
+
+	if project.get('accountsSelected'):
+		downstream_task_ids.extend(
+			['account_entry_merge', 'account_group_merge']
+		)
+
+	if project.get('contactsSelected'):
+		downstream_task_ids.extend([
+			'expando_column_merge', 'expando_value_delete',
+			'expando_value_merge', 'group_merge', 'organization_merge',
+			'role_merge', 'team_merge', 'user_group_merge', 'user_merge'
+		])
+
+		downstream_task_ids = [downstream_task_ids] + ['individual_merge']
+
+	if len(downstream_task_ids) > 0:
+		dag_id = 'dxp_entity_ingestion_{}'.format(
+			project.get('id')
+		)
+
+		globals()[dag_id] = create_dag(
+			project.get('id'), project.get('accountsSelected'), dag_id,
+			'DXP Entity Ingestion DAG For {}'.format(
+				project.get('id')
+			)
+		)
---
+++ modules/dxp/apps/osb/osb-asah/osb-asah-composer/src/main/resources/sql/dxp_entity_insert_statement.sql
@@ -0,0 +1,47 @@
+INSERT INTO `{{ dag.default_args['ac_project_id'] }}.dxpentity` (
+	classPK,
+	dataSourceId,
+	expandoFields,
+	fields,
+	id,
+	modifiedDate,
+	projectId,
+	type,
+	uploadDate,
+	uploadType
+)
+
+SELECT
+	id AS classPK,
+	CAST('{{ params['dataSourceId'] }}' AS INTEGER) AS dataSourceId,
+	expandoFields,
+	fields,
+	id,
+	modifiedDate,
+	'{{ dag.default_args['ac_project_id'] }}' AS projectId,
+	type,
+	CAST('{{ params['uploadDate'] }}' AS TIMESTAMP) AS uploadDate,
+	'{{ params['uploadType'] }}' AS uploadType
+FROM
+	(
+		SELECT
+			(
+				SELECT
+					TRUE
+				FROM
+					UNNEST(fields)
+				JOIN
+					`{{ dag.default_args['ac_project_id'] }}.suppression` AS Suppression
+				ON
+					value = Suppression.emailAddress
+				WHERE
+					NAME = 'emailAddress' AND
+					TYPE = 'com.liferay.portal.kernel.model.User'
+			) AS suppressed,
+			*
+		FROM
+			`{{ dag.default_args['ac_project_id'] }}.dxpentity_external_{{ ts_nodash }}`
+	) TMP
+WHERE
+
+	suppressed IS NULL
