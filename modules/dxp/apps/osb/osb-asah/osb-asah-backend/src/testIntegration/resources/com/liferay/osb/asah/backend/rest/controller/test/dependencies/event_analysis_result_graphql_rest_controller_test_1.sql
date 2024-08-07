64683cc999a82b3a3e3dfd1bc1f759d8e64ca83c
Rachael Koestartyo <rachael.koestartyo@liferay.com>
2024-08-07 14:17:45 -0700

    LPD-32795 sort

--- modules/dxp/apps/osb/osb-asah/osb-asah-backend/src/testIntegration/resources/com/liferay/osb/asah/backend/rest/controller/test/dependencies/event_analysis_result_graphql_rest_controller_test_1.sql
+++ modules/dxp/apps/osb/osb-asah/osb-asah-backend/src/testIntegration/resources/com/liferay/osb/asah/backend/rest/controller/test/dependencies/event_analysis_result_graphql_rest_controller_test_1.sql
@@ -1,10 +1,10 @@
 INSERT INTO Channel (id, createDate, name) VALUES (2468, '2021-05-12 18:12:00', 'testChannelName');
 
-INSERT INTO EventDefinition (applicationId, id, blocked, description, displayName, name, type) VALUES ('CustomEvent', 12345, false, 'Test Event Description', 'Test Display Name', 'testName', 'CUSTOM');
-
 INSERT INTO EventAttributeDefinition (dataType, displayName, id, name, type) VALUES ('STRING', 'Test Attribute 1', 12346, 'testUrl', 'LOCAL');
 INSERT INTO EventAttributeDefinition (dataType, displayName, id, name, type) VALUES ('STRING', 'Test Attribute 2', 23456, 'testCode', 'LOCAL');
 INSERT INTO EventAttributeDefinition (dataType, displayName, id, name, type) VALUES ('STRING', 'Test Attribute 3', 34567, 'testRating', 'LOCAL');
+
+INSERT INTO EventDefinition (applicationId, id, blocked, description, displayName, name, type) VALUES ('CustomEvent', 12345, false, 'Test Event Description', 'Test Display Name', 'testName', 'CUSTOM');
 
 INSERT INTO EventDefinitionEventAttributeDefinition (eventAttributeDefinitionId, eventDefinitionId, sampleValue) VALUES (12346, 12345, 'Sample Text');
 INSERT INTO EventDefinitionEventAttributeDefinition (eventAttributeDefinitionId, eventDefinitionId, sampleValue) VALUES (23456, 12345, 'Sample Text 1');
