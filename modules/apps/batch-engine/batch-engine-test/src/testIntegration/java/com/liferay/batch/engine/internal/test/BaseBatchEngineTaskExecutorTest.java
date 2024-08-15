d23a636c4cc8450c790f4babcea76783df220428
Alan Huang <alan.huang@liferay.com>
2024-08-16 00:48:20 +0800

    LPD-34110 Auto SF

--- modules/apps/batch-engine/batch-engine-test/src/testIntegration/java/com/liferay/batch/engine/internal/test/BaseBatchEngineTaskExecutorTest.java
+++ modules/apps/batch-engine/batch-engine-test/src/testIntegration/java/com/liferay/batch/engine/internal/test/BaseBatchEngineTaskExecutorTest.java
@@ -120,11 +120,11 @@ public class BaseBatchEngineTaskExecutorTest {
 		public BlogPostingEntityModel() {
 			_entityFieldsMap = EntityModel.toEntityFieldsMap(
 				new CollectionEntityField(
-					new StringEntityField(
-						"keywords", locale -> "assetTagNames.raw")),
-				new CollectionEntityField(
 					new IntegerEntityField(
 						"taxonomyCategoryIds", locale -> "assetCategoryIds")),
+				new CollectionEntityField(
+					new StringEntityField(
+						"keywords", locale -> "assetTagNames.raw")),
 				new DateTimeEntityField(
 					"dateCreated",
 					locale -> Field.getSortableFieldName(Field.CREATE_DATE),
