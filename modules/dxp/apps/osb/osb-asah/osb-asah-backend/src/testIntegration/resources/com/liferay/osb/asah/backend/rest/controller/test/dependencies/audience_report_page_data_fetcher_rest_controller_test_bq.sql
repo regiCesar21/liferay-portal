30e769ef98c8d64b85988a86deed26eb1dcfc222
Leslie Wong <8095751+lesliewong92@users.noreply.github.com>
2024-08-28 17:54:16 -0700

    LPD-35098 Add test for audience report on page views

---
+++ modules/dxp/apps/osb/osb-asah/osb-asah-backend/src/testIntegration/resources/com/liferay/osb/asah/backend/rest/controller/test/dependencies/audience_report_page_data_fetcher_rest_controller_test_bq.sql
@@ -0,0 +1,16 @@
+INSERT INTO MembershipChange (identitiesCount, segmentId) VALUES (2, 24680);
+
+INSERT INTO Membership (channelId, identityId, individualId, segmentId) VALUES (12345, 'identity_b', 'individual_b', 24680);
+INSERT INTO Membership (channelId, identityId, individualId, segmentId) VALUES (12345, 'identity_c', 'individual_c', 24680);
+
+INSERT INTO PageDaily (canonicalUrl, channelId, eventDate, title, userId, views) VALUES ('https://www.beryl.com/page-1', 12345, TIMESTAMP(DATETIME_TRUNC(timestamp '${today-4d}', HOUR)), 'Page Title 1', 'identity_a', 1);
+INSERT INTO PageDaily (canonicalUrl, channelId, eventDate, title, userId, views) VALUES ('https://www.beryl.com/page-1', 12345, TIMESTAMP(DATETIME_TRUNC(timestamp '${today-4d}', HOUR)), 'Page Title 1', 'identity_b', 1);
+INSERT INTO PageDaily (canonicalUrl, channelId, eventDate, title, userId, views) VALUES ('https://www.beryl.com/page-2', 12345, TIMESTAMP(DATETIME_TRUNC(timestamp '${today-4d}', HOUR)), 'Page Title 2', 'identity_c', 1);
+
+INSERT INTO Identity_Raw (id, individualId) VALUES ('identity_a', 'individual_a');
+INSERT INTO Identity_Raw (id, individualId) VALUES ('identity_b', 'individual_b');
+INSERT INTO Identity_Raw (id, individualId) VALUES ('identity_c', 'individual_c');
+
+INSERT INTO Individual (emailAddress, id) VALUES ('individual.a@liferay.com', 'individual_a');
+INSERT INTO Individual (emailAddress, id) VALUES ('individual.b@liferay.com', 'individual_b');
+INSERT INTO Individual (emailAddress, id) VALUES ('individual.c@liferay.com', 'individual_c');
