29c3825caaea1a9b11a78cc00d82ab5ea196b273
Leslie Wong <8095751+lesliewong92@users.noreply.github.com>
2024-08-30 11:09:55 -0700

    LPD-35091 SF

--- modules/dxp/apps/osb/osb-asah/osb-asah-backend/src/testIntegration/resources/com/liferay/osb/asah/backend/rest/controller/test/dependencies/audience_report_document_library_data_fetcher_rest_controller_test_bq.sql
+++ modules/dxp/apps/osb/osb-asah/osb-asah-backend/src/testIntegration/resources/com/liferay/osb/asah/backend/rest/controller/test/dependencies/audience_report_document_library_data_fetcher_rest_controller_test_bq.sql
@@ -1,11 +1,6 @@
-INSERT INTO MembershipChange (identitiesCount, segmentId) VALUES (2, 24680);
-
-INSERT INTO Membership (channelId, identityId, individualId, segmentId) VALUES (12345, 'identity_b', 'individual_b', 24680);
-INSERT INTO Membership (channelId, identityId, individualId, segmentId) VALUES (12345, 'identity_c', 'individual_c', 24680);
-
-INSERT INTO DocumentLibraryDaily (assetId, assetTitle, canonicalUrl, downloads, channelId, eventDate, pageTitle, previews, userId) VALUES ('DocumentLibrary1', 'Document 1', 'https://www.beryl.com/documents/document-1', 1, 12345, TIMESTAMP(DATETIME_TRUNC(timestamp '${today-4d}', HOUR)), 'Document Page 1', 1, 'identity_a');
-INSERT INTO DocumentLibraryDaily (assetId, assetTitle, canonicalUrl, downloads, channelId, eventDate, pageTitle, previews, userId) VALUES ('DocumentLibrary1', 'Document 1', 'https://www.beryl.com/documents/document-1', 1, 12345, TIMESTAMP(DATETIME_TRUNC(timestamp '${today-4d}', HOUR)), 'Document Page 1', 1, 'identity_b');
-INSERT INTO DocumentLibraryDaily (assetId, assetTitle, canonicalUrl, downloads, channelId, eventDate, pageTitle, previews, userId) VALUES ('DocumentLibrary2', 'Document 2', 'https://www.beryl.com/documents/document-2', 1, 12345, TIMESTAMP(DATETIME_TRUNC(timestamp '${today-4d}', HOUR)), 'Document Page 2', 1, 'identity_c');
+INSERT INTO DocumentLibraryDaily (assetId, assetTitle, canonicalUrl, downloads, channelId, eventDate, pageTitle, previews, userId) VALUES ('DocumentLibrary1', 'Document 1', 'https://www.beryl.com/documents/document-1', 1, 12345, TIMESTAMP '${today-4d}', 'Document Page 1', 1, 'identity_a');
+INSERT INTO DocumentLibraryDaily (assetId, assetTitle, canonicalUrl, downloads, channelId, eventDate, pageTitle, previews, userId) VALUES ('DocumentLibrary1', 'Document 1', 'https://www.beryl.com/documents/document-1', 1, 12345, TIMESTAMP '${today-4d}', 'Document Page 1', 1, 'identity_b');
+INSERT INTO DocumentLibraryDaily (assetId, assetTitle, canonicalUrl, downloads, channelId, eventDate, pageTitle, previews, userId) VALUES ('DocumentLibrary2', 'Document 2', 'https://www.beryl.com/documents/document-2', 1, 12345, TIMESTAMP '${today-4d}', 'Document Page 2', 1, 'identity_c');
 
 INSERT INTO Identity_Raw (id, individualId) VALUES ('identity_a', 'individual_a');
 INSERT INTO Identity_Raw (id, individualId) VALUES ('identity_b', 'individual_b');
@@ -14,3 +9,8 @@ INSERT INTO Identity_Raw (id, individualId) VALUES ('identity_c', 'individual_c'
 INSERT INTO Individual (emailAddress, id) VALUES ('individual.a@liferay.com', 'individual_a');
 INSERT INTO Individual (emailAddress, id) VALUES ('individual.b@liferay.com', 'individual_b');
 INSERT INTO Individual (emailAddress, id) VALUES ('individual.c@liferay.com', 'individual_c');
+
+INSERT INTO Membership (channelId, identityId, individualId, segmentId) VALUES (12345, 'identity_b', 'individual_b', 24680);
+INSERT INTO Membership (channelId, identityId, individualId, segmentId) VALUES (12345, 'identity_c', 'individual_c', 24680);
+
+INSERT INTO MembershipChange (identitiesCount, segmentId) VALUES (2, 24680);
