231e104bf82c183ce3fe060f13aa1e05ff14f62a
Rachael Koestartyo <rachael.koestartyo@liferay.com>
2024-09-06 12:46:36 -0700

    LPD-33311 move to AssetMetricRestController

--- modules/dxp/apps/osb/osb-asah/osb-asah-backend/src/main/java/com/liferay/osb/asah/backend/rest/controller/api/data/source/v1/AssetMetricRestController.java
+++ modules/dxp/apps/osb/osb-asah/osb-asah-backend/src/main/java/com/liferay/osb/asah/backend/rest/controller/api/data/source/v1/AssetMetricRestController.java
@@ -5,20 +5,27 @@
 
 package com.liferay.osb.asah.backend.rest.controller.api.data.source.v1;
 
+import com.liferay.osb.asah.backend.dog.HistogramDog;
 import com.liferay.osb.asah.backend.dog.MetricDog;
 import com.liferay.osb.asah.backend.dog.MetricTypeDog;
 import com.liferay.osb.asah.backend.dog.helper.SearchQueryContext;
+import com.liferay.osb.asah.backend.dto.AssetHistogramMetricDTO;
 import com.liferay.osb.asah.backend.dto.AssetMetricDTO;
 import com.liferay.osb.asah.backend.model.AssetMetric;
 import com.liferay.osb.asah.backend.model.AssetType;
+import com.liferay.osb.asah.backend.model.BlogMetricType;
+import com.liferay.osb.asah.backend.model.DocumentLibraryMetricType;
 import com.liferay.osb.asah.backend.model.IdentityType;
+import com.liferay.osb.asah.backend.model.JournalMetricType;
 import com.liferay.osb.asah.common.model.MetricType;
 import com.liferay.osb.asah.common.model.TimeRange;
+import com.liferay.osb.asah.common.spring.http.exception.OSBAsahException;
 
 import java.util.LinkedHashSet;
 import java.util.Set;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -33,6 +40,77 @@ import org.springframework.web.bind.annotation.RestController;
 	"com.liferay.osb.asah.backend.rest.controller.api.data.source.v1.AssetMetricRestController"
 )
 public class AssetMetricRestController {
+
+	@GetMapping("/histogram")
+	public AssetHistogramMetricDTO getAssetHistogramMetricDTO(
+		@RequestParam String assetId,
+		@PathVariable("assetType") String assetTypeString,
+		@RequestParam Set<Long> channelIds, @RequestParam String identityType,
+		@RequestParam(defaultValue = "30") int rangeKey) {
+
+		AssetType assetType = AssetType.of(assetTypeString);
+
+		SearchQueryContext searchQueryContext = new SearchQueryContext(
+			assetType);
+
+		searchQueryContext.setAssetId(assetId);
+		searchQueryContext.setInterval("D");
+		searchQueryContext.setTimeRange(TimeRange.of(rangeKey));
+
+		Set<AssetHistogramMetricDTO> assetHistogramMetricDTOs =
+			new LinkedHashSet<>();
+
+		if (assetType == AssetType.BLOG) {
+			assetHistogramMetricDTOs.add(
+				new AssetHistogramMetricDTO(
+					_histogramDog.getHistogramMetricBag(
+						channelIds, IdentityType.valueOf(identityType),
+						BlogMetricType.COMMENTS, searchQueryContext),
+					BlogMetricType.COMMENTS.getName()));
+			assetHistogramMetricDTOs.add(
+				new AssetHistogramMetricDTO(
+					_histogramDog.getHistogramMetricBag(
+						channelIds, IdentityType.valueOf(identityType),
+						BlogMetricType.VIEWS, searchQueryContext),
+					BlogMetricType.VIEWS.getName()));
+		}
+		else if (assetType == AssetType.DOCUMENT) {
+			assetHistogramMetricDTOs.add(
+				new AssetHistogramMetricDTO(
+					_histogramDog.getHistogramMetricBag(
+						channelIds, IdentityType.valueOf(identityType),
+						DocumentLibraryMetricType.COMMENTS, searchQueryContext),
+					DocumentLibraryMetricType.COMMENTS.getName()));
+			assetHistogramMetricDTOs.add(
+				new AssetHistogramMetricDTO(
+					_histogramDog.getHistogramMetricBag(
+						channelIds, IdentityType.valueOf(identityType),
+						DocumentLibraryMetricType.DOWNLOADS,
+						searchQueryContext),
+					DocumentLibraryMetricType.DOWNLOADS.getName()));
+			assetHistogramMetricDTOs.add(
+				new AssetHistogramMetricDTO(
+					_histogramDog.getHistogramMetricBag(
+						channelIds, IdentityType.valueOf(identityType),
+						DocumentLibraryMetricType.PREVIEWS, searchQueryContext),
+					DocumentLibraryMetricType.PREVIEWS.getName()));
+		}
+		else if (assetType == AssetType.JOURNAL) {
+			assetHistogramMetricDTOs.add(
+				new AssetHistogramMetricDTO(
+					_histogramDog.getHistogramMetricBag(
+						channelIds, IdentityType.valueOf(identityType),
+						JournalMetricType.VIEWS, searchQueryContext),
+					JournalMetricType.VIEWS.getName()));
+		}
+		else {
+			throw new OSBAsahException(
+				HttpStatus.BAD_REQUEST,
+				"Unsupported asset type: " + assetTypeString);
+		}
+
+		return new AssetHistogramMetricDTO(assetHistogramMetricDTOs);
+	}
 
 	@GetMapping
 	public AssetMetricDTO getAssetMetricDTO(
@@ -69,6 +147,9 @@ public class AssetMetricRestController {
 
 		return metricTypes;
 	}
+
+	@Autowired
+	private HistogramDog _histogramDog;
 
 	@Autowired
 	private MetricDog _metricDog;
--- modules/dxp/apps/osb/osb-asah/osb-asah-backend/src/testIntegration/java/com/liferay/osb/asah/backend/rest/controller/api/data/source/v1/test/AssetMetricRestControllerTest.java
+++ modules/dxp/apps/osb/osb-asah/osb-asah-backend/src/testIntegration/java/com/liferay/osb/asah/backend/rest/controller/api/data/source/v1/test/AssetMetricRestControllerTest.java
@@ -6,8 +6,12 @@
 package com.liferay.osb.asah.backend.rest.controller.api.data.source.v1.test;
 
 import com.liferay.osb.asah.backend.OSBAsahBackendSpringTestContext;
+import com.liferay.osb.asah.backend.dto.AssetHistogramMetricDTO;
 import com.liferay.osb.asah.backend.dto.AssetMetricDTO;
+import com.liferay.osb.asah.backend.dto.HistogramMetricDTO;
 import com.liferay.osb.asah.backend.model.AssetType;
+import com.liferay.osb.asah.backend.model.BlogMetricType;
+import com.liferay.osb.asah.backend.model.DocumentLibraryMetricType;
 import com.liferay.osb.asah.backend.model.Metric;
 import com.liferay.osb.asah.backend.rest.controller.api.data.source.v1.AssetMetricRestController;
 import com.liferay.osb.asah.common.util.SetUtil;
@@ -16,7 +20,9 @@ import com.liferay.osb.asah.test.util.spring.OSBAsahTestExecutionListenersContex
 
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.Iterator;
 import java.util.List;
+import java.util.Set;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
@@ -29,6 +35,340 @@ import org.springframework.beans.factory.annotation.Autowired;
 public class AssetMetricRestControllerTest
 	implements OSBAsahBackendSpringTestContext,
 			   OSBAsahTestExecutionListenersContext {
+
+	@BQSQLResource(resourcePath = "asset_histogram_metric.sql")
+	@Test
+	public void testGetAssetHistogramMetricDTO1() {
+		AssetHistogramMetricDTO assetHistogramMetricDTO =
+			_assetMetricRestController.getAssetHistogramMetricDTO(
+				"e131fabc", "blog", Collections.singleton(1L), "ALL", 7);
+
+		Set<AssetHistogramMetricDTO> assetHistogramMetricDTOs =
+			assetHistogramMetricDTO.getAssetHistogramMetricDTOs();
+
+		Assertions.assertEquals(2, assetHistogramMetricDTOs.size());
+
+		for (AssetHistogramMetricDTO curAssetHistogramMetricDTO :
+				assetHistogramMetricDTOs) {
+
+			Set<HistogramMetricDTO> histogramMetricDTOs =
+				curAssetHistogramMetricDTO.getHistogramMetricDTOs();
+
+			Assertions.assertEquals(7, histogramMetricDTOs.size());
+
+			String metricName = curAssetHistogramMetricDTO.getMetricName();
+
+			if (metricName.equals(BlogMetricType.COMMENTS.getName())) {
+				Assertions.assertArrayEquals(
+					new double[] {0, 0, 0, 0, 0, 1, 0},
+					_getActualValues(histogramMetricDTOs));
+			}
+			else if (metricName.equals(BlogMetricType.VIEWS.getName())) {
+				Assertions.assertArrayEquals(
+					new double[] {0, 1, 0, 0, 0, 1, 1},
+					_getActualValues(histogramMetricDTOs));
+			}
+		}
+
+		assetHistogramMetricDTO =
+			_assetMetricRestController.getAssetHistogramMetricDTO(
+				"e131fabc", "blog", Collections.singleton(1L), "KNOWN", 28);
+
+		assetHistogramMetricDTOs =
+			assetHistogramMetricDTO.getAssetHistogramMetricDTOs();
+
+		Assertions.assertEquals(2, assetHistogramMetricDTOs.size());
+
+		for (AssetHistogramMetricDTO curAssetHistogramMetricDTO :
+				assetHistogramMetricDTOs) {
+
+			Set<HistogramMetricDTO> histogramMetricDTOs =
+				curAssetHistogramMetricDTO.getHistogramMetricDTOs();
+
+			Assertions.assertEquals(28, histogramMetricDTOs.size());
+
+			String metricName = curAssetHistogramMetricDTO.getMetricName();
+
+			if (metricName.equals(BlogMetricType.COMMENTS.getName())) {
+				Assertions.assertArrayEquals(
+					new double[] {
+						0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+						0, 1, 0, 0, 0, 0, 0, 1, 0
+					},
+					_getActualValues(histogramMetricDTOs));
+			}
+			else if (metricName.equals(BlogMetricType.VIEWS.getName())) {
+				Assertions.assertArrayEquals(
+					new double[] {
+						0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+						0, 2, 0, 1, 0, 0, 0, 1, 1
+					},
+					_getActualValues(histogramMetricDTOs));
+			}
+		}
+
+		assetHistogramMetricDTO =
+			_assetMetricRestController.getAssetHistogramMetricDTO(
+				"e131fabc", "blog", Collections.singleton(1L), "UNKNOWN", 30);
+
+		assetHistogramMetricDTOs =
+			assetHistogramMetricDTO.getAssetHistogramMetricDTOs();
+
+		Assertions.assertEquals(2, assetHistogramMetricDTOs.size());
+
+		for (AssetHistogramMetricDTO curAssetHistogramMetricDTO :
+				assetHistogramMetricDTOs) {
+
+			Set<HistogramMetricDTO> histogramMetricDTOs =
+				curAssetHistogramMetricDTO.getHistogramMetricDTOs();
+
+			Assertions.assertEquals(30, histogramMetricDTOs.size());
+
+			String metricName = curAssetHistogramMetricDTO.getMetricName();
+
+			if (metricName.equals(BlogMetricType.COMMENTS.getName())) {
+				Assertions.assertArrayEquals(
+					new double[] {
+						0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1,
+						0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0
+					},
+					_getActualValues(histogramMetricDTOs));
+			}
+			else if (metricName.equals(BlogMetricType.VIEWS.getName())) {
+				Assertions.assertArrayEquals(
+					new double[] {
+						0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 1,
+						0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0
+					},
+					_getActualValues(histogramMetricDTOs));
+			}
+		}
+	}
+
+	@BQSQLResource(resourcePath = "asset_histogram_metric.sql")
+	@Test
+	public void testGetAssetHistogramMetricDTO2() {
+		AssetHistogramMetricDTO assetHistogramMetricDTO =
+			_assetMetricRestController.getAssetHistogramMetricDTO(
+				"zsrwerf", "document", Collections.singleton(1L), "ALL", 7);
+
+		Set<AssetHistogramMetricDTO> assetHistogramMetricDTOs =
+			assetHistogramMetricDTO.getAssetHistogramMetricDTOs();
+
+		Assertions.assertEquals(3, assetHistogramMetricDTOs.size());
+
+		for (AssetHistogramMetricDTO curAssetHistogramMetricDTO :
+				assetHistogramMetricDTOs) {
+
+			Set<HistogramMetricDTO> histogramMetricDTOs =
+				curAssetHistogramMetricDTO.getHistogramMetricDTOs();
+
+			Assertions.assertEquals(7, histogramMetricDTOs.size());
+
+			String metricName = curAssetHistogramMetricDTO.getMetricName();
+
+			if (metricName.equals(
+					DocumentLibraryMetricType.COMMENTS.getName())) {
+
+				Assertions.assertArrayEquals(
+					new double[] {0, 1, 0, 0, 0, 3, 2},
+					_getActualValues(histogramMetricDTOs));
+			}
+			else if (metricName.equals(
+						DocumentLibraryMetricType.DOWNLOADS.getName())) {
+
+				Assertions.assertArrayEquals(
+					new double[] {0, 1, 0, 0, 0, 3, 2},
+					_getActualValues(histogramMetricDTOs));
+			}
+			else if (metricName.equals(
+						DocumentLibraryMetricType.PREVIEWS.getName())) {
+
+				Assertions.assertArrayEquals(
+					new double[] {0, 1, 0, 0, 0, 1, 1},
+					_getActualValues(histogramMetricDTOs));
+			}
+		}
+
+		assetHistogramMetricDTO =
+			_assetMetricRestController.getAssetHistogramMetricDTO(
+				"zsrwerf", "document", Collections.singleton(1L), "KNOWN", 28);
+
+		assetHistogramMetricDTOs =
+			assetHistogramMetricDTO.getAssetHistogramMetricDTOs();
+
+		Assertions.assertEquals(3, assetHistogramMetricDTOs.size());
+
+		for (AssetHistogramMetricDTO curAssetHistogramMetricDTO :
+				assetHistogramMetricDTOs) {
+
+			Set<HistogramMetricDTO> histogramMetricDTOs =
+				curAssetHistogramMetricDTO.getHistogramMetricDTOs();
+
+			Assertions.assertEquals(28, histogramMetricDTOs.size());
+
+			String metricName = curAssetHistogramMetricDTO.getMetricName();
+
+			if (metricName.equals(
+					DocumentLibraryMetricType.COMMENTS.getName())) {
+
+				Assertions.assertArrayEquals(
+					new double[] {
+						0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3, 0, 2, 0, 0,
+						0, 2, 0, 1, 0, 0, 0, 0, 0
+					},
+					_getActualValues(histogramMetricDTOs));
+			}
+			else if (metricName.equals(
+						DocumentLibraryMetricType.DOWNLOADS.getName())) {
+
+				Assertions.assertArrayEquals(
+					new double[] {
+						0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3, 0, 2, 0, 0,
+						0, 2, 0, 1, 0, 0, 0, 0, 0
+					},
+					_getActualValues(histogramMetricDTOs));
+			}
+			else if (metricName.equals(
+						DocumentLibraryMetricType.PREVIEWS.getName())) {
+
+				Assertions.assertArrayEquals(
+					new double[] {
+						0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1, 0, 0,
+						0, 1, 0, 1, 0, 0, 0, 0, 0
+					},
+					_getActualValues(histogramMetricDTOs));
+			}
+		}
+
+		assetHistogramMetricDTO =
+			_assetMetricRestController.getAssetHistogramMetricDTO(
+				"zsrwerf", "document", Collections.singleton(1L), "UNKNOWN",
+				30);
+
+		assetHistogramMetricDTOs =
+			assetHistogramMetricDTO.getAssetHistogramMetricDTOs();
+
+		Assertions.assertEquals(3, assetHistogramMetricDTOs.size());
+
+		for (AssetHistogramMetricDTO curAssetHistogramMetricDTO :
+				assetHistogramMetricDTOs) {
+
+			Set<HistogramMetricDTO> histogramMetricDTOs =
+				curAssetHistogramMetricDTO.getHistogramMetricDTOs();
+
+			Assertions.assertEquals(30, histogramMetricDTOs.size());
+
+			String metricName = curAssetHistogramMetricDTO.getMetricName();
+
+			if (metricName.equals(
+					DocumentLibraryMetricType.COMMENTS.getName())) {
+
+				Assertions.assertArrayEquals(
+					new double[] {
+						0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+						0, 0, 1, 3, 0, 0, 0, 0, 0, 3, 2
+					},
+					_getActualValues(histogramMetricDTOs));
+			}
+			else if (metricName.equals(
+						DocumentLibraryMetricType.DOWNLOADS.getName())) {
+
+				Assertions.assertArrayEquals(
+					new double[] {
+						0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+						0, 0, 1, 3, 0, 0, 0, 0, 0, 3, 2
+					},
+					_getActualValues(histogramMetricDTOs));
+			}
+			else if (metricName.equals(
+						DocumentLibraryMetricType.PREVIEWS.getName())) {
+
+				Assertions.assertArrayEquals(
+					new double[] {
+						0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+						0, 0, 1, 1, 0, 0, 0, 0, 0, 1, 1
+					},
+					_getActualValues(histogramMetricDTOs));
+			}
+		}
+	}
+
+	@BQSQLResource(resourcePath = "asset_histogram_metric.sql")
+	@Test
+	public void testGetAssetHistogramMetricDTO3() {
+		AssetHistogramMetricDTO assetHistogramMetricDTO =
+			_assetMetricRestController.getAssetHistogramMetricDTO(
+				"egdasdf", "journal", Collections.singleton(1L), "ALL", 7);
+
+		Set<AssetHistogramMetricDTO> assetHistogramMetricDTOs =
+			assetHistogramMetricDTO.getAssetHistogramMetricDTOs();
+
+		Assertions.assertEquals(1, assetHistogramMetricDTOs.size());
+
+		for (AssetHistogramMetricDTO curAssetHistogramMetricDTO :
+				assetHistogramMetricDTOs) {
+
+			Set<HistogramMetricDTO> histogramMetricDTOs =
+				curAssetHistogramMetricDTO.getHistogramMetricDTOs();
+
+			Assertions.assertEquals(7, histogramMetricDTOs.size());
+
+			Assertions.assertArrayEquals(
+				new double[] {0, 1, 0, 0, 0, 1, 1},
+				_getActualValues(histogramMetricDTOs));
+		}
+
+		assetHistogramMetricDTO =
+			_assetMetricRestController.getAssetHistogramMetricDTO(
+				"egdasdf", "journal", Collections.singleton(1L), "KNOWN", 28);
+
+		assetHistogramMetricDTOs =
+			assetHistogramMetricDTO.getAssetHistogramMetricDTOs();
+
+		Assertions.assertEquals(1, assetHistogramMetricDTOs.size());
+
+		for (AssetHistogramMetricDTO curAssetHistogramMetricDTO :
+				assetHistogramMetricDTOs) {
+
+			Set<HistogramMetricDTO> histogramMetricDTOs =
+				curAssetHistogramMetricDTO.getHistogramMetricDTOs();
+
+			Assertions.assertEquals(28, histogramMetricDTOs.size());
+
+			Assertions.assertArrayEquals(
+				new double[] {
+					0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1,
+					1, 0, 0, 0, 0, 0, 1, 0
+				},
+				_getActualValues(histogramMetricDTOs));
+		}
+
+		assetHistogramMetricDTO =
+			_assetMetricRestController.getAssetHistogramMetricDTO(
+				"egdasdf", "journal", Collections.singleton(1L), "UNKNOWN", 30);
+
+		assetHistogramMetricDTOs =
+			assetHistogramMetricDTO.getAssetHistogramMetricDTOs();
+
+		Assertions.assertEquals(1, assetHistogramMetricDTOs.size());
+
+		for (AssetHistogramMetricDTO curAssetHistogramMetricDTO :
+				assetHistogramMetricDTOs) {
+
+			Set<HistogramMetricDTO> histogramMetricDTOs =
+				curAssetHistogramMetricDTO.getHistogramMetricDTOs();
+
+			Assertions.assertEquals(30, histogramMetricDTOs.size());
+
+			Assertions.assertArrayEquals(
+				new double[] {
+					0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 1, 0,
+					0, 0, 1, 0, 1, 0, 0, 0, 0, 1
+				},
+				_getActualValues(histogramMetricDTOs));
+		}
+	}
 
 	@BQSQLResource(resourcePath = "blog_asset_metric_views.sql")
 	@Test
@@ -75,6 +415,24 @@ public class AssetMetricRestControllerTest
 		metric = metrics.get(0);
 
 		Assertions.assertEquals(4, metric.getValue());
+	}
+
+	private double[] _getActualValues(
+		Set<HistogramMetricDTO> histogramMetricDTOs) {
+
+		double[] actualValues = new double[histogramMetricDTOs.size()];
+
+		Iterator<HistogramMetricDTO> iterator = histogramMetricDTOs.iterator();
+
+		int i = 0;
+
+		while (iterator.hasNext()) {
+			HistogramMetricDTO histogramMetricDTO = iterator.next();
+
+			actualValues[i++] = histogramMetricDTO.getValue();
+		}
+
+		return actualValues;
 	}
 
 	@Autowired
