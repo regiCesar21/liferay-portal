b998f096c921a93808ba7b57dfba7f0cfa6a87ec
Leslie Wong <8095751+lesliewong92@users.noreply.github.com>
2024-09-11 22:22:30 -0700

    LPD-36165 Set default content encoding as zip

--- modules/dxp/apps/osb/osb-asah/osb-asah-publisher/src/main/java/com/liferay/osb/asah/publisher/rest/controller/DXPBatchEntitiesRestController.java
+++ modules/dxp/apps/osb/osb-asah/osb-asah-publisher/src/main/java/com/liferay/osb/asah/publisher/rest/controller/DXPBatchEntitiesRestController.java
@@ -100,7 +100,8 @@ public class DXPBatchEntitiesRestController {
 	@PostMapping(consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
 	public ResponseEntity<?> post(
 			@RequestHeader(
-				required = false, value = HttpHeaders.CONTENT_ENCODING
+				defaultValue = "zip", required = false,
+				value = HttpHeaders.CONTENT_ENCODING
 			)
 			String contentEncoding,
 			@RequestHeader(value = HeaderConstants.DATA_SOURCE_ID) String
