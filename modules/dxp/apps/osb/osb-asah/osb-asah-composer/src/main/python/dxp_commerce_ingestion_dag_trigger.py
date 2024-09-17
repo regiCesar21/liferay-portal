081f9148764bcd7bd6a94ecf43140e616bb9c814
Riccardo Ferrari <riccardo.ferrari@liferay.com>
2024-09-17 14:43:43 +0200

    LPD-36661 osb-asah-composer Refactor BigQuery Product load

---
+++ modules/dxp/apps/osb/osb-asah/osb-asah-composer/src/main/python/dxp_commerce_ingestion_dag_trigger.py
@@ -0,0 +1,343 @@
+#
+# Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+#
+# The contents of this file are subject to the terms of the Liferay Enterprise
+# Subscription License ("License"). You may not use this file except in
+# compliance with the License. You can obtain a copy of the License by
+# contacting Liferay, Inc. See the License for the specific language governing
+# permissions and limitations under the License, including but not limited to
+# distribution rights of the Software.
+#
+
+from airflow.models import Variable
+from airflow.models.baseoperator import chain
+from airflow.providers.google.cloud.operators.bigquery import \
+	BigQueryCreateExternalTableOperator, BigQueryDeleteTableOperator
+
+from liferay.bigquery import BigQueryInsertJobFromTemplateOperator
+
+import airflow
+import os
+import pendulum
+import requests
+
+def create_dag(ac_project_id, dag_id, dag_description, entity, schema_fields):
+	with airflow.DAG(
+		dag_id=dag_id,
+		default_args={
+			'ac_project_id': ac_project_id,
+			'google_project_id': os.environ['GOOGLE_PROJECT_ID'],
+			'owner': 'Liferay'
+		},
+		description=dag_description,
+		max_active_runs=1,
+		schedule_interval=None,
+		start_date=pendulum.now() - pendulum.duration(days=2)
+	) as dag:
+
+		create_external_table = BigQueryCreateExternalTableOperator(
+			bucket="{{ params['bucketName'] }}",
+			compression='GZIP',
+			destination_project_dataset_table="{{ dag.default_args['ac_project_id'] }}.{{ entity }}_external_{{ ts_nodash }}",
+			schema_fields=schema_fields,
+			source_format='NEWLINE_DELIMITED_JSON',
+			source_objects=["{{ dag.default_args['ac_project_id'] }}/{{ params['bucketFolder'] }}/{{ params['uploadDate'] }}.gz"],
+			task_id=f"create_{ entity }_external_table"
+		)
+
+		insert_job = BigQueryInsertJobFromTemplateOperator(
+			task_id=f'{ entity }_raw_insert'
+		)
+
+		merge_job = BigQueryInsertJobFromTemplateOperator(
+			task_id=f'{ entity }_merge'
+		)
+
+		delete_external_table = BigQueryDeleteTableOperator(
+			deletion_dataset_table="{{ dag.default_args['ac_project_id'] }}.{{ entity }}_external_{{ ts_nodash }}",
+			task_id=f"delete_{ entity }_external_table"
+		)
+
+		chain(
+			create_external_table, insert_job, merge_job, delete_external_table
+		)
+
+		return dag
+
+response = requests.get(
+	Variable.get('osb.asah.backend.url'),
+	headers={
+		'OSB-Asah-Faro-Backend-Security-Signature': Variable.get('osb.asah.faro.backend.security.signature'),
+		'OSB-Asah-Project-ID': 'osbasah',
+		'User-Agent': 'LiferayAnalyticsCloud'
+	}
+)
+
+for project in response.json():
+
+	if not project.get('commerceChannelsSelected'):
+		continue
+
+	#
+	# Commerce Product
+	#
+
+	dag_id = 'dxp_product_ingestion_{}'.format(
+		project.get('id')
+	)
+
+	globals()[dag_id] = create_dag(
+		project.get('id'), dag_id,
+		'DXP Product Ingestion DAG For {}'.format(
+			project.get('id')
+		),
+		'product',
+		[
+			{
+				"mode": "NULLABLE",
+				"name": "catalogId",
+				"type": "INT64"
+			},
+			{
+				"mode": "REPEATED",
+				"name": "categoryIds",
+				"type": "INT64"
+			},
+			{
+				"mode": "NULLABLE",
+				"name": "createDate",
+				"type": "TIMESTAMP"
+			},
+			{
+				"mode": "REQUIRED",
+				"name": "dataSourceId",
+				"type": "INT64"
+			},
+			{
+				"mode": "NULLABLE",
+				"name": "description",
+				"type": "JSON"
+			},
+			{
+				"mode": "NULLABLE",
+				"name": "displayDate",
+				"type": "TIMESTAMP"
+			},
+			{
+				"mode": "NULLABLE",
+				"name": "expirationDate",
+				"type": "TIMESTAMP"
+			},
+			{
+				"mode": "NULLABLE",
+				"name": "externalReferenceCode",
+				"type": "STRING"
+			},
+			{
+				"mode": "NULLABLE",
+				"name": "id",
+				"type": "INT64"
+			},
+			{
+				"mode": "NULLABLE",
+				"name": "metaDescription",
+				"type": "JSON"
+			},
+			{
+				"mode": "NULLABLE",
+				"name": "metaKeyword",
+				"type": "JSON"
+			},
+			{
+				"mode": "NULLABLE",
+				"name": "metaTitle",
+				"type": "JSON"
+			},
+			{
+				"mode": "NULLABLE",
+				"name": "modifiedDate",
+				"type": "TIMESTAMP"
+			},
+			{
+				"mode": "NULLABLE",
+				"name": "name",
+				"type": "JSON"
+			},
+			{
+				"mode": "REPEATED",
+				"name": "productChannelIds",
+				"type": "INT64"
+			},
+			{
+				"mode": "NULLABLE",
+				"name": "productId",
+				"type": "INT64"
+			},
+			{
+				"fields": [
+					{
+						"mode": "NULLABLE",
+						"name": "key",
+						"type": "STRING"
+					},
+					{
+						"mode": "NULLABLE",
+						"name": "optionKey",
+						"type": "STRING"
+					},
+					{
+						"mode": "REPEATED",
+						"name": "values",
+						"type": "JSON"
+					}
+				],
+				"mode": "REPEATED",
+				"name": "productOptions",
+				"type": "RECORD"
+			},
+			{
+				"fields": [
+					{
+						"mode": "NULLABLE",
+						"name": "id",
+						"type": "INT64"
+					},
+					{
+						"mode": "NULLABLE",
+						"name": "label",
+						"type": "STRING"
+					},
+					{
+						"mode": "NULLABLE",
+						"name": "optionCategoryId",
+						"type": "INT64"
+					},
+					{
+						"mode": "NULLABLE",
+						"name": "priority",
+						"type": "NUMERIC"
+					},
+					{
+						"mode": "NULLABLE",
+						"name": "specificationId",
+						"type": "INT64"
+					},
+					{
+						"mode": "NULLABLE",
+						"name": "specificationKey",
+						"type": "STRING"
+					},
+					{
+						"mode": "NULLABLE",
+						"name": "value",
+						"type": "JSON"
+					}
+				],
+				"mode": "REPEATED",
+				"name": "productSpecifications",
+				"type": "RECORD"
+			},
+			{
+				"mode": "NULLABLE",
+				"name": "productType",
+				"type": "STRING"
+			},
+			{
+				"mode": "REQUIRED",
+				"name": "projectId",
+				"type": "STRING"
+			},
+			{
+				"fields": [
+					{
+						"mode": "NULLABLE",
+						"name": "cost",
+						"type": "NUMERIC"
+					},
+					{
+						"mode": "NULLABLE",
+						"name": "discontinued",
+						"type": "BOOL"
+					},
+					{
+						"mode": "NULLABLE",
+						"name": "displayDate",
+						"type": "TIMESTAMP"
+					},
+					{
+						"mode": "NULLABLE",
+						"name": "expirationDate",
+						"type": "TIMESTAMP"
+					},
+					{
+						"mode": "NULLABLE",
+						"name": "externalReferenceCode",
+						"type": "STRING"
+					},
+					{
+						"mode": "NULLABLE",
+						"name": "gtin",
+						"type": "STRING"
+					},
+					{
+						"mode": "NULLABLE",
+						"name": "id",
+						"type": "INT64"
+					},
+					{
+						"mode": "NULLABLE",
+						"name": "manufacturerPartNumber",
+						"type": "STRING"
+					},
+					{
+						"mode": "NULLABLE",
+						"name": "published",
+						"type": "BOOL"
+					},
+					{
+						"mode": "NULLABLE",
+						"name": "purchasable",
+						"type": "BOOL"
+					},
+					{
+						"mode": "NULLABLE",
+						"name": "sku",
+						"type": "STRING"
+					}
+				],
+				"mode": "REPEATED",
+				"name": "skus",
+				"type": "RECORD"
+			},
+			{
+				"mode": "NULLABLE",
+				"name": "status",
+				"type": "INT64"
+			},
+			{
+				"mode": "NULLABLE",
+				"name": "subscriptionEnabled",
+				"type": "BOOL"
+			},
+			{
+				"mode": "REPEATED",
+				"name": "tags",
+				"type": "STRING"
+			},
+			{
+				"mode": "NULLABLE",
+				"name": "uploadDate",
+				"type": "TIMESTAMP"
+			},
+			{
+				"mode": "NULLABLE",
+				"name": "uploadType",
+				"type": "STRING"
+			},
+			{
+				"mode": "NULLABLE",
+				"name": "urls",
+				"type": "JSON"
+			}
+		]
+	)
