5dd6f5487452b680e9513f87c0cd009b9306d760
Marcos Martins <marcos.martins@liferay.com>
2024-09-18 18:16:38 -0300

    LPD-33412 create endpoint

--- modules/dxp/apps/osb/osb-asah/osb-asah-backend/src/main/java/com/liferay/osb/asah/backend/rest/controller/api/data/source/v1/AssetMetricRestController.java
+++ modules/dxp/apps/osb/osb-asah/osb-asah-backend/src/main/java/com/liferay/osb/asah/backend/rest/controller/api/data/source/v1/AssetMetricRestController.java
@@ -13,6 +13,7 @@ import com.liferay.osb.asah.backend.dto.AppearsOnHistogramMetricDTO;
 import com.liferay.osb.asah.backend.dto.AssetAppearsOnHistogramMetricDTO;
 import com.liferay.osb.asah.backend.dto.AssetHistogramMetricDTO;
 import com.liferay.osb.asah.backend.dto.AssetMetricDTO;
+import com.liferay.osb.asah.backend.dto.DeviceMetricDTO;
 import com.liferay.osb.asah.backend.model.AssetMetric;
 import com.liferay.osb.asah.backend.model.AssetType;
 import com.liferay.osb.asah.backend.model.BlogMetricType;
@@ -137,6 +138,77 @@ public class AssetMetricRestController {
 				channelIds, IdentityType.valueOf(identityType),
 				searchQueryContext, selectedMetrics),
 			_getMetricTypes(assetType, selectedMetrics));
+	}
+
+	@GetMapping("/devices")
+	public DeviceMetricDTO getDeviceMetricDTO(
+		@RequestParam String assetId,
+		@PathVariable("assetType") String assetTypeString,
+		@RequestParam Set<Long> channelIds, @RequestParam String identityType,
+		@RequestParam(defaultValue = "30") int rangeKey) {
+
+		AssetType assetType = AssetType.of(assetTypeString);
+
+		SearchQueryContext searchQueryContext = new SearchQueryContext(
+			assetType);
+
+		searchQueryContext.setAssetId(assetId);
+		searchQueryContext.setTimeRange(TimeRange.of(rangeKey));
+
+		Set<DeviceMetricDTO> deviceMetricDTOs = new LinkedHashSet<>();
+
+		if (assetType == AssetType.BLOG) {
+			deviceMetricDTOs.add(
+				new DeviceMetricDTO(
+					BlogMetricType.COMMENTS.getName(),
+					_metricDog.getDeviceMetrics(
+						IdentityType.valueOf(identityType),
+						BlogMetricType.COMMENTS, searchQueryContext)));
+			deviceMetricDTOs.add(
+				new DeviceMetricDTO(
+					BlogMetricType.VIEWS.getName(),
+					_metricDog.getDeviceMetrics(
+						IdentityType.valueOf(identityType),
+						BlogMetricType.VIEWS, searchQueryContext)));
+		}
+		else if (assetType == AssetType.DOCUMENT) {
+			deviceMetricDTOs.add(
+				new DeviceMetricDTO(
+					DocumentLibraryMetricType.COMMENTS.getName(),
+					_metricDog.getDeviceMetrics(
+						IdentityType.valueOf(identityType),
+						DocumentLibraryMetricType.COMMENTS,
+						searchQueryContext)));
+			deviceMetricDTOs.add(
+				new DeviceMetricDTO(
+					DocumentLibraryMetricType.DOWNLOADS.getName(),
+					_metricDog.getDeviceMetrics(
+						IdentityType.valueOf(identityType),
+						DocumentLibraryMetricType.DOWNLOADS,
+						searchQueryContext)));
+			deviceMetricDTOs.add(
+				new DeviceMetricDTO(
+					DocumentLibraryMetricType.PREVIEWS.getName(),
+					_metricDog.getDeviceMetrics(
+						IdentityType.valueOf(identityType),
+						DocumentLibraryMetricType.PREVIEWS,
+						searchQueryContext)));
+		}
+		else if (assetType == AssetType.JOURNAL) {
+			deviceMetricDTOs.add(
+				new DeviceMetricDTO(
+					JournalMetricType.VIEWS.getName(),
+					_metricDog.getDeviceMetrics(
+						IdentityType.valueOf(identityType),
+						JournalMetricType.VIEWS, searchQueryContext)));
+		}
+		else {
+			throw new OSBAsahException(
+				HttpStatus.BAD_REQUEST,
+				"Unsupported asset type: " + assetTypeString);
+		}
+
+		return new DeviceMetricDTO(deviceMetricDTOs);
 	}
 
 	@GetMapping("/appears-on/histogram")
