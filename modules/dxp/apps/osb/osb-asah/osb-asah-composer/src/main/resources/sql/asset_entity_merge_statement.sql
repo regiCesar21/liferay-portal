ec132141ee7db3197458a7683ee5a6e515eb63ac
Rachael Koestartyo <rachael.koestartyo@liferay.com>
2024-09-18 15:17:42 -0700

    LPD-36348 missed update on asset_entity

--- modules/dxp/apps/osb/osb-asah/osb-asah-composer/src/main/resources/sql/asset_entity_merge_statement.sql
+++ modules/dxp/apps/osb/osb-asah/osb-asah-composer/src/main/resources/sql/asset_entity_merge_statement.sql
@@ -61,10 +61,10 @@ USING
 			assetentity.projectId = analyticsDeleteMessage.projectId
 		WHERE
 			assetentity.uploadDate >=
-				{% if prev_start_date_success is not none %}
-					'{{ prev_start_date_success }}'
-				{% else %}
+				{% if params.uploadType == 'FULL' %}
 					'1970-01-01T00:00:00'
+				{% else %}
+					CAST('{{ params['uploadDate'] }}' AS TIMESTAMP)
 				{% endif %}
 		) AS staging
 ON
