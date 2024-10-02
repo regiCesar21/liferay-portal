bde87522d418610d2358f0d7fd44200f345f2955
Leslie Wong <8095751+lesliewong92@users.noreply.github.com>
2024-10-02 13:38:28 -0700

    LPD-37529 Rename JAR instead of moving to another directory

--- modules/dxp/apps/osb/osb-asah/osb-asah-dataflow-java/upload_jar_manual.sh
+++ modules/dxp/apps/osb/osb-asah/osb-asah-dataflow-java/upload_jar_manual.sh
@@ -4,6 +4,6 @@ export PROJECT_ID=$(gcloud config get-value project)
 
 ../gradlew clean shadowJar
 
-mv build/libs/osb-asah-dataflow-java-all.jar docker-resources/osb-asah-dataflow-java.jar
+mv build/libs/osb-asah-dataflow-java-all.jar build/libs/osb-asah-dataflow-java.jar
 
 ./docker-resources/upload_jar.sh
