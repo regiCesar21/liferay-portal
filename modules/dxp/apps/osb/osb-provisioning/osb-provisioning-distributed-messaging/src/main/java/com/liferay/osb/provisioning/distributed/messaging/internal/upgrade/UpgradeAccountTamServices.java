c3b71416d489f4c572120e3e5c8c3c4b85ee6300
Felipe Veloso <felipe.veloso@liferay.com>
2024-10-04 16:50:00 -0300

    LRSD-6305 script to update existing accounts with the new tag

--- modules/dxp/apps/osb/osb-provisioning/osb-provisioning-distributed-messaging/src/main/java/com/liferay/osb/provisioning/distributed/messaging/internal/upgrade/UpgradeAccountTamServices.java
+++ modules/dxp/apps/osb/osb-provisioning/osb-provisioning-distributed-messaging/src/main/java/com/liferay/osb/provisioning/distributed/messaging/internal/upgrade/UpgradeAccountTamServices.java
@@ -1,4 +1,69 @@
+/**
+ * SPDX-FileCopyrightText: (c) 2024 Liferay, Inc. https://liferay.com
+ * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
+ */
+
 package com.liferay.osb.provisioning.distributed.messaging.internal.upgrade;
 
-public class UpgradeAccountTamServices {
+import com.liferay.osb.koroneiki.phloem.rest.client.constants.ExternalLinkDomain;
+import com.liferay.osb.koroneiki.phloem.rest.client.constants.ExternalLinkEntityName;
+import com.liferay.osb.koroneiki.phloem.rest.client.dto.v1_0.Account;
+import com.liferay.osb.provisioning.distributed.messaging.internal.subscribing.util.SalesSubscriberUtil;
+import com.liferay.osb.provisioning.koroneiki.web.service.AccountWebService;
+import com.liferay.portal.kernel.log.Log;
+import com.liferay.portal.kernel.log.LogFactoryUtil;
+import com.liferay.portal.kernel.upgrade.UpgradeProcess;
+import com.liferay.portal.kernel.util.StringPool;
+
+import java.util.List;
+import java.util.Map;
+
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+
+/**
+ * @author Felipe Veloso
+ */
+@Component(service = UpgradeAccountTamServices.class)
+public class UpgradeAccountTamServices extends UpgradeProcess {
+
+	public void upgradeAccount(String salesforceProjectKey) throws Exception {
+		List<Account> accounts = _accountWebService.getAccounts(
+			ExternalLinkDomain.SALESFORCE,
+			ExternalLinkEntityName.SALESFORCE_PROJECT, salesforceProjectKey, 1,
+			1);
+
+		if (accounts.isEmpty()) {
+			_log.error(
+				"Unable to find account with Salesforce project key " +
+					salesforceProjectKey);
+
+			return;
+		}
+
+		Account account = accounts.get(0);
+
+		Map<String, String> properties = account.getProperties();
+
+		properties.put("tam_services", "true");
+
+		_accountWebService.updateAccount(
+			StringPool.BLANK, StringPool.BLANK, account.getKey(), account);
+
+		_salesSubscriberUtil.updateTickets(account, properties);
+	}
+
+	@Override
+	protected void doUpgrade() throws Exception {
+	}
+
+	private static final Log _log = LogFactoryUtil.getLog(
+		UpgradeAccountTamServices.class);
+
+	@Reference
+	private AccountWebService _accountWebService;
+
+	@Reference
+	private SalesSubscriberUtil _salesSubscriberUtil;
+
 }
