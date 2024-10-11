7ad9924c15ad22d02453e16357e534e94085a0bf
shuyangzhou <shuyang.zhou@liferay.com>
2024-10-10 20:08:55 -0700

    LPD-36127 Revert "LPD-22357 Allow to search by hidden viewable public layouts", the entire homepage is missing due to this change.
    This reverts commit 071450d944ec04901a8d101e3c8e311083a3b945.

--- modules/apps/layout/layout-service/src/main/java/com/liferay/layout/internal/instance/lifecycle/AddDefaultLayoutPortalInstanceLifecycleListener.java
+++ modules/apps/layout/layout-service/src/main/java/com/liferay/layout/internal/instance/lifecycle/AddDefaultLayoutPortalInstanceLifecycleListener.java
@@ -68,7 +68,7 @@ public class AddDefaultLayoutPortalInstanceLifecycleListener
 		if (defaultLayout == null) {
 			defaultLayout = _layoutLocalService.fetchFirstLayout(
 				group.getGroupId(), false,
-				LayoutConstants.DEFAULT_PARENT_LAYOUT_ID);
+				LayoutConstants.DEFAULT_PARENT_LAYOUT_ID, false);
 
 			if (defaultLayout == null) {
 				addDefaultGuestPublicLayoutByProperties(group);
