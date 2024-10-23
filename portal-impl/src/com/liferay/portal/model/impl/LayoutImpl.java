518a1b6dc927f10363de8939e2c362f12ca4381f
Lourdes Fernández Besada <lourdes.fernandez@liferay.com>
2024-10-23 16:12:59 +0200

    LPD-39185 Revert "LPD-29872 Caches the effective theme"
    This reverts commit 1f14497023829843c78b49b0d216520f1cdbefc3.

--- portal-impl/src/com/liferay/portal/model/impl/LayoutImpl.java
+++ portal-impl/src/com/liferay/portal/model/impl/LayoutImpl.java
@@ -759,13 +759,20 @@ public class LayoutImpl extends LayoutBaseImpl {
 	 */
 	@Override
 	public Theme getTheme() throws PortalException {
-		if (_theme != null) {
-			return _theme;
+		if (isInheritLookAndFeel()) {
+			LayoutSet layoutSet = getLayoutSet();
+
+			return layoutSet.getTheme();
 		}
 
-		_theme = _getTheme();
+		Layout masterLayout = _getMasterLayout();
 
-		return _theme;
+		if (masterLayout != null) {
+			return ThemeLocalServiceUtil.getTheme(
+				masterLayout.getCompanyId(), masterLayout.getThemeId());
+		}
+
+		return ThemeLocalServiceUtil.getTheme(getCompanyId(), getThemeId());
 	}
 
 	@Override
@@ -1455,23 +1462,6 @@ public class LayoutImpl extends LayoutBaseImpl {
 		return portletPreferences;
 	}
 
-	private Theme _getTheme() throws PortalException {
-		if (isInheritLookAndFeel()) {
-			LayoutSet layoutSet = getLayoutSet();
-
-			return layoutSet.getTheme();
-		}
-
-		Layout masterLayout = _getMasterLayout();
-
-		if (masterLayout != null) {
-			return ThemeLocalServiceUtil.getTheme(
-				masterLayout.getCompanyId(), masterLayout.getThemeId());
-		}
-
-		return ThemeLocalServiceUtil.getTheme(getCompanyId(), getThemeId());
-	}
-
 	private String _getURL(
 			HttpServletRequest httpServletRequest, boolean resetMaxState,
 			boolean resetRenderParameters)
@@ -1572,7 +1562,6 @@ public class LayoutImpl extends LayoutBaseImpl {
 	private LayoutSet _layoutSet;
 	private transient LayoutType _layoutType;
 	private Layout _masterLayout;
-	private Theme _theme;
 	private UnicodeProperties _typeSettingsUnicodeProperties;
 
 }
