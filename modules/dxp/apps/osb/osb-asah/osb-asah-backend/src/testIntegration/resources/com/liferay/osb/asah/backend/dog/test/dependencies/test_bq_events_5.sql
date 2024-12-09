721611aafd7b5b6c3f3a9b568a33d0c97414a109
Ivica Cardic <ivica.cardic@liferay.com>
2024-12-09 22:31:16 +0100

    LPD-28191 | LPD-43703 Add test resources

---
+++ modules/dxp/apps/osb/osb-asah/osb-asah-backend/src/testIntegration/resources/com/liferay/osb/asah/backend/dog/test/dependencies/test_bq_events_5.sql
@@ -0,0 +1,20 @@
+INSERT INTO Identity_Raw(id, individualId) VALUES ('1', '1');
+INSERT INTO Identity_Raw(id, individualId) VALUES ('2', '2');
+
+INSERT INTO Individual(id) VALUES ('1');
+INSERT INTO Individual(id) VALUES ('2');
+INSERT INTO Individual(id) VALUES ('3');
+INSERT INTO Individual(id) VALUES ('4');
+
+INSERT INTO Session (channelId, id, sessionStart, sessionEnd, userId) VALUES (1, '1', timestamp '${now}', timestamp '${now}', '1');
+INSERT INTO Session (channelId, id, sessionStart, sessionEnd, userId) VALUES (1, '2', timestamp '${now-7h}', timestamp '${now-7h}', '2');
+INSERT INTO Session (channelId, id, sessionStart, sessionEnd, userId) VALUES (1, '3', timestamp '${now-10h}', timestamp '${now-10h}', '3');
+INSERT INTO Session (channelId, id, sessionStart, sessionEnd, userId) VALUES (1, '4', timestamp '${now-2d}', timestamp '${now-1d}', '4');
+INSERT INTO Session (channelId, id, sessionStart, sessionEnd, userId) VALUES (1, '5', timestamp '${now-4d}', timestamp '${now-3d}', '1');
+INSERT INTO Session (channelId, id, sessionStart, sessionEnd, userId) VALUES (1, '6', timestamp '${now-8d}', timestamp '${now-8d}', '2');
+INSERT INTO Session (channelId, id, sessionStart, sessionEnd, userId) VALUES (1, '7', timestamp '${now-8d}', timestamp '${now-8d}', '3');
+INSERT INTO Session (channelId, id, sessionStart, sessionEnd, userId) VALUES (1, '8', timestamp '${now-10d}', timestamp '${now-10d}', '4');
+INSERT INTO Session (channelId, id, sessionStart, sessionEnd, userId) VALUES (2, '9', timestamp '${now-12d}', timestamp '${now-12d}', '1');
+INSERT INTO Session (channelId, id, sessionStart, sessionEnd, userId) VALUES (2, '10', timestamp '${now-3d}', timestamp '${now-3d}', '2');
+INSERT INTO Session (channelId, id, sessionStart, sessionEnd, userId) VALUES (2, '11', timestamp '${now-3d}', timestamp '${now-3d}', '3');
+INSERT INTO Session (channelId, id, sessionStart, sessionEnd, userId) VALUES (2, '12', timestamp '${now-3d}', timestamp '${now-3d}', '4');
