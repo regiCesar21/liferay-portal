42b1c7ffd857d07427426c7181fe49da90151a21
Rachael Koestartyo <rachael.koestartyo@liferay.com>
2024-12-09 15:59:39 -0800

    LPD-20646 | LPD-39721 default to false

--- modules/dxp/apps/osb/osb-asah/osb-asah-upgrade/src/main/resources/v4_11_0/upgrade.sql
+++ modules/dxp/apps/osb/osb-asah/osb-asah-upgrade/src/main/resources/v4_11_0/upgrade.sql
@@ -1,2 +1,2 @@
-ALTER TABLE DataSource ADD IF NOT EXISTS contentRecommenderMostPopularItemsEnabled BOOLEAN;
-ALTER TABLE DataSource ADD IF NOT EXISTS contentRecommenderUserPersonalizationEnabled BOOLEAN;
+ALTER TABLE DataSource ADD IF NOT EXISTS contentRecommenderMostPopularItemsEnabled BOOLEAN DEFAULT false;
+ALTER TABLE DataSource ADD IF NOT EXISTS contentRecommenderUserPersonalizationEnabled BOOLEAN DEFAULT false;
