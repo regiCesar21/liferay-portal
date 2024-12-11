536a9ae9171af44fe54e01fa8084ba787c455f66
amosfong <amos.fong@liferay.com>
2024-12-10 17:07:09 -0800

    LRSD-6218 fix fixpack downloads

--- modules/dxp/apps/osb/osb-customer/osb-customer-downloads/osb-customer-downloads-proxy-web/src/main/java/com/liferay/osb/customer/downloads/proxy/web/internal/portlet/DownloadsProxyPortlet.java
+++ modules/dxp/apps/osb/osb-customer/osb-customer-downloads/osb-customer-downloads-proxy-web/src/main/java/com/liferay/osb/customer/downloads/proxy/web/internal/portlet/DownloadsProxyPortlet.java
@@ -7,13 +7,9 @@ package com.liferay.osb.customer.downloads.proxy.web.internal.portlet;
 
 import com.liferay.osb.customer.constants.OSBCustomerConstants;
 import com.liferay.osb.customer.downloads.proxy.web.internal.constants.DownloadsProxyPortletKeys;
-import com.liferay.osb.customer.downloads.proxy.web.internal.util.AgreementUtil;
-import com.liferay.osb.customer.downloads.proxy.web.internal.util.DownloadsUtil;
-import com.liferay.petra.string.CharPool;
 import com.liferay.petra.string.StringPool;
 import com.liferay.portal.kernel.audit.AuditMessage;
 import com.liferay.portal.kernel.audit.AuditRouterUtil;
-import com.liferay.portal.kernel.exception.PortalException;
 import com.liferay.portal.kernel.json.JSONFactoryUtil;
 import com.liferay.portal.kernel.json.JSONObject;
 import com.liferay.portal.kernel.log.Log;
@@ -21,17 +17,12 @@ import com.liferay.portal.kernel.log.LogFactoryUtil;
 import com.liferay.portal.kernel.model.User;
 import com.liferay.portal.kernel.portlet.bridges.mvc.MVCPortlet;
 import com.liferay.portal.kernel.security.permission.ActionKeys;
-import com.liferay.portal.kernel.service.OrganizationLocalService;
 import com.liferay.portal.kernel.theme.ThemeDisplay;
-import com.liferay.portal.kernel.util.ArrayUtil;
 import com.liferay.portal.kernel.util.Digester;
 import com.liferay.portal.kernel.util.DigesterUtil;
 import com.liferay.portal.kernel.util.FileUtil;
-import com.liferay.portal.kernel.util.Http;
 import com.liferay.portal.kernel.util.ParamUtil;
 import com.liferay.portal.kernel.util.Portal;
-import com.liferay.portal.kernel.util.StringBundler;
-import com.liferay.portal.kernel.util.StringUtil;
 import com.liferay.portal.kernel.util.Validator;
 import com.liferay.portal.kernel.util.WebKeys;
 
@@ -93,54 +84,6 @@ public class DownloadsProxyPortlet extends MVCPortlet {
 		return DigesterUtil.digestHex(Digester.MD5, shortFileName + _CODE_SALT);
 	}
 
-	protected boolean isCustomerAccess(
-		ThemeDisplay themeDisplay, PortletPreferences preferences,
-		String fileName) {
-
-		if (themeDisplay.getScopeGroupId() ==
-				OSBCustomerConstants.GROUP_CUSTOMER_ID) {
-
-			return true;
-		}
-
-		String customerAccessPatternString = preferences.getValue(
-			"customerAccessPattern", null);
-
-		if (Validator.isNull(customerAccessPatternString)) {
-			return false;
-		}
-
-		if ((_customerAccessPattern == null) ||
-			!customerAccessPatternString.equals(_customerAccessPatternString)) {
-
-			_customerAccessPatternString = customerAccessPatternString;
-
-			_customerAccessPattern = Pattern.compile(
-				customerAccessPatternString);
-		}
-
-		Matcher matcher = _customerAccessPattern.matcher(fileName);
-
-		if (!matcher.matches()) {
-			return false;
-		}
-
-		if (_organizationLocalService.hasUserOrganization(
-				themeDisplay.getUserId(),
-				OSBCustomerConstants.ORGANIZATION_CUSTOMER_DXP_ID) ||
-			_organizationLocalService.hasUserOrganization(
-				themeDisplay.getUserId(),
-				OSBCustomerConstants.ORGANIZATION_LIFERAY_INC_ID) ||
-			_organizationLocalService.hasUserOrganization(
-				themeDisplay.getUserId(),
-				OSBCustomerConstants.ORGANIZATION_PARTNER_ID)) {
-
-			return true;
-		}
-
-		return false;
-	}
-
 	protected boolean isGuestAccess(
 		PortletPreferences preferences, String fileName, String code) {
 
@@ -166,43 +109,6 @@ public class DownloadsProxyPortlet extends MVCPortlet {
 		}
 
 		if (!code.equals(generateCode(fileName))) {
-			return false;
-		}
-
-		return true;
-	}
-
-	protected boolean isPrivateFixPack(String fileName) throws IOException {
-		if (!fileName.startsWith(_FIX_PACK_DIR)) {
-			return false;
-		}
-
-		if (!fileName.endsWith(".zip")) {
-			return true;
-		}
-
-		String[] fileNameArray = StringUtil.split(fileName, CharPool.SLASH);
-
-		if (fileNameArray.length != 5) {
-			return true;
-		}
-
-		String liferayVersion = fileNameArray[2];
-		String componentName = fileNameArray[3];
-
-		StringBundler sb = new StringBundler(4);
-
-		sb.append(_URL_PREFIX);
-		sb.append(_FIX_PACK_DIR);
-		sb.append(liferayVersion);
-		sb.append("/public-components.txt");
-
-		String key = _http.URLtoString(sb.toString(), true);
-
-		String[] publicComponents = DownloadsUtil.getPublicComponents(
-			_URL_PREFIX + StringPool.SLASH + key, liferayVersion);
-
-		if (ArrayUtil.contains(publicComponents, componentName)) {
 			return false;
 		}
 
@@ -236,59 +142,10 @@ public class DownloadsProxyPortlet extends MVCPortlet {
 				return false;
 			}
 
-			if (isPrivateFixPack(fileName)) {
-				return false;
-			}
-
-			if (isCustomerAccess(themeDisplay, preferences, fileName)) {
-				return true;
-			}
-
-			if (isTrial(themeDisplay, preferences, fileName)) {
-				return true;
-			}
+			return true;
 		}
 		catch (Exception e) {
 			_log.error(e, e);
-		}
-
-		return false;
-	}
-
-	protected boolean isTrial(
-			ThemeDisplay themeDisplay, PortletPreferences preferences,
-			String fileName)
-		throws PortalException {
-
-		String trialPatternString = preferences.getValue("trialPattern", null);
-
-		if (Validator.isNull(trialPatternString)) {
-			return false;
-		}
-
-		if ((_trialPattern == null) ||
-			!trialPatternString.equals(_trialPatternString)) {
-
-			_trialPatternString = trialPatternString;
-
-			_trialPattern = Pattern.compile(trialPatternString);
-		}
-
-		Matcher matcher = _trialPattern.matcher(fileName);
-
-		if (!matcher.matches()) {
-			return false;
-		}
-
-		if (fileName.startsWith("/trial/sync/")) {
-			if (AgreementUtil.hasLiferaySyncEULA(themeDisplay.getUserId())) {
-				return true;
-			}
-		}
-		else {
-			if (AgreementUtil.hasTrialEULA(themeDisplay.getUserId())) {
-				return true;
-			}
 		}
 
 		return false;
@@ -320,25 +177,13 @@ public class DownloadsProxyPortlet extends MVCPortlet {
 
 	private static final String _CODE_SALT = "L1f3R@y";
 
-	private static final String _FIX_PACK_DIR = "/fix-packs/";
-
 	private static final String _URL_PREFIX = "https://downloads.liferay.com";
 
 	private static final Log _log = LogFactoryUtil.getLog(
 		DownloadsProxyPortlet.class);
 
-	private static Pattern _customerAccessPattern;
-	private static String _customerAccessPatternString;
 	private static Pattern _guestAccessPattern;
 	private static String _guestAccessPatternString;
-	private static Pattern _trialPattern;
-	private static String _trialPatternString;
-
-	@Reference
-	private Http _http;
-
-	@Reference
-	private OrganizationLocalService _organizationLocalService;
 
 	@Reference
 	private Portal _portal;
