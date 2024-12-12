a42217f5601d3b633370d7804d1582d8501e1496
Ivica Cardic <ivica.cardic@liferay.com>
2024-12-12 08:15:28 +0100

    LPD-28191 | LPD-43703 - Update test resources to avoid flaky tests

--- modules/dxp/apps/osb/osb-asah/osb-asah-backend/src/testIntegration/resources/com/liferay/osb/asah/backend/dog/test/dependencies/test_bq_events_5.sql
+++ modules/dxp/apps/osb/osb-asah/osb-asah-backend/src/testIntegration/resources/com/liferay/osb/asah/backend/dog/test/dependencies/test_bq_events_5.sql
@@ -7,8 +7,8 @@ INSERT INTO Individual(id) VALUES ('3');
 INSERT INTO Individual(id) VALUES ('4');
 
 INSERT INTO Session (channelId, id, sessionStart, sessionEnd, userId) VALUES (1, '1', timestamp '${now}', timestamp '${now}', '1');
-INSERT INTO Session (channelId, id, sessionStart, sessionEnd, userId) VALUES (1, '2', timestamp '${now-7h}', timestamp '${now-7h}', '2');
-INSERT INTO Session (channelId, id, sessionStart, sessionEnd, userId) VALUES (1, '3', timestamp '${now-10h}', timestamp '${now-10h}', '3');
+INSERT INTO Session (channelId, id, sessionStart, sessionEnd, userId) VALUES (1, '2', timestamp '${now-3m}', timestamp '${now-2m}', '2');
+INSERT INTO Session (channelId, id, sessionStart, sessionEnd, userId) VALUES (1, '3', timestamp '${now-4m}', timestamp '${now-3m}', '3');
 INSERT INTO Session (channelId, id, sessionStart, sessionEnd, userId) VALUES (1, '4', timestamp '${now-2d}', timestamp '${now-1d}', '4');
 INSERT INTO Session (channelId, id, sessionStart, sessionEnd, userId) VALUES (1, '5', timestamp '${now-4d}', timestamp '${now-3d}', '1');
 INSERT INTO Session (channelId, id, sessionStart, sessionEnd, userId) VALUES (1, '6', timestamp '${now-8d}', timestamp '${now-8d}', '2');
