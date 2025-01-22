a2595ad3040203297b8361d2321c10d22fa4247b
Marcellus Tavares <marcellus.tavares@liferay.com>
2025-01-22 14:57:25 -0800

    LPD-39451 | LPD-45774 Remove executed upgrade steps

--- modules/dxp/apps/osb/osb-asah/osb-asah-upgrade/src/main/java/com/liferay/osb/asah/upgrade/UpgradeProcessConfiguration.java
+++ modules/dxp/apps/osb/osb-asah/osb-asah-upgrade/src/main/java/com/liferay/osb/asah/upgrade/UpgradeProcessConfiguration.java
@@ -5,9 +5,6 @@
 
 package com.liferay.osb.asah.upgrade;
 
-import com.liferay.osb.asah.upgrade.v4_10_0.AssetHourlyViewsUpgradeStep;
-import com.liferay.osb.asah.upgrade.v4_10_0.AssetMetricUpgradeStep;
-import com.liferay.osb.asah.upgrade.v4_10_0.EventUpgradeStep;
 import com.liferay.osb.asah.upgrade.v4_11_0.EventPropertyUpgradeStep;
 import com.liferay.osb.asah.upgrade.v4_11_0.PostgreSQLUpgradeStep;
 
@@ -27,11 +24,6 @@ public class UpgradeProcessConfiguration {
 		UpgradeProcess upgradeProcess = new UpgradeProcess();
 
 		upgradeProcess.addUpgradeSteps(
-			"4.0.36", "4.0.37", _assetHourlyViewsUpgradeStep);
-		upgradeProcess.addUpgradeSteps(
-			"4.0.37", "4.0.38", _assetMetricUpgradeStep);
-		upgradeProcess.addUpgradeSteps("4.0.38", "4.0.39", _eventUpgradeStep);
-		upgradeProcess.addUpgradeSteps(
 			"4.0.39", "4.0.40", _postgreSQLUpgradeStep);
 		upgradeProcess.addUpgradeSteps(
 			"4.0.40", "4.0.41", _eventPropertyUpgradeStep);
@@ -40,16 +32,7 @@ public class UpgradeProcessConfiguration {
 	}
 
 	@Autowired
-	private AssetHourlyViewsUpgradeStep _assetHourlyViewsUpgradeStep;
-
-	@Autowired
-	private AssetMetricUpgradeStep _assetMetricUpgradeStep;
-
-	@Autowired
 	private EventPropertyUpgradeStep _eventPropertyUpgradeStep;
-
-	@Autowired
-	private EventUpgradeStep _eventUpgradeStep;
 
 	@Autowired
 	@Qualifier("com.liferay.osb.asah.upgrade.v4_11_0.PostgreSQLUpgradeStep")
