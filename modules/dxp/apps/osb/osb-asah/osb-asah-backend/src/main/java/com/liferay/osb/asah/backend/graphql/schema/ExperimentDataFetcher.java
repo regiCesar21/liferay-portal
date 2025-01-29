147e91fc24bcb0cb369814ed4bbb50a03e0a0125
Caio Elias Rabelo Pinheiro <caio.elias@liferay.com>
2025-01-29 17:56:31 -0300

    LPD-1174 delete unused

--- modules/dxp/apps/osb/osb-asah/osb-asah-backend/src/main/java/com/liferay/osb/asah/backend/graphql/schema/ExperimentDataFetcher.java
+++ modules/dxp/apps/osb/osb-asah/osb-asah-backend/src/main/java/com/liferay/osb/asah/backend/graphql/schema/ExperimentDataFetcher.java
@@ -8,19 +8,17 @@ package com.liferay.osb.asah.backend.graphql.schema;
 import com.liferay.osb.asah.backend.dog.ExperimentDog;
 import com.liferay.osb.asah.backend.dto.ExperimentDTO;
 import com.liferay.osb.asah.backend.graphql.annotation.GraphQLTypeWiring;
-
 import com.liferay.osb.asah.common.entity.Experiment;
 import com.liferay.osb.asah.common.spring.http.exception.OSBAsahException;
-import graphql.GraphQLContext;
 
 import graphql.schema.DataFetcher;
 import graphql.schema.DataFetchingEnvironment;
 
+import java.util.Objects;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Component;
-
-import java.util.Objects;
 
 /**
  * @author André Miranda
@@ -30,24 +28,22 @@ import java.util.Objects;
 public class ExperimentDataFetcher implements DataFetcher<ExperimentDTO> {
 
 	@Override
-	public ExperimentDTO get(DataFetchingEnvironment dataFetchingEnvironment) throws OSBAsahException {
+	public ExperimentDTO get(DataFetchingEnvironment dataFetchingEnvironment) {
 		String experimentId = dataFetchingEnvironment.getArgument(
 			"experimentId");
-		GraphQLContext graphQLContext =
-			dataFetchingEnvironment.getGraphQlContext();
 
-		graphQLContext.put("experimentId", experimentId);
-		graphQLContext.put("channelId", channelId);
-
-		Experiment experiment = _experimentDog.fetchExperiment(Long.valueOf(experimentId));
 		String channelId = dataFetchingEnvironment.getArgument("channelId");
-		if (!Objects.equals(experiment.getChannelId(), Long.valueOf(channelId))) {
-			 throw new OSBAsahException(
-                    HttpStatus.NOT_FOUND,
-                    "no experiment was found");
-        }
-        return new ExperimentDTO(
-			experiment);
+		Experiment experiment = _experimentDog.fetchExperiment(
+			Long.valueOf(experimentId));
+
+		if (!Objects.equals(
+				Long.valueOf(channelId), experiment.getChannelId())) {
+
+			throw new OSBAsahException(
+				HttpStatus.NOT_FOUND, "no experiment was found");
+		}
+
+		return new ExperimentDTO(experiment);
 	}
 
 	@Autowired
