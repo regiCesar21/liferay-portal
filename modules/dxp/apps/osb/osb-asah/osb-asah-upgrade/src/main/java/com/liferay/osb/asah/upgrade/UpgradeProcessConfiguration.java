69ef00a22afbf7720832f257363d76327ce3933f
Rachael Koestartyo <rachael.koestartyo@liferay.com>
2025-01-28 16:46:24 -0800

    LPD-47146 register

--- modules/dxp/apps/osb/osb-asah/osb-asah-upgrade/src/main/java/com/liferay/osb/asah/upgrade/UpgradeProcessConfiguration.java
+++ modules/dxp/apps/osb/osb-asah/osb-asah-upgrade/src/main/java/com/liferay/osb/asah/upgrade/UpgradeProcessConfiguration.java
@@ -5,6 +5,7 @@
 
 package com.liferay.osb.asah.upgrade;
 
+import com.liferay.osb.asah.upgrade.v4_11_0.DataSourcesUpgradeStep;
 import com.liferay.osb.asah.upgrade.v4_11_0.EventPropertyUpgradeStep;
 import com.liferay.osb.asah.upgrade.v4_11_0.PostgreSQLUpgradeStep;
 
@@ -26,9 +27,14 @@ public class UpgradeProcessConfiguration {
 			"4.0.39", "4.0.40", _postgreSQLUpgradeStep);
 		upgradeProcess.addUpgradeSteps(
 			"4.0.40", "4.0.41", _eventPropertyUpgradeStep);
+		upgradeProcess.addUpgradeSteps(
+			"4.0.41", "4.0.42", _dataSourcesUpgradeStep);
 
 		return upgradeProcess;
 	}
+
+	@Autowired
+	private DataSourcesUpgradeStep _dataSourcesUpgradeStep;
 
 	@Autowired
 	private EventPropertyUpgradeStep _eventPropertyUpgradeStep;
