83575943ca644f4d621119b7e4533b34ef21ab1e
Marcellus Tavares <marcellus.tavares@liferay.com>
2025-01-31 15:52:28 -0800

    LPD-46359 | LPD-47868 Create BQ individual export job

---
+++ modules/dxp/apps/osb/osb-asah/osb-asah-composer/src/main/resources/sql/individual_export_statement.sql
@@ -0,0 +1,14 @@
+EXPORT DATA
+    OPTIONS (
+        field_delimiter = ';',
+        format = 'CSV',
+        header = false,
+        overwrite = true,
+        uri = "gs://{{ dag.default_args['google_project_id'] }}-data-replica/{{ dag.default_args['ac_project_id'] }}/individual/{{ ts }}/*.csv"
+    )
+AS (
+    SELECT
+        id, emailAddress, TO_JSON_STRING(fields) AS fields
+    FROM
+       `{{ dag.default_args['ac_project_id'] }}.individual`
+);
