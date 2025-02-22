5bf90f45db9ad6b4428d66df3a9f7cebd83fd8f8
Marcellus Tavares <marcellus.tavares@liferay.com>
2025-02-21 16:12:31 -0800

    LPD-46364 | LPD-49122 Remove import operator from DAGs

--- modules/dxp/apps/osb/osb-asah/osb-asah-composer/src/main/python/data_replication_dag_trigger.py
+++ modules/dxp/apps/osb/osb-asah/osb-asah-composer/src/main/python/data_replication_dag_trigger.py
@@ -11,13 +11,8 @@
 
 from airflow.models import Variable
 from airflow.models.baseoperator import chain
-from airflow.providers.google.cloud.operators.cloud_sql import \
-	CloudSQLExecuteQueryOperator
-from airflow.providers.google.cloud.operators.bigquery import \
-	BigQueryCreateExternalTableOperator, BigQueryDeleteTableOperator
 
 from liferay.bigquery import BigQueryInsertJobFromTemplateOperator
-from liferay.cloud_sql import CloudSQLCSVImportOperator
 
 import airflow
 import os
@@ -28,7 +23,6 @@ def create_dag(ac_project_id, ac_project_time_zone_id, dag_id, dag_description):
 	with airflow.DAG(
 		dag_id=dag_id,
 		default_args={
-			'ac_sql_instance': Variable.get('osb.asah.sql.instance'),
 			'ac_project_id': ac_project_id,
 			'ac_project_time_zone_id': ac_project_time_zone_id,
 			'google_project_id': os.environ['GOOGLE_PROJECT_ID'],
@@ -48,36 +42,8 @@ def create_dag(ac_project_id, ac_project_time_zone_id, dag_id, dag_description):
 			task_id='individual_activity_export'
 		)
 
-		psql_truncate_individual_table_job = CloudSQLExecuteQueryOperator(
-			gcp_cloudsql_conn_id="google_cloud_sql",
-			sql="TRUNCATE TABLE {{dag.default_args['ac_project_id']}}.individual",
-			task_id="truncate_individual_table"
-		)
-
-		psql_individual_import_job = CloudSQLCSVImportOperator(
-			bucket_name="{{dag.default_args['google_project_id']}}-data-replica",
-			bucket_prefix="{{dag.default_args['ac_project_id']}}/individual/{{ts}}",
-			database="osbasah",
-			gcp_conn_id="google_cloud_default",
-			instance="{{dag.default_args['ac_sql_instance']}}",
-			table="{{dag.default_args['ac_project_id']}}.individual",
-			task_id='cloudsql_individual_import'
-		)
-
-		psql_individual_activity_import_job = CloudSQLCSVImportOperator(
-			bucket_name="{{dag.default_args['google_project_id']}}-data-replica",
-			bucket_prefix="{{dag.default_args['ac_project_id']}}/individual-activity/{{ts}}",
-			database="osbasah",
-			gcp_conn_id="google_cloud_default",
-			instance="{{dag.default_args['ac_sql_instance']}}",
-			table="{{dag.default_args['ac_project_id']}}.individualactivity",
-			task_id='cloudsql_individual_activity_import'
-		)
-
 		chain(
-			bq_individual_export_job, bq_individual_activity_export_job,
-			psql_truncate_individual_table_job, psql_individual_import_job,
-			psql_individual_activity_import_job
+			bq_individual_export_job, bq_individual_activity_export_job
 		)
 
 		return dag
