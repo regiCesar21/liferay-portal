1b35b00aa86d2627a07fab625f0b5f4865187f87
Marcellus Tavares <marcellus.tavares@liferay.com>
2025-03-06 10:18:14 -0800

    LPD-46363 | LPD-49995 Sync upgrade process step script

--- modules/dxp/apps/osb/osb-asah/osb-asah-upgrade/src/main/resources/v4_12_0/upgrade.sql
+++ modules/dxp/apps/osb/osb-asah/osb-asah-upgrade/src/main/resources/v4_12_0/upgrade.sql
@@ -1,7 +1,8 @@
 CREATE TABLE IF NOT EXISTS Individual (
-    id TEXT PRIMARY KEY,
-    emailAddress TEXT,
-    fields JSON
+	id TEXT PRIMARY KEY,
+	emailAddress TEXT,
+	fields JSON,
+	suppressed BOOLEAN
 );
 
 CREATE TABLE IF NOT EXISTS IndividualActivity (
@@ -13,4 +14,22 @@ CREATE TABLE IF NOT EXISTS IndividualActivity (
 	eventId TEXT,
 	properties JSON,
 	individualId TEXT
+);
+
+CREATE TABLE IF NOT EXISTS IndividualInterest (
+	channelId BIGINT,
+	individualId TEXT,
+	interested BOOLEAN,
+	interestScore DOUBLE PRECISION,
+	keyword TEXT,
+	recordedDate DATE
+);
+
+CREATE TABLE IF NOT EXISTS IndividualSegment (
+	createDate TIMESTAMPTZ,
+	channelId BIGINT,
+	individualId TEXT,
+	modifiedDate TIMESTAMPTZ,
+	segmentId BIGINT,
+	status TEXT
 );
