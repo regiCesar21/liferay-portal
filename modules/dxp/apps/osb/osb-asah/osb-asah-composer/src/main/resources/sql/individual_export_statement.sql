c5e58485e9f5d3a76d05bba0376157481f845fde
Rachael Koestartyo <rachael.koestartyo@liferay.com>
2025-03-05 16:16:25 -0800

    LPD-46371 | LPD-49124 modify json string

--- modules/dxp/apps/osb/osb-asah/osb-asah-composer/src/main/resources/sql/individual_export_statement.sql
+++ modules/dxp/apps/osb/osb-asah/osb-asah-composer/src/main/resources/sql/individual_export_statement.sql
@@ -8,7 +8,9 @@ EXPORT DATA
 	)
 AS (
 	SELECT
-		id, emailAddress, TO_JSON_STRING(fields) AS fields
+		id, emailAddress, JSON_OBJECT(ARRAY_AGG(fields.name), ARRAY_AGG(fields.value)) AS fields, suppressed
 	FROM
-		`{{ dag.default_args['ac_project_id'] }}.individual`
+		`{{ dag.default_args['ac_project_id'] }}.individual`, UNNEST(fields) AS fields
+	GROUP BY
+		id, emailAddress, suppressed
 );
