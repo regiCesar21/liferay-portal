3b427913416943dfbbbd3e02175e21b71aa6c247
Leslie Wong <8095751+lesliewong92@users.noreply.github.com>
2025-03-14 14:57:59 -0700

    LPD-46363 | LPD-50076 Create script to create folder in bucket to store template

---
+++ modules/dxp/apps/osb/osb-asah/osb-asah-dataflow-java/create_flex_template_bucket.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+PROJECT_ID=$(gcloud config get-value project)
+
+BUCKET_NAME="${PROJECT_ID}-dataflow"
+
+FLEX_TEMPLATE_BUCKET=$(gcloud storage ls --project=${PROJECT_ID} | grep -e "^gs://${PROJECT_ID}-dataflow/$" )
+
+if [ -z "$FLEX_TEMPLATE_BUCKET" ]
+then
+	echo "Unable to find bucket ${BUCKET_NAME}"
+	exit 1
+fi
+
+gcloud storage folders create --recursive gs://$BUCKET_NAME/flex-templates
