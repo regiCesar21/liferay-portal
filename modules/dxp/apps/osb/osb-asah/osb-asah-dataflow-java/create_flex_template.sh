4661e906de357e66517e1de2f7d2cf6e82db0942
Leslie Wong <8095751+lesliewong92@users.noreply.github.com>
2025-03-17 10:59:33 -0700

    LPD-46363 | LPD-50076 SF

--- modules/dxp/apps/osb/osb-asah/osb-asah-dataflow-java/create_flex_template.sh
+++ modules/dxp/apps/osb/osb-asah/osb-asah-dataflow-java/create_flex_template.sh
@@ -5,16 +5,18 @@ declare -A templates=( ["postgresql-replication-pipeline"]="com.liferay.osb.asah
 if [ "$#" -lt 2 ]
 then
 	echo -e "Please provide template name and version as arguments.\nPossible template names:"
+
 	printf '  * %s\n' "${!templates[@]}"
 
 	GIT_TAGS=$(git tag --points-at HEAD)
 
-	if [ ! -z "$GIT_TAGS" ]
+	if [ ! -z "${GIT_TAGS}" ]
 	then
-	  SUGGESTED_VERSION_NAMES=($GIT_TAGS)
+		SUGGESTED_VERSION_NAMES=($GIT_TAGS)
 
-	  echo -e "\nSuggested template version(s):"
-	  printf '  * %s\n' "${SUGGESTED_VERSION_NAMES[@]}"
+		echo -e "\nSuggested template version(s):"
+
+		printf '  * %s\n' "${SUGGESTED_VERSION_NAMES[@]}"
 	fi
 
 	exit 1
@@ -23,13 +25,16 @@ fi
 if [ ! -v templates["$1"] ]
 then
 	echo "Template $1 does not exist. Possible values:"
+
 	printf '  * %s\n' "${!templates[@]}"
+
 	exit 1
 fi
 
 if [ ! -f build/libs/osb-asah-dataflow-java-all.jar ]
 then
 	echo "Unable to find JAR file build/libs/osb-asah-dataflow-java.jar. Please run gradlew clean shadowJar --refresh-dependencies."
+
 	exit 1
 fi
 
@@ -41,24 +46,26 @@ REGION=$(gcloud config get-value compute/region)
 
 ARTIFACT_REPOSITORY=$(gcloud artifacts repositories list --location=${REGION} --filter='name="projects/'"${PROJECT_ID}"'/locations/'"${REGION}"'/repositories/ac-dataflow"')
 
-if [ -z "$ARTIFACT_REPOSITORY" ]
+if [ -z "${ARTIFACT_REPOSITORY}" ]
 then
-	echo "Unable to find repository in artifact registry. Please run create_flex_template_repository.sh"
+	echo "Unable to find repository in artifact registry. Please run create_flex_template_repository.sh and try again."
+
 	exit 1
 fi
 
 FLEX_TEMPLATE_BUCKET=$(gcloud storage ls gs://${PROJECT_ID}-dataflow/flex-templates | grep -e "gs://${PROJECT_ID}-dataflow/flex-templates/$" )
 
-if [ -z "$FLEX_TEMPLATE_BUCKET" ]
+if [ -z "${FLEX_TEMPLATE_BUCKET}" ]
 then
-	echo "Unable to find GCS bucket for flex templates. Please run create_flex_template_bucket.sh"
+	echo "Unable to find GCS bucket for flex templates. Please run create_flex_template_bucket.sh and try again."
+
 	exit 1
 fi
 
 gcloud dataflow flex-template build gs://${PROJECT_ID}-dataflow/flex-templates/${FLEX_TEMPLATE_NAME}.json \
 	--env FLEX_TEMPLATE_JAVA_MAIN_CLASS="${FLEX_TEMPLATE_JAVA_MAIN_CLASS}" \
 	--flex-template-base-image JAVA21 \
-	--image-gcr-path "${REGION}-docker.pkg.dev/${PROJECT_ID}/ac-dataflow/${FLEX_TEMPLATE_NAME}:$FLEX_TEMPLATE_VERSION" \
+	--image-gcr-path "${REGION}-docker.pkg.dev/${PROJECT_ID}/ac-dataflow/${FLEX_TEMPLATE_NAME}:${FLEX_TEMPLATE_VERSION}" \
 	--jar "build/libs/osb-asah-dataflow-java-all.jar" \
 	--metadata-file "flex-templates/metadata/${FLEX_TEMPLATE_NAME}.json" \
 	--sdk-language "JAVA"
--- modules/dxp/apps/osb/osb-asah/osb-asah-dataflow-java/create_flex_template_repository.sh
+++ modules/dxp/apps/osb/osb-asah/osb-asah-dataflow-java/create_flex_template_repository.sh
@@ -4,4 +4,4 @@ export REGION=$(gcloud config get-value compute/region)
 
 gcloud artifacts repositories create ac-dataflow \
 	--repository-format=docker \
-	--location=$REGION
+	--location=${REGION}
