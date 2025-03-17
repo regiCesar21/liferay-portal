40adf4d6bd0aa703ebebcb8133e56e993a61857e
Brian Chan <brian.chan@liferay.com>
2025-03-17 15:39:55 -0300

    LPD-50076 Sort

--- modules/dxp/apps/osb/osb-asah/osb-asah-dataflow-java/create_flex_template_repository.sh
+++ modules/dxp/apps/osb/osb-asah/osb-asah-dataflow-java/create_flex_template_repository.sh
@@ -3,5 +3,5 @@
 export REGION=$(gcloud config get-value compute/region)
 
 gcloud artifacts repositories create ac-dataflow \
-	--repository-format=docker \
-	--location=${REGION}
+	--location=${REGION} \
+	--repository-format=docker
