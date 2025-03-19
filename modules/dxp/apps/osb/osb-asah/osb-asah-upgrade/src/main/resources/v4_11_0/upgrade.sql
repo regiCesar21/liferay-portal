7fc0cc9b241be94d7395260830dd45e5f46d9862
Marcellus Tavares <marcellus.tavares@liferay.com>
2025-03-19 10:48:59 -0700

    LPD-46363 | LPD-51697 Delete previous steps

--- modules/dxp/apps/osb/osb-asah/osb-asah-upgrade/src/main/resources/v4_11_0/upgrade.sql
+++
@@ -1,2 +0,0 @@
-ALTER TABLE DataSource ADD IF NOT EXISTS contentRecommenderMostPopularItemsEnabled BOOLEAN DEFAULT false;
-ALTER TABLE DataSource ADD IF NOT EXISTS contentRecommenderUserPersonalizationEnabled BOOLEAN DEFAULT false;
