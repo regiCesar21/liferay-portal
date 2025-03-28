251d974a3bb91c28df090a6cf4c69fe459e7fed6
Brian Chan <brian.chan@liferay.com>
2025-03-28 09:09:09 -0400

    LPD-52113 SF

--- modules/dxp/apps/osb/osb-asah/osb-asah-dataflow-java/create_data_replica_bucket.sh
+++ modules/dxp/apps/osb/osb-asah/osb-asah-dataflow-java/create_data_replica_bucket.sh
@@ -3,7 +3,7 @@
 PROJECT_ID=$(gcloud config get-value project)
 REGION=$(gcloud config get-value compute/region)
 
-echo '{ "rule": [ { "action": { "type": "Delete" }, "condition": { "age": 400 } } ] }' > data-replica-bucket-lifecycle.json
+echo '{"rule": [{"action": {"type": "Delete"}, "condition": {"age": 400}}]}' > data-replica-bucket-lifecycle.json
 
 gcloud storage buckets create gs://${PROJECT_ID}-data-replica --location=${REGION} --lifecycle-file=data-replica-bucket-lifecycle.json
 
