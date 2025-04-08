93c8fd957b936f4c776c5f42d6e1d18c8130034f
Leslie Wong <8095751+lesliewong92@users.noreply.github.com>
2025-04-08 10:30:59 -0700

    LPD-44377 | LPD-52610 Only expose what is necessary

--- modules/dxp/apps/osb/osb-asah/osb-asah-backend/src/main/java/com/liferay/osb/asah/backend/rest/controller/NanitesRestController.java
+++ modules/dxp/apps/osb/osb-asah/osb-asah-backend/src/main/java/com/liferay/osb/asah/backend/rest/controller/NanitesRestController.java
@@ -22,37 +22,14 @@ import org.springframework.web.bind.annotation.RestController;
 /**
  * @author Leslie Wong
  */
-@ConditionalOnProperty(
-	matchIfMissing = true, value = "osb.asah.enable.scheduling"
-)
 @Profile("!prod")
 @RequestMapping("/nanites")
 @RestController
 public class NanitesRestController {
 
-	@PostMapping("/remove-schedule")
-	public ResponseEntity<String> removeSchedule() {
-		return _nanitesHttp.removeSchedule();
-	}
-
-	@PostMapping("/reschedule")
-	public ResponseEntity<String> reschedule() {
-		return _nanitesHttp.rescheduleNanites();
-	}
-
 	@PostMapping("/run")
 	public ResponseEntity<String> run(@RequestBody String json) {
 		return _nanitesHttp.run(new JSONArray(json));
-	}
-
-	@PostMapping("/schedule/{asahTaskId}")
-	public ResponseEntity<String> schedule(@PathVariable Long asahTaskId) {
-		return _nanitesHttp.scheduleAsahTask(asahTaskId);
-	}
-
-	@PostMapping("/unschedule/{asahTaskId}")
-	public ResponseEntity<String> unschedule(@PathVariable Long asahTaskId) {
-		return _nanitesHttp.unscheduleAsahTask(asahTaskId);
 	}
 
 	@Autowired
