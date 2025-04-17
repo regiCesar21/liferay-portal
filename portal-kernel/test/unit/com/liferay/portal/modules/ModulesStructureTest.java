f9d1457fa59c7657b850e69d5ea9793cc5cad4f0
Alan Huang <alan.huang@liferay.com>
2025-04-17 18:07:08 +0800

    LPD-35795 SF

--- portal-kernel/test/unit/com/liferay/portal/modules/ModulesStructureTest.java
+++ portal-kernel/test/unit/com/liferay/portal/modules/ModulesStructureTest.java
@@ -748,14 +748,13 @@ public class ModulesStructureTest {
 
 		if (!sortedBuildExtGradleFileNames.isEmpty()) {
 			StringBundler sb = new StringBundler(
-				(4 * sortedBuildExtGradleFileNames.size()) + 2);
+				(sortedBuildExtGradleFileNames.size() * 3) + 2);
 
 			sb.append(buildGradleTemplate);
 			sb.append(StringPool.NEW_LINE);
 
 			for (String fileName : sortedBuildExtGradleFileNames) {
-				sb.append(StringPool.NEW_LINE);
-				sb.append("apply from: \"");
+				sb.append("\napply from: \"");
 				sb.append(fileName);
 				sb.append("\"");
 			}
@@ -1118,8 +1117,7 @@ public class ModulesStructureTest {
 		if (Files.notExists(dirPath.resolve("settings-ext.gradle"))) {
 			settingsGradleTemplate = StringUtil.removeSubstring(
 				settingsGradleTemplate,
-				StringPool.NEW_LINE + StringPool.NEW_LINE +
-					"apply from: \"settings-ext.gradle\"");
+				"\n\napply from: \"settings-ext.gradle\"");
 		}
 
 		if (!dxpRepo && !privateRepo && !readOnlyRepo) {
