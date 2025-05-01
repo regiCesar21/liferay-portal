3db296ffd138323dad6dccf3dae61a8da9d8f1c5
Marcellus Tavares <marcellus.tavares@liferay.com>
2025-05-01 10:32:10 -0700

    LPD-50658 | LPD-54504 Upgrade

--- modules/dxp/apps/osb/osb-asah/osb-asah-upgrade/src/main/resources/v4_13_0/big_query_upgrade_schema.sql
+++ modules/dxp/apps/osb/osb-asah/osb-asah-upgrade/src/main/resources/v4_13_0/big_query_upgrade_schema.sql
@@ -1,3 +1,6 @@
 ALTER TABLE DocumentLibraryDaily ADD COLUMN impressions INT64;
 
+ALTER TABLE Event ADD COLUMN externalReferenceCode STRING;
+ALTER TABLE Event ADD COLUMN objectType STRING;
+
 UPDATE DocumentLibraryDaily SET impressions = previews WHERE previews IS NOT NULL;
