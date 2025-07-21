7878a5367ef804948b4339aefa2b22f7724f0672
Leslie Wong <8095751+lesliewong92@users.noreply.github.com>
2025-07-21 13:14:45 -0700

    LPD-61301 | LPD-61302 Register upgrade step

--- modules/dxp/apps/osb/osb-asah/osb-asah-upgrade/src/main/java/com/liferay/osb/asah/upgrade/UpgradeProcessConfiguration.java
+++ modules/dxp/apps/osb/osb-asah/osb-asah-upgrade/src/main/java/com/liferay/osb/asah/upgrade/UpgradeProcessConfiguration.java
@@ -10,6 +10,7 @@ import com.liferay.osb.asah.upgrade.v4_12_0.DataReplicationTablePrimaryKeyUpgrad
 import com.liferay.osb.asah.upgrade.v4_12_0.PostgreSQLUpgradeStep;
 import com.liferay.osb.asah.upgrade.v4_13_0.BigQuerySchemaUpgradeStep;
 import com.liferay.osb.asah.upgrade.v4_13_0.EventDefinitionUpgradeStep;
+import com.liferay.osb.asah.upgrade.v4_13_1.EventAttributeDefinitionUpgradeStep;
 import com.liferay.osb.asah.upgrade.v4_13_1.EventUpgradeStep;
 import com.liferay.osb.asah.upgrade.v4_13_2.EventGroupIdUpgradeStep;
 import com.liferay.osb.asah.upgrade.v4_13_2.ObjectEntryUpgradeStep;
@@ -38,9 +39,11 @@ public class UpgradeProcessConfiguration {
 			"4.0.46", "4.0.47", _bigQuerySchemaUpgradeStep);
 		upgradeProcess.addUpgradeSteps(
 			"4.0.47", "4.0.48", _eventDefinitionUpgradeStep);
-		upgradeProcess.addUpgradeSteps("4.0.48", "4.0.49", _eventUpgradeStep);
 		upgradeProcess.addUpgradeSteps(
-			"4.0.49", "4.0.50", _eventGroupIdUpgradeStep,
+			"4.0.48", "4.0.49", _eventDefinitionUpgradeStep);
+		upgradeProcess.addUpgradeSteps("4.0.49", "4.0.50", _eventUpgradeStep);
+		upgradeProcess.addUpgradeSteps(
+			"4.0.50", "4.0.51", _eventGroupIdUpgradeStep,
 			_objectEntryUpgradeStep);
 
 		return upgradeProcess;
@@ -55,6 +58,10 @@ public class UpgradeProcessConfiguration {
 	@Autowired
 	private DataReplicationTablePrimaryKeyUpgradeStep
 		_dataReplicationTablePrimaryKeyUpgradeStep;
+
+	@Autowired
+	private EventAttributeDefinitionUpgradeStep
+		_eventAttributeDefinitionUpgradeStep;
 
 	@Autowired
 	private EventDefinitionUpgradeStep _eventDefinitionUpgradeStep;
