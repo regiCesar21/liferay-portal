09f63255fbf48a6e637ec0bad2cb5a077abc1aaf
Leslie Wong <8095751+lesliewong92@users.noreply.github.com>
2025-08-05 16:53:32 -0700

    LPD-56675 | LPD-61732 Speed up Dataflow job deployment with flex template

--- modules/dxp/apps/osb/osb-asah/osb-asah-dataflow-java/run_event.sh
+++ modules/dxp/apps/osb/osb-asah/osb-asah-dataflow-java/run_event.sh
@@ -1,40 +1,34 @@
 #!/bin/bash
 
-if [ "$#" -lt 1 ]
+if [ "$#" -lt 3 ]
 then
-	echo "Usage: run-event.sh [dxp-cloud-project]"
+	echo "Usage: run-event.sh [dxp-cloud-project] [network] [subnetwork]"
 	exit 1
 fi
 
 PROJECT_ID=$(gcloud config get-value project)
 
 DXP_CLOUD_PROJECT=${1}
-MAIN_CLASS_NAME=com.liferay.osb.asah.dataflow.ingestion.event.EventIngestionPipeline
 MAX_NUMBER_WORKERS=10
 NETWORK=${2}
 OUTPUT_FOLDER=gs://${PROJECT_ID}-analytics-events
 PIPELINE_FOLDER=gs://${PROJECT_ID}-dataflow
 REGION=$(gcloud config get-value compute/region)
-RUNNER=DataflowRunner
 SESSION_WINDOW_ALLOWED_LATENESS=5
 SESSION_WINDOW_GAP_DURATION=30
 SUBNETWORK=${3}
 
-../gradlew clean assemble execute \
-	-Dexec.args=" \
-		--enableStreamingEngine \
-		--inputSubscription=projects/${PROJECT_ID}/subscriptions/${DXP_CLOUD_PROJECT}_analytics_events_dataflow \
-		--jobName=eventingestionpipeline-${DXP_CLOUD_PROJECT} \
-		--maxNumWorkers=${MAX_NUMBER_WORKERS} \
-		--network=${NETWORK} \
-		--outputDirectory=${OUTPUT_FOLDER} \
-		--outputFileNamePrefix=analytics-events \
-		--project=${PROJECT_ID} \
-		--region=${REGION} \
-		--runner=${RUNNER} \
-		--sessionWindowAllowedLateness=${SESSION_WINDOW_ALLOWED_LATENESS} \
-		--sessionWindowGapDuration=${SESSION_WINDOW_GAP_DURATION} \
-		--subnetwork=regions/${REGION}/subnetworks/${SUBNETWORK} \
-		--tempLocation=${PIPELINE_FOLDER}/temp" \
-	-Dexec.cleanupDaemonThreads=false \
-	-Dexec.mainClass=${MAIN_CLASS_NAME}
+gcloud dataflow flex-template run eventingestionpipeline-${DXP_CLOUD_PROJECT} \
+	--enable-streaming-engine \
+	--template-file-gcs-location="${PIPELINE_FOLDER}/flex-templates/event-ingestion-pipeline.json" \
+	--max-workers=${MAX_NUMBER_WORKERS} \
+	--network=${NETWORK} \
+	--parameters="inputSubscription=projects/${PROJECT_ID}/subscriptions/${DXP_CLOUD_PROJECT}_analytics_events_dataflow,\
+outputDirectory=${OUTPUT_FOLDER},\
+outputFileNamePrefix=analytics-events,\
+sessionWindowAllowedLateness=${SESSION_WINDOW_ALLOWED_LATENESS},\
+sessionWindowGapDuration=${SESSION_WINDOW_GAP_DURATION}" \
+	--project=${PROJECT_ID} \
+	--region=${REGION} \
+	--subnetwork=regions/${REGION}/subnetworks/${SUBNETWORK} \
+	--temp-location=${PIPELINE_FOLDER}/temp
